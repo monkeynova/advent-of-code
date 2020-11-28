8c511a3a83d475d6cdcd0f7e57777d28ea307bdf
Keith Peters <keith@monkeynova.com>
2020-11-28 06:42:20 -0800

    create vscode workspace and update perl day1 solutions from command line oneliners to programs

--- 2019/day1/1.pl
+++ 2019/day1/1.pl
@@ -1,1 +1,12 @@
-while (<>) { $sum += int($_ /3) - 2; } print $sum, "\n";
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+my $sum = 0;
+
+while (<>) {
+    $sum += int($_ /3) - 2;
+}
+
+print $sum, "\n";
--- 2019/day1/2.pl
+++ 2019/day1/2.pl
@@ -1,1 +1,18 @@
-while (<>) { $fuel = int($_ /3) - 2; $ff = $fuel; while (int($ff / 3) - 2 > 0) { $ff = int($ff / 3) - 2; $fuel += $ff; } $sum += $fuel } print $sum, "\n"
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my $sum = 0;
+
+while (<>) {
+    my $fuel = int($_ /3) - 2;
+    my $ff = $fuel;
+    while (int($ff / 3) - 2 > 0) {
+         $ff = int($ff / 3) - 2;
+         $fuel += $ff;
+    }
+    $sum += $fuel
+}
+
+print $sum, "\n"
