9359322ed1cee876cc6f3268d9aeeb1e5394a061
Keith Peters <keith@monkeynova.com>
2020-11-28 11:08:02 -0800

    add a "NewDay" path for an empty days work

---
+++ advent_of_code/new_day_path/new_day.cc
@@ -0,0 +1,14 @@
+#include "advent_of_code/new_day_path/new_day.h"
+
+#include "absl/strings/numbers.h"
+#include "absl/strings/str_cat.h"
+
+absl::StatusOr<std::vector<std::string>> NewDay::Part1(
+    const std::vector<absl::string_view>& input) const {
+  return std::vector<std::string>{""};
+}
+
+absl::StatusOr<std::vector<std::string>> NewDay::Part2(
+    const std::vector<absl::string_view>& input) const {
+  return std::vector<std::string>{""};
+}
