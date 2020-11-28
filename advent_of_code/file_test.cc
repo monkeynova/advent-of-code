bf2761e1fdf339646f0842a1c4a60f0afeea926f
Keith Peters <keith@monkeynova.com>
2020-11-28 10:53:10 -0800

    add an include mechanism for the FBTD work

--- advent_of_code/file_test.cc
+++ advent_of_code/file_test.cc
@@ -2,6 +2,8 @@
 
 #include "absl/flags/flag.h"
 #include "absl/strings/str_split.h"
+#include "base/file_util.h"
+#include "re2/re2.h"
 #include "run_test_case_result.h"
 #include "test_case_options.h"
 
@@ -19,6 +21,26 @@ void RunTestCase(const AdventDay* advent_day,
   options.RegisterInt64(kPartOption, 0);
 
   std::string test_case = std::string(test_case_with_options);
+  absl::string_view include_fname;
+  RE2 include_pattern{"@include{([^}]*)}"};
+  while (RE2::PartialMatch(test_case, include_pattern, &include_fname)) {
+    std::string contents;
+    // TODO(@monkeynova): This is terrible. Both in terms of using the
+    // ::internal namespace (though we're using the same fetching code as
+    // the rest of FBTD this way) as well as the need for the ./ without
+    // which the load fails.
+    if (absl::Status st = file_based_test_driver::internal::GetContents(
+            absl::StrCat("./", include_fname), &contents);
+        !st.ok()) {
+      test_result->AddTestOutput(
+          absl::StrCat("ERROR: Unable to include file \"", include_fname,
+                       "\": ", st.message()));
+      return;
+    }
+    RE2::Replace(&test_case, absl::StrCat("@include{", include_fname, "}"),
+                 contents);
+  }
+
   if (absl::Status st = options.ParseTestCaseOptions(&test_case); !st.ok()) {
     test_result->AddTestOutput(
         absl::StrCat("ERROR: Could not parse options: ", st.message()));
