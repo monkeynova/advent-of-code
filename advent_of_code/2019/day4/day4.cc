befa28c246961f5938942fa395086e00680f1048
Keith Peters <keith@monkeynova.com>
2020-11-29 13:58:55 -0800

    Add an empty 2019/09

---
+++ advent_of_code/2019/day4/day4.cc
@@ -0,0 +1,15 @@
+#include "advent_of_code/2019/day4/day4.h"
+
+#include "absl/strings/numbers.h"
+#include "absl/strings/str_cat.h"
+#include "glog/logging.h"
+
+absl::StatusOr<std::vector<std::string>> Day4_2019::Part1(
+    const std::vector<absl::string_view>& input) const {
+  return std::vector<std::string>{""};
+}
+
+absl::StatusOr<std::vector<std::string>> Day4_2019::Part2(
+    const std::vector<absl::string_view>& input) const {
+  return std::vector<std::string>{""};
+}
---
+++ advent_of_code/2019/day4/day4.h
@@ -0,0 +1,14 @@
+#ifndef ADVENT_OF_CODE_2019_DAY4_H
+#define ADVENT_OF_CODE_2019_DAY4_H
+
+#include "advent_of_code/advent_day.h"
+
+class Day4_2019 : public AdventDay {
+ public:
+  absl::StatusOr<std::vector<std::string>> Part1(
+      const std::vector<absl::string_view>& input) const override;
+  absl::StatusOr<std::vector<std::string>> Part2(
+      const std::vector<absl::string_view>& input) const override;
+};
+
+#endif  // ADVENT_OF_CODE_2019_DAY4_H
