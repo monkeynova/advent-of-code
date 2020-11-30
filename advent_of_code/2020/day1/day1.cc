813b49b1fe2877bddb49293b8c82c9058c632dd6
Keith Peters <keith@monkeynova.com>
2020-11-30 15:31:14 -0800

    Empty 2020/01

---
+++ advent_of_code/2020/day1/day1.cc
@@ -0,0 +1,16 @@
+#include "advent_of_code/2020/day1/day1.h"
+
+#include "absl/strings/numbers.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_split.h"
+#include "glog/logging.h"
+
+absl::StatusOr<std::vector<std::string>> Day1_2020::Part1(
+    const std::vector<absl::string_view>& input) const {
+  return std::vector<std::string>{""};
+}
+
+absl::StatusOr<std::vector<std::string>> Day1_2020::Part2(
+    const std::vector<absl::string_view>& input) const {
+  return std::vector<std::string>{""};
+}
