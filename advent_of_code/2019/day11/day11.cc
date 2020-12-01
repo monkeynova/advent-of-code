e646f5f557d8956b3cc62b1215297ace6bc5449f
Keith Peters <keith@monkeynova.com>
2020-11-30 16:36:37 -0800

    2019/11 Part2

--- advent_of_code/2019/day11/day11.cc
+++ advent_of_code/2019/day11/day11.cc
@@ -52,6 +52,10 @@ class Painter : public IntCode::InputSource, public IntCode::OutputSink {
     return it->second;
   }
 
+  void Set(Point p, int val) {
+    panel_to_painted_color_[p] = val;
+  }
+
   absl::Status Put(int64_t val) override {
     if (val != 0 && val != 1) return absl::InvalidArgumentError("Bad output value");
     if (paint) {
@@ -82,6 +86,30 @@ class Painter : public IntCode::InputSource, public IntCode::OutputSink {
     return panel_to_painted_color_.size();
   }
 
+  std::vector<std::string> Panels() {
+    Point min{.x = std::numeric_limits<int>::max(), .y = std::numeric_limits<int>::max()};
+    Point max{.x = std::numeric_limits<int>::min(), .y = std::numeric_limits<int>::min()};
+    for (const auto& point_and_color : panel_to_painted_color_) {
+      const Point& p = point_and_color.first;
+      min.x = std::min(min.x, p.x);
+      min.y = std::min(min.y, p.y);
+      max.x = std::max(max.x, p.x);
+      max.y = std::max(max.y, p.y);
+    }
+    LOG(WARNING) << min << " => " << max;
+    std::vector<std::string> ret;
+    for (int y = min.y; y <= max.y; ++y) {
+      std::string next;
+      for (int x = min.x; x <= max.x; ++x) {
+        auto it = panel_to_painted_color_.find({.x = x, .y = y});
+        bool is_white = (it != panel_to_painted_color_.end() && it->second == 1);
+        next.append(is_white ? "X" : " ");
+      }
+      ret.push_back(next);
+    }
+    return ret;
+  }
+
  private:
   bool paint = true;
   Point cur_ = {0, 0};
@@ -102,5 +130,12 @@ absl::StatusOr<std::vector<std::string>> Day11_2019::Part1(
 
 absl::StatusOr<std::vector<std::string>> Day11_2019::Part2(
     const std::vector<absl::string_view>& input) const {
-  return std::vector<std::string>{""};
+  absl::StatusOr<IntCode> codes = IntCode::Parse(input);
+  if (!codes.ok()) return codes.status();
+
+  Painter painter;
+  painter.Set({.x = 0, .y = 0}, 1);
+  if (absl::Status st = codes->Run(&painter, &painter); !st.ok()) return st;
+
+  return painter.Panels();
 }
