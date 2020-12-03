377aa5cd97110e02654d455d033258181739b98e
Keith Peters <keith@monkeynova.com>
2020-12-03 07:07:59 -0800

    Benchmark targets for all of 2020

---
+++ advent_of_code/2020/day3/day3_benchmark.cc
@@ -0,0 +1,13 @@
+#include "advent_of_code/2020/day3/day3.h"
+#include "advent_of_code/file_benchmark.h"
+#include "gmock/gmock.h"
+
+constexpr int kMaxTestCount = 4;
+
+TEST(DayBenchmarkTest, Consistency) {
+  absl::StatusOr<std::vector<DirtyTestParseResult>> tests = FileBenchmarkTests();
+  ASSERT_TRUE(tests.ok());
+  EXPECT_EQ(kMaxTestCount, tests->size());
+}
+
+BENCHMARK_TEMPLATE(BM_Day, Day3_2020)->DenseRange(0, kMaxTestCount - 1);
