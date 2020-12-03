594fee5464eefbf9f0c58a83e6fa29121d22bdb5
Keith Peters <keith@monkeynova.com>
2020-12-03 07:01:30 -0800

    Update new_day_path to include a benchmark

---
+++ advent_of_code/new_day_path/new_day_benchmark.cc
@@ -0,0 +1,13 @@
+#include "advent_of_code/new_day_path/new_day.h"
+#include "advent_of_code/file_benchmark.h"
+#include "gmock/gmock.h"
+
+constexpr int kMaxTestCount = 2;
+
+TEST(DayBenchmarkTest, Consistency) {
+  absl::StatusOr<std::vector<DirtyTestParseResult>> tests = FileBenchmarkTests();
+  ASSERT_TRUE(tests.ok()) << tests.status().message();
+  EXPECT_EQ(kMaxTestCount, tests->size());
+}
+
+BENCHMARK_TEMPLATE(BM_Day, NewDay)->DenseRange(0, kMaxTestCount - 1);
