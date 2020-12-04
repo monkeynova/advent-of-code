0eccc5f097f7eb69d0ddd7d73e8da97a40999574
Keith Peters <keith@monkeynova.com>
2020-12-03 21:09:09 -0800

    2020/04 Part1

--- advent_of_code/2020/day4/day4.cc
+++ advent_of_code/2020/day4/day4.cc
@@ -8,9 +8,36 @@
 #include "glog/logging.h"
 #include "re2/re2.h"
 
+bool Valid(const absl::flat_hash_map<std::string, std::string>& passport) {
+  for(absl::string_view test : {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}) {
+    if (!passport.contains(test)) return false;
+  }
+  return true;
+}
+
 absl::StatusOr<std::vector<std::string>> Day4_2020::Part1(
     const std::vector<absl::string_view>& input) const {
-  return std::vector<std::string>{""};
+  absl::flat_hash_map<std::string, std::string> passport;
+  int valid = 0;
+  for (absl::string_view line : input) {
+    if (line.empty()) {
+      if (Valid(passport)) {
+        ++valid;
+      }
+      passport.clear();
+    }
+    std::vector<absl::string_view> records = absl::StrSplit(line, " ");
+    for (absl::string_view r : records) {
+      if (r.empty()) continue;
+      std::vector<absl::string_view> kv = absl::StrSplit(r, ":");
+      if (kv.size() != 2) return absl::InvalidArgumentError(absl::StrCat("bad k/v: ", r));
+      passport[kv[0]] = passport[kv[1]];
+    }
+  }
+      if (Valid(passport)) {
+        ++valid;
+      }
+  return std::vector<std::string>{absl::StrCat(valid)};
 }
 
 absl::StatusOr<std::vector<std::string>> Day4_2020::Part2(
