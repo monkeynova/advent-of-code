18449eef09085fb32c60a45f486cab4350c82061
Keith Peters <keith@monkeynova.com>
2020-12-04 07:52:51 -0800

    Empty 2020/05

---
+++ advent_of_code/2020/day5/day5.cc
@@ -0,0 +1,19 @@
+#include "advent_of_code/2020/day5/day5.h"
+
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/strings/numbers.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_split.h"
+#include "glog/logging.h"
+#include "re2/re2.h"
+
+absl::StatusOr<std::vector<std::string>> Day5_2020::Part1(
+    const std::vector<absl::string_view>& input) const {
+  return std::vector<std::string>{""};
+}
+
+absl::StatusOr<std::vector<std::string>> Day5_2020::Part2(
+    const std::vector<absl::string_view>& input) const {
+  return std::vector<std::string>{""};
+}
