ecb82360c31f8e2aa4926458771b47f5ae0861df
Keith Peters <keith@monkeynova.com>
2020-12-06 16:51:46 -0800

    2019/21 Part1

--- advent_of_code/2019/day21/day21.cc
+++ advent_of_code/2019/day21/day21.cc
@@ -5,12 +5,68 @@
 #include "absl/strings/numbers.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_split.h"
+#include "advent_of_code/2019/int_code.h"
 #include "glog/logging.h"
 #include "re2/re2.h"
 
+class SpringBot : public IntCode::IOModule {
+ public:
+  SpringBot(absl::string_view program)
+   : in_buf_(program) {}
+
+ int64_t damage() const { return damage_; }
+
+ bool PauseIntCode() override { return false; }
+
+ absl::StatusOr<int64_t> Fetch() override {
+   if (in_buf_pos_ < in_buf_.size()) {
+     return in_buf_[in_buf_pos_++];
+   }
+   return absl::InternalError("No more input");
+ }
+
+ absl::Status Put(int64_t val) override {
+   if (val >= 128) {
+     damage_ = val;
+     LOG(WARNING) << "Damage: " << damage_;
+   } else if (val == '\n') {
+     LOG(WARNING) << "Output: " << cur_line_;
+     out_buf_.push_back(std::move(cur_line_));
+     cur_line_.clear();
+   } else {
+     cur_line_.append(1, val);
+   }
+   return absl::OkStatus();
+ }
+
+ private:
+  std::string in_buf_;
+  int in_buf_pos_ = 0;
+  std::vector<std::string> out_buf_;
+  std::string cur_line_;
+  int64_t damage_ = -1;
+};
+
 absl::StatusOr<std::vector<std::string>> Day21_2019::Part1(
     const std::vector<absl::string_view>& input) const {
-  return IntReturn(-1);
+  absl::StatusOr<IntCode> codes = IntCode::Parse(input);
+  if (!codes.ok()) return codes.status();
+
+  // JUMP goes 4 spaces forward.
+  // jump if D and any of ABC are missing.
+
+  SpringBot spring_bot(
+    "NOT A J\n"
+    "NOT B T\n"
+    "OR T J\n"
+    "NOT C T\n"
+    "OR T J\n"
+    "AND D J\n"
+    "WALK\n"
+  );
+  if (absl::Status st = codes->Run(&spring_bot); !st.ok()) return st;
+
+  return IntReturn(spring_bot.damage());
 }
 
 absl::StatusOr<std::vector<std::string>> Day21_2019::Part2(
