8fa7b41c4deb0739026aa06c8639abd366ccff68
Keith Peters <keith@monkeynova.com>
2020-12-12 07:08:11 -0800

    Empty 2015/06

---
+++ advent_of_code/2015/day06/day06.cc
@@ -0,0 +1,20 @@
+#include "advent_of_code/2015/day06/day06.h"
+
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/strings/numbers.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_join.h"
+#include "absl/strings/str_split.h"
+#include "glog/logging.h"
+#include "re2/re2.h"
+
+absl::StatusOr<std::vector<std::string>> Day06_2015::Part1(
+    const std::vector<absl::string_view>& input) const {
+  return IntReturn(-1);
+}
+
+absl::StatusOr<std::vector<std::string>> Day06_2015::Part2(
+    const std::vector<absl::string_view>& input) const {
+  return IntReturn(-1);
+}
