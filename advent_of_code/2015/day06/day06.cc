61cc4b00105371beb44c38ce9d949280f383c99c
Keith Peters <keith@monkeynova.com>
2020-12-12 07:25:04 -0800

    2015/06 Part2

--- advent_of_code/2015/day06/day06.cc
+++ advent_of_code/2015/day06/day06.cc
@@ -62,5 +62,50 @@ absl::StatusOr<std::vector<std::string>> Day06_2015::Part1(
 
 absl::StatusOr<std::vector<std::string>> Day06_2015::Part2(
     const std::vector<absl::string_view>& input) const {
-  return IntReturn(-1);
+  std::vector<std::vector<int>> yard;
+  std::vector<int> empty_row;
+  empty_row.resize(1000);
+  for (int i = 0; i < 1000; ++i) {
+    yard.push_back(empty_row);
+  }
+
+  for (absl::string_view str : input) {
+    Point min;
+    Point max;
+    absl::string_view cmd;
+    if (!RE2::FullMatch(str, "(.*) (\\d+),(\\d+) through (\\d+),(\\d+)",
+                        &cmd, &min.x, &min.y, &max.x, &max.y)) {
+      return absl::InvalidArgumentError(absl::StrCat("Bad instruction: ", str));
+    }
+    if (cmd == "turn on") {
+      for (int y = min.y; y <= max.y; ++y) {
+        for (int x = min.x; x <= max.x; ++x) {
+          ++yard[y][x];
+        }
+      }
+    } else if (cmd == "turn off") {
+      for (int y = min.y; y <= max.y; ++y) {
+        for (int x = min.x; x <= max.x; ++x) {
+          if (--yard[y][x] <= 0) yard[y][x] = 0;
+        }
+      }
+    } else if (cmd == "toggle") {
+      for (int y = min.y; y <= max.y; ++y) {
+        for (int x = min.x; x <= max.x; ++x) {
+          yard[y][x] += 2;
+        }
+      }
+    } else {
+      return absl::InvalidArgumentError(absl::StrCat("Bad instruction: ", str));
+    }
+  }
+
+  int total_brightness = 0;
+  for (const std::vector<int>& row : yard) {
+    for (int b : row) {
+      total_brightness += b;
+    }
+  }
+
+  return IntReturn(total_brightness);
 }
