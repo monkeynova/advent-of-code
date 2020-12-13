7402f0bd0edc085f37d29a2e8fc528c424bf1eeb
Keith Peters <keith@monkeynova.com>
2020-12-12 18:25:08 -0800

    Make opt cmp functions templated to allow hadling any comparable types

--- advent_of_code/opt_cmp.h
+++ advent_of_code/opt_cmp.h
@@ -1,34 +1,40 @@
 #ifndef ADVENT_OF_CODE_2019_OPT_CMP_H
 #define ADVENT_OF_CODE_2019_OPT_CMP_H
 
-absl::optional<int> opt_min(absl::optional<int> a, absl::optional<int> b) {
+template <typename T>
+absl::optional<T> opt_min(absl::optional<T> a, absl::optional<T> b) {
   if (!a) return b;
   if (!b) return a;
   return std::min(*a, *b);
 }
 
-absl::optional<int> opt_min(absl::optional<int> a, int b) {
+template <typename T>
+absl::optional<int> opt_min(absl::optional<T> a, T b) {
   if (!a) return b;
   return std::min(*a, b);
 }
 
-absl::optional<int> opt_max(absl::optional<int> a, absl::optional<int> b) {
+template <typename T>
+absl::optional<T> opt_max(absl::optional<T> a, absl::optional<T> b) {
   if (!a) return b;
   if (!b) return a;
   return std::max(*a, *b);
 }
 
-absl::optional<int> opt_max(absl::optional<int> a, int b) {
+template <typename T>
+absl::optional<T> opt_max(absl::optional<T> a, T b) {
   if (!a) return b;
   return std::max(*a, b);
 }
 
-absl::optional<int> opt_add(int d, absl::optional<int> a) {
+template <typename T>
+absl::optional<T> opt_add(T d, absl::optional<T> a) {
   if (!a) return a;
   return *a + d;
 }
 
-absl::optional<int> opt_add(absl::optional<int> a, int d) {
+template <typename T>
+absl::optional<T> opt_add(absl::optional<T> a, T d) {
   return opt_add(d, a);
 }
 
