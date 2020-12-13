a015c60f5f330738d81dfa5606ef846d271777ce
Keith Peters <keith@monkeynova.com>
2020-12-12 18:06:45 -0800

    2015/10 Part1

--- advent_of_code/2015/day10/day10.cc
+++ advent_of_code/2015/day10/day10.cc
@@ -9,9 +9,28 @@
 #include "glog/logging.h"
 #include "re2/re2.h"
 
+std::string LookAndSay(absl::string_view input) {
+  std::string out;
+  for (int i = 0; i < input.size();) {
+    int j;
+    for (j = i; j < input.size() && input[j] == input[i]; ++j) {
+      // Spin.
+    }
+    out.append(absl::StrCat(j - i, input.substr(i, 1)));
+    i = j;
+  }
+  return out;
+}
+
 absl::StatusOr<std::vector<std::string>> Day10_2015::Part1(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  if (input.size() != 1) return Error("Bad input");
+  std::string str = std::string(input[0]);
+  for (int i = 0; i < 40; ++i) {
+    VLOG(1) << str;
+    str = LookAndSay(str);
+  }
+  return IntReturn(str.size());
 }
 
 absl::StatusOr<std::vector<std::string>> Day10_2015::Part2(
