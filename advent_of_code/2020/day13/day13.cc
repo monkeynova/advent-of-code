f81e985a8d05019d93bf9d7b9d4bcc4d4002b96d
Keith Peters <keith@monkeynova.com>
2020-12-12 23:12:04 -0800

    2020/13 Part2

--- advent_of_code/2020/day13/day13.cc
+++ advent_of_code/2020/day13/day13.cc
@@ -31,5 +31,49 @@ absl::StatusOr<std::vector<std::string>> Day13_2020::Part1(
 
 absl::StatusOr<std::vector<std::string>> Day13_2020::Part2(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  if (input.size() != 2) return Error("Bad input");
+  int start;
+  if (!absl::SimpleAtoi(input[0], &start)) return Error("Bad input");
+
+  std::vector<absl::string_view> ids = absl::StrSplit(input[1], ",");
+  int max_id = -1;
+  int max_offset = 0;
+  struct Route {
+    int64_t id;
+    int64_t offset;
+  };
+  std::vector<Route> routes;
+  for (int i = 0; i < ids.size(); ++i) {
+    if (ids[i] == "x") continue;
+    int id;
+    if (!absl::SimpleAtoi(ids[i], &id)) return Error("Bad id:", id);
+    
+    routes.push_back({.id = id, .offset = i});
+    if (id > max_id) {
+      max_id = id;
+      max_offset = i;
+    }
+  }
+
+  int64_t full_cycle_time = 1;
+  for (Route r : routes) {
+    full_cycle_time = full_cycle_time * r.id / std::gcd(r.id, full_cycle_time);
+  }
+  VLOG(1) << "full_cycle_time: " << full_cycle_time;
+
+  int64_t ts = 0;
+  int64_t product = 1;
+  for (Route r : routes) {
+    while (true) {
+      int64_t mod = ts % r.id;
+      int64_t should_mod = (r.id - r.offset) % r.id;
+      if (should_mod < 0) should_mod += r.id;
+      if (mod == should_mod) break;
+      ts += product;
+      VLOG(2) << "ts: " << ts;
+    }
+    product = product * r.id / std::gcd(product, r.id);
+    VLOG(1) << "prodict: " << product;
+  }
+  return IntReturn(ts);
 }
