9612370f6fd7ccb256494433e50254f01138bd76
Keith Peters <keith@monkeynova.com>
2020-12-13 12:15:57 -0800

    2015/14 Part 2

--- advent_of_code/2015/day14/day14.cc
+++ advent_of_code/2015/day14/day14.cc
@@ -17,6 +17,31 @@ struct Flight {
   int rest_time;
 };
 
+struct RaceResult {
+  std::vector<absl::string_view> winners;
+  int dist = std::numeric_limits<int>::min();
+};
+
+RaceResult RunRace(const std::vector<Flight>& flights, int time) {
+  RaceResult res;
+  for (Flight f : flights) {
+    int cycles = time / (f.run_time + f.rest_time);
+    int rem_time = time % (f.run_time + f.rest_time);
+    if (rem_time > f.run_time) {
+      ++cycles;
+      rem_time = 0;
+    }
+    int dist = cycles * f.speed * f.run_time + rem_time * f.speed;
+    if (dist > res.dist) {
+      res.dist = dist;
+      res.winners = {f.name};
+    } else if (dist == res.dist) {
+      res.winners.push_back(f.name);
+    }
+  }
+  return res;
+}
+
 absl::StatusOr<std::vector<std::string>> Day14_2015::Part1(
     absl::Span<absl::string_view> input) const {
   //Prancer can fly 25 km/s for 6 seconds, but then must rest for 143 seconds.
@@ -29,22 +54,30 @@ absl::StatusOr<std::vector<std::string>> Day14_2015::Part1(
     }
     flights.push_back(next);
   }
-  int time = 2503;
-  absl::optional<int> max;
-  for (Flight f : flights) {
-    int cycles = time / (f.run_time + f.rest_time);
-    int rem_time = time % (f.run_time + f.rest_time);
-    if (rem_time > f.run_time) {
-      ++cycles;
-      rem_time = 0;
-    }
-    int dist = cycles * f.speed * f.run_time + rem_time * f.speed;
-    max = opt_max(max, dist);
-  }
-  return IntReturn(max);
+  return IntReturn(RunRace(flights, 2503).dist);
 }
 
 absl::StatusOr<std::vector<std::string>> Day14_2015::Part2(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  std::vector<Flight> flights;
+  for (absl::string_view str : input) {
+    Flight next;
+    if (!RE2::FullMatch(str, "(.*) can fly (\\d+) km/s for (\\d+) seconds, but then must rest for (\\d+) seconds\\.",
+                        &next.name, &next.speed, &next.run_time, &next.rest_time)) {
+      return Error("Bad input: ", str);
+    }
+    flights.push_back(next);
+  }
+  absl::flat_hash_map<absl::string_view, int> points;
+  for (int i = 1; i <= 2503; ++i) {
+    RaceResult rr = RunRace(flights, i);
+    for (absl::string_view name : rr.winners) {
+      ++points[name];
+    }
+  }
+  absl::optional<int> max;
+  for (const auto& pair : points) {
+    max = opt_max(max, pair.second);
+  }
+  return IntReturn(max);
 }
