34488fbbed42db0dc1a9a1ad68bfaac7a8d9593b
Keith Peters <keith@monkeynova.com>
2020-12-12 18:10:05 -0800

    cleanup.sh

--- advent_of_code/2015/day11/day11_benchmark.cc
+++ advent_of_code/2015/day11/day11_benchmark.cc
@@ -1,5 +1,5 @@
-#include "advent_of_code/file_benchmark.h"
 #include "advent_of_code/2015/day11/day11.h"
+#include "advent_of_code/file_benchmark.h"
 #include "gmock/gmock.h"
 
 constexpr int kMaxTestCount = 2;
