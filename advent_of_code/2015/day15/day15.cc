6fc8bc23a8a374bc8ae95d48ebbce282b4ddc4a8
Keith Peters <keith@monkeynova.com>
2020-12-13 15:15:22 -0800

    2015/15 Part2

--- advent_of_code/2015/day15/day15.cc
+++ advent_of_code/2015/day15/day15.cc
@@ -51,6 +51,40 @@ int64_t FindBestScore(absl::Span<Ingredient> ingredients, int q_remain, Ingredie
   return max;
 }
 
+int64_t FindBestScoreRequiredCalories(absl::Span<Ingredient> ingredients, int q_remain, int calories, Ingredient score = {}) {
+  VLOG(1) << "FindBestScore: " << ingredients[0].name << ": " << q_remain;
+  if (ingredients.size() == 1) {
+    score.capacity += ingredients[0].capacity * q_remain;
+    score.durability += ingredients[0].durability * q_remain;
+    score.flavor += ingredients[0].flavor * q_remain;
+    score.texture += ingredients[0].texture * q_remain;
+    score.calories += ingredients[0].calories * q_remain;
+    if (score.calories != calories) return -1;
+    int64_t ret = 1;
+    ret *= score.capacity < 0 ? 0 : score.capacity;
+    ret *= score.durability < 0 ? 0 : score.durability;
+    ret *= score.flavor < 0 ? 0 : score.flavor;
+    ret *= score.texture < 0 ? 0 : score.texture;
+    // ret *= score.calories < 0 ? 0 : score.calories;
+    VLOG(1) << "FindBestScore: = " << ret;
+    return ret;
+  }
+
+  int64_t max = std::numeric_limits<int>::min();
+  for (int i = 0; i <= q_remain; ++i) {
+    if (i > 0) {
+      score.capacity += ingredients[0].capacity;
+      score.durability += ingredients[0].durability;
+      score.flavor += ingredients[0].flavor;
+      score.texture += ingredients[0].texture;
+      score.calories += ingredients[0].calories;
+    }
+    max = std::max(max, FindBestScoreRequiredCalories(ingredients.subspan(1), q_remain - i, calories, score));
+  }
+
+  return max;
+}
+
 
 absl::StatusOr<std::vector<std::string>> Day15_2015::Part1(
     absl::Span<absl::string_view> input) const {
@@ -70,5 +104,16 @@ absl::StatusOr<std::vector<std::string>> Day15_2015::Part1(
 
 absl::StatusOr<std::vector<std::string>> Day15_2015::Part2(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  // Sprinkles: capacity 5, durability -1, flavor 0, texture 0, calories 5
+  std::vector<Ingredient> ingredients;
+  for (absl::string_view str : input) {
+    Ingredient i;
+    if (!RE2::FullMatch(str, "(.*): capacity (-?\\d+), durability (-?\\d+), flavor (-?\\d+), texture (-?\\d+), calories (-?\\d+)",
+                        &i.name, &i.capacity, &i.durability, &i.flavor, &i.texture, &i.calories)) {
+      return Error("Bad input: " , str);
+    }
+    ingredients.push_back(i);
+  }
+
+  return IntReturn(FindBestScoreRequiredCalories(absl::MakeSpan(ingredients), 100, 500));
 }
