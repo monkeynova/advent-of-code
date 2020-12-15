81052f8fb2d8a66a93a810c223bf2fd535c772a9
Keith Peters <keith@monkeynova.com>
2020-12-14 21:17:49 -0800

    2020/15 Part1 (slightly broken on one sample)

--- advent_of_code/2020/day15/day15.cc
+++ advent_of_code/2020/day15/day15.cc
@@ -11,7 +11,31 @@
 
 absl::StatusOr<std::vector<std::string>> Day15_2020::Part1(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  if (input.size() != 1) return Error("Bad input");
+  std::vector<absl::string_view> pieces = absl::StrSplit(input[0], ",");
+
+  absl::flat_hash_map<int, int> hist;
+  int round = 0;
+  int last;
+  for (; round < pieces.size(); ++round) {
+    if (round > 0) {
+      hist[last] = round;
+    }
+    int parsed;
+    if (!absl::SimpleAtoi(pieces[round], &parsed)) return Error("Bad int: ", pieces[round]);
+    last = parsed;
+    VLOG(1) << last;
+  }
+  for (; round < 2020; ++round) {
+    auto it = hist.find(last);
+    int next = it == hist.end() ? 0 : round - it->second;
+    if (round > 0) {
+      hist[last] = round;
+    }
+    last = next;
+    VLOG(1) << last;
+  }
+  return IntReturn(last);
 }
 
 absl::StatusOr<std::vector<std::string>> Day15_2020::Part2(
