a08343d085606e45bf15822b2a1ceddda8834612
Keith Peters <keith@monkeynova.com>
2020-12-15 15:40:54 -0800

    Update benchmark to include tests up to 5s

--- benchmark/run_all_benchmarks.sh
+++ benchmark/run_all_benchmarks.sh
@@ -3,5 +3,5 @@
 echo > ~/benchmark_out.txt
 for t in `bazel query 'filter(".*day\d+_benchmark",...)'`
 do
-  bazel run -c opt $t -- --benchmark | grep BM_ >> ~/benchmark_out.txt
+  bazel run -c opt $t -- --benchmark --run_long_tests=10s| grep BM_ >> ~/benchmark_out.txt
 done
