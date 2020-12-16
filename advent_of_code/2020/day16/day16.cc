92a88a41be65c30628d45927c05ff2a7531ca657
Keith Peters <keith@monkeynova.com>
2020-12-16 08:19:10 -0800

    2020/16 Part2

--- advent_of_code/2020/day16/day16.cc
+++ advent_of_code/2020/day16/day16.cc
@@ -57,6 +57,20 @@ absl::StatusOr<ParseResult> Parse(absl::Span<absl::string_view> input) {
   return res;
 }
 
+bool CheckRange(int v, OrRange or_range) {
+        if (v >= or_range.min1 && v <= or_range.max1) {
+          VLOG(3) << "Valid: [" << or_range.min1 << "," <<  or_range.max1 << "],["
+                  <<or_range.min2 << "," <<  or_range.max2 << "]";
+          return true;
+        }
+        if (v >= or_range.min2 && v <= or_range.max2) {
+          VLOG(3) << "Valid: [" << or_range.min1 << "," <<  or_range.max1 << "],["
+                  <<or_range.min2 << "," <<  or_range.max2 << "]";
+          return true;
+        }
+        return false;
+}
+
 absl::StatusOr<std::vector<std::string>> Day16_2020::Part1(
     absl::Span<absl::string_view> input) const {
   absl::StatusOr<ParseResult> parse = Parse(input);
@@ -64,24 +78,16 @@ absl::StatusOr<std::vector<std::string>> Day16_2020::Part1(
   int bad_val_sum = 0;
   for (const std::vector<int64_t>& ticket : parse->other_tickets) {
     for (int v : ticket) {
-      VLOG(1) << "Checking: " << v;
+      VLOG(2) << "Checking: " << v;
       bool valid_any = false;
       for (const auto& [name, or_range] : parse->rules) {
-        if (v >= or_range.min1 && v <= or_range.max1) {
-          VLOG(2) << "Valid: [" << or_range.min1 << "," <<  or_range.max1 << "],["
-                  <<or_range.min2 << "," <<  or_range.max2 << "]";
-          valid_any = true;
-          break;
-        }
-        if (v >= or_range.min2 && v <= or_range.max2) {
-          VLOG(2) << "Valid: [" << or_range.min1 << "," <<  or_range.max1 << "],["
-                  <<or_range.min2 << "," <<  or_range.max2 << "]";
+        if (CheckRange(v, or_range)) {
           valid_any = true;
           break;
         }
       }
       if (!valid_any) {
-        VLOG(1) << "Invalid";
+        VLOG(2) << "Invalid";
         bad_val_sum += v;
       }
     }
@@ -89,7 +95,125 @@ absl::StatusOr<std::vector<std::string>> Day16_2020::Part1(
   return IntReturn(bad_val_sum);
 }
 
+bool IsValid(const ParseResult& parse, absl::string_view field_name, int pos) {
+  auto it = parse.rules.find(field_name);
+  CHECK(it != parse.rules.end());
+  OrRange or_range = it->second;
+  VLOG(3) << "IsValid: " << field_name << ": " << pos;
+  for (const std::vector<int64_t>& ticket : parse.other_tickets) {
+    if (!CheckRange(ticket[pos], or_range)) return false;
+  }
+  VLOG(3) << "  = true";
+  return true;
+}
+
+bool FindRemainingFieldOrder(const ParseResult& parse, 
+ const absl::flat_hash_map<absl::string_view, absl::flat_hash_set<int>>& is_valid,
+ std::vector<absl::string_view>* ret,
+                             const absl::flat_hash_set<absl::string_view>& remaining) {
+  VLOG(3) << "FindRemaningFieldOrder: " << absl::StrJoin(*ret, ",");
+  absl::flat_hash_set<absl::string_view> sub_remaining = remaining;
+  for (absl::string_view field : remaining) {
+    if (auto it = is_valid.find(field); !it->second.contains(ret->size())) {
+      continue;
+    }
+    ret->push_back(field);
+    sub_remaining.erase(field);
+    if (sub_remaining.empty()) return true;
+    if (FindRemainingFieldOrder(parse, is_valid, ret, sub_remaining)) return true;
+    sub_remaining.insert(field);
+    ret->pop_back();
+  }
+  return false;
+}
+
+void BurnIsValid(absl::flat_hash_map<absl::string_view, absl::flat_hash_set<int>>* is_valid) {
+  std::vector<absl::string_view> singletons;
+  for (const auto& [field, map] : *is_valid) {
+    if (map.size() == 1) singletons.push_back(field); 
+  }
+  while (!singletons.empty()) {
+    std::vector<absl::string_view> new_singletons;
+    for (absl::string_view singleton_field : singletons) {
+      int pos = *is_valid->find(singleton_field)->second.begin();
+      VLOG(1) << "removing: pos=" << pos << "; singleton=" << singleton_field;
+      for (auto& [field, map] : *is_valid) {
+        if (singleton_field == field) continue;
+        if (!map.contains(pos)) continue;
+        map.erase(pos);
+        if (map.size() == 1) new_singletons.push_back(field); 
+      }
+    }
+    singletons = std::move(new_singletons);
+  }
+}
+
+std::vector<absl::string_view> FindFieldOrder(const ParseResult& parse) {
+  std::vector<absl::string_view> ret;
+  absl::flat_hash_set<absl::string_view> remaining;
+  for (const auto& [name, _] : parse.rules) {
+    remaining.insert(name);
+  }
+  absl::flat_hash_map<absl::string_view, absl::flat_hash_set<int>> is_valid_map;
+  for (absl::string_view field : remaining) {
+    for (int pos = 0; pos < remaining.size(); ++pos) {
+      bool is_valid = IsValid(parse, field, pos);
+      VLOG(2) << "IsValid(" << field << "," << pos << ") = " << is_valid;
+      if (is_valid) {
+        is_valid_map[field].insert(pos);
+      }
+    }
+    VLOG(1) << "IsValid(" << field << ") = " << is_valid_map[field].size();
+  }
+  BurnIsValid(&is_valid_map);
+  CHECK(FindRemainingFieldOrder(parse, is_valid_map, &ret, remaining));
+  return ret;
+}
+
 absl::StatusOr<std::vector<std::string>> Day16_2020::Part2(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  absl::StatusOr<ParseResult> parse = Parse(input);
+  if (!parse.ok()) return parse.status();
+
+  std::vector<std::vector<int64_t>> valid_tickets;
+  for (const std::vector<int64_t>& ticket : parse->other_tickets) {
+    bool valid_ticket = true;
+    for (int v : ticket) {
+      VLOG(2) << "Checking: " << v;
+      bool valid_any_value = false;
+      for (const auto& [name, or_range] : parse->rules) {
+        if (CheckRange(v, or_range)) {
+          valid_any_value = true;
+          break;
+        }
+      }
+      if (!valid_any_value) {
+        valid_ticket = false;
+        break;
+      }
+    }
+    if (valid_ticket) {
+      valid_tickets.push_back(ticket);
+    }
+  }
+  VLOG(1) << valid_tickets.size() << " valid tickets";
+  VLOG(2) << absl::StrJoin(valid_tickets, "\n", 
+             [](std::string* out, const std::vector<int64_t>& t) { absl::StrAppend(out, absl::StrJoin(t, ",")); });
+  parse->other_tickets = valid_tickets;
+  std::vector<absl::string_view> field_order = FindFieldOrder(*parse);
+  VLOG(1) << "found size == " << field_order.size() << " of " << parse->rules.size();
+  VLOG(1) << "Found order: " << absl::StrJoin(field_order, ",");
+  absl::string_view prefix = "departure";
+  int prefix_count = 0;
+  int64_t product = 1;
+  for (int i = 0; i < field_order.size(); ++i) {
+    if (field_order[i].substr(0, prefix.size()) == prefix) {
+      ++prefix_count;
+      VLOG(1) << " field_order: " << field_order[i] << ": " << i;
+      VLOG(1) << " *= " << parse->my_ticket[i];
+      product *= parse->my_ticket[i];
+    }
+  }
+  if (prefix_count != 6) return Error("Wrong number of prefix=", prefix);
+  return IntReturn(product);
 }
