d2cf72ed8e7288c9b4d145c17252b7e6978f43dd
Keith Peters <keith@monkeynova.com>
2020-12-18 19:08:09 -0800

    Empty 2020/25

---
+++ advent_of_code/2015/day25/day25.h
@@ -0,0 +1,18 @@
+#ifndef ADVENT_OF_CODE_2015_DAY25_H
+#define ADVENT_OF_CODE_2015_DAY25_H
+
+#include "advent_of_code/advent_day.h"
+
+namespace advent_of_code {
+
+class Day25_2015 : public AdventDay {
+ public:
+  absl::StatusOr<std::vector<std::string>> Part1(
+      absl::Span<absl::string_view> input) const override;
+  absl::StatusOr<std::vector<std::string>> Part2(
+      absl::Span<absl::string_view> input) const override;
+};
+
+}  // namespace advent_of_code
+
+#endif  // ADVENT_OF_CODE_2015_DAY25_H
