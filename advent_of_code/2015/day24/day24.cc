a2195d15b530175b2ab3b841e84a1e91ed294bc2
Keith Peters <keith@monkeynova.com>
2020-12-18 18:28:46 -0800

    2015/24 Part2

--- advent_of_code/2015/day24/day24.cc
+++ advent_of_code/2015/day24/day24.cc
@@ -87,7 +87,36 @@ bool CanPartitionRemainder(const WeightsSet& weights_set, const Partition& p, in
   return false;
 }
 
-absl::StatusOr<int64_t> FindMinPartition(const WeightsSet& weights_set) {
+bool CanPartitionRemainder2(const WeightsSet& weights_set, const Partition& p, int target_weight) {
+  std::vector<int> remaining_weights;
+  remaining_weights.reserve(weights_set.weights.size() - p.weights.size());
+  for (int w : weights_set.weights) {
+    if (!p.weights.contains(w)) remaining_weights.push_back(w);
+  }
+  VLOG(1) << "Remaining = {" << absl::StrJoin(remaining_weights, ",") << "}";
+  for (int i = 0; i < (1 << remaining_weights.size()); ++i) {
+    Partition sub_p = p;
+    int test_weight = 0;
+    std::vector<int> part_1_weights;
+    std::vector<int> part_2_weights;
+    for (int b = 0; (1 << b) <= i; ++b) {
+      if (i & (1 << b)) {
+        if (VLOG_IS_ON(1)) part_1_weights.push_back(remaining_weights[b]);
+        test_weight += remaining_weights[b];
+        sub_p.weights.insert(remaining_weights[b]);
+      } else {
+        if (VLOG_IS_ON(1)) part_2_weights.push_back(remaining_weights[b]);
+      }
+    }
+    if (test_weight == target_weight) {
+      VLOG(1) << "Found Partial Partitioning: {" << absl::StrJoin(part_1_weights, ",") << "}";
+      return CanPartitionRemainder(weights_set, sub_p, target_weight);
+    }
+  }
+  return false;
+}
+
+absl::StatusOr<int64_t> FindMinPartition3(const WeightsSet& weights_set) {
   int total_weight = 0;
   for (int w : weights_set.weights) total_weight += w; 
   if (total_weight % 3 != 0) {
@@ -117,6 +146,37 @@ absl::StatusOr<int64_t> FindMinPartition(const WeightsSet& weights_set) {
   return -1;
 }
 
+absl::StatusOr<int64_t> FindMinPartition4(const WeightsSet& weights_set) {
+  int total_weight = 0;
+  for (int w : weights_set.weights) total_weight += w; 
+  if (total_weight % 4 != 0) {
+    return AdventDay::Error("Total weight isn't partitionable: ", total_weight);
+  }
+  int partition_weight = total_weight / 4;
+  VLOG(1) << "Target partition weight = " << partition_weight;
+
+  for (int first_partition_size = 1; first_partition_size < weights_set.weights.size() / 4; ++first_partition_size) {
+    VLOG(1) << "Finding partitions of size " << first_partition_size;
+    std::vector<Partition> p_list = FindPartitions(weights_set, first_partition_size, partition_weight);
+    // Order by entanglement.
+    std::sort(p_list.begin(), p_list.end());
+    VLOG(1) << " Found " << p_list.size() << " of size " << first_partition_size;
+    for (const Partition& p : p_list) {
+      if (p.weights.size() != first_partition_size) {
+        return AdventDay::Error("Bad partition size (expected=", first_partition_size, "): ", 
+                                absl::StrJoin(p.weights, ","));
+      }
+      VLOG(1) << "Testing partition: " << absl::StrJoin(p.weights, ",");
+      if (CanPartitionRemainder2(weights_set, p, partition_weight)) {
+        return p.entanglement;
+      }
+    }
+  }
+
+  return -1;
+}
+
+
 }  // namespace
 
 absl::StatusOr<std::vector<std::string>> Day24_2015::Part1(
@@ -129,12 +189,20 @@ absl::StatusOr<std::vector<std::string>> Day24_2015::Part1(
     weights_set.max_weight = std::max<int>(weights_set.max_weight, v);
   }
   
-  return IntReturn(FindMinPartition(weights_set));
+  return IntReturn(FindMinPartition3(weights_set));
 }
 
 absl::StatusOr<std::vector<std::string>> Day24_2015::Part2(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  absl::StatusOr<std::vector<int64_t>> weights = ParseAsInts(input);
+  WeightsSet weights_set;
+  for (int64_t v : *weights) {
+    if (weights_set.weights.contains(v)) return Error("Can't handle dupes");
+    weights_set.weights.insert(v);
+    weights_set.max_weight = std::max<int>(weights_set.max_weight, v);
+  }
+  
+  return IntReturn(FindMinPartition4(weights_set));
 }
 
 }  // namespace advent_of_code
