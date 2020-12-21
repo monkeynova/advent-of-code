daa23ebfbb3832039790d9e23f6119f4a7fdf698
Keith Peters <keith@monkeynova.com>
2020-12-21 08:20:51 -0800

    2016/07 Part1

--- advent_of_code/2016/day07/day07.cc
+++ advent_of_code/2016/day07/day07.cc
@@ -13,13 +13,38 @@ namespace advent_of_code {
 
 namespace {
 
+bool SupportsTLS(absl::string_view str) {
+  int level = 0;
+  bool match_outside = false;
+  bool match_inside = false;
+  for (int i = 0; i < str.size(); ++i) {
+    if (str[i] == '[') ++level;
+    if (str[i] == ']') --level;
+    if (i >= 3) {
+      if (str[i] == str[i-3] &&
+          str[i-1] == str[i-2] &&
+          str[i] != str[i-1]) {
+        if (level == 0) match_outside = true;
+        else match_inside = true;
+      }
+    }
+  }
+  return match_outside && !match_inside;
+}
+
 // Helper methods go here.
 
 }  // namespace
 
 absl::StatusOr<std::vector<std::string>> Day07_2016::Part1(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  int count = 0;
+  for (absl::string_view str : input) {
+    bool supports = SupportsTLS(str);
+    VLOG(1) << str << ": " << supports;
+    if (supports) ++count;
+  }
+  return IntReturn(count);
 }
 
 absl::StatusOr<std::vector<std::string>> Day07_2016::Part2(
