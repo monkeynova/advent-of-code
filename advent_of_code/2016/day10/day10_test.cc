4b5bff68412b57b6e93e5f9eebdb5e86164333e7
Keith Peters <keith@monkeynova.com>
2020-12-21 09:59:07 -0800

    Empty 2020/10

---
+++ advent_of_code/2016/day10/day10_test.cc
@@ -0,0 +1,15 @@
+#include "advent_of_code/2016/day10/day10.h"
+
+#include "advent_of_code/file_test.h"
+#include "gmock/gmock.h"
+
+namespace advent_of_code {
+
+namespace advent_of_code {
+
+using DayTest = FileTest<Day10_2016>;
+
+TEST_F(DayTest, FileBasedTest) { EXPECT_TRUE(RunTest()); }
+
+}  // namespace advent_of_code
+}  // namespace advent_of_code
