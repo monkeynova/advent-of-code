d506cc32f17b74d977078432e417042baecdfc9c
Keith Peters <keith@monkeynova.com>
2020-12-22 14:11:51 -0800

    2016/16 Part1

--- advent_of_code/2016/day16/day16.cc
+++ advent_of_code/2016/day16/day16.cc
@@ -13,13 +13,63 @@ namespace advent_of_code {
 
 namespace {
 
-// Helper methods go here.
+class DragonCurve {
+ public:
+  static std::string Expand(absl::string_view tmp) {
+    std::string copy = std::string(tmp);
+    std::reverse(copy.begin(), copy.end());
+    for (int i = 0; i < copy.size(); ++i) {
+      copy[i] = copy[i] == '0' ? '1' : '0';
+    }
+    std::string ret = absl::StrCat(tmp, "0", copy);
+    VLOG(2) << ret;
+    return ret;
+  }
+
+  static std::string Checksum(absl::string_view tmp) {
+    std::string ret;
+    ret.resize((tmp.size() + 1) / 2);
+    for (int i = 0; i < tmp.size(); i +=2 ) {
+      if (tmp[i] == tmp[i+1]) {
+        ret[i/2] = '1';
+      } else {
+        ret[i/2] = '0';
+      }
+    }
+    VLOG(2) << ret;
+    return ret;
+  }
+
+  explicit DragonCurve(absl::string_view str) : str_(str) {}
+
+
+ private:
+  absl::string_view str_;
+};
+
+std::string RunPart1(absl::string_view input, int size) {
+  std::string s = std::string(input);
+  VLOG(1) << "Start: " << s;
+  while (s.size() < size) {
+    s = DragonCurve::Expand(s);
+  }
+  VLOG(1) << "Expanded: " << s;
+  s = s.substr(0, size);
+  VLOG(1) << "Truncated: " << s;
+  while (s.size() % 2 == 0) {
+    s = DragonCurve::Checksum(s);
+  }
+  VLOG(1) << "Checksum: " << s;
+  return s;
+}
 
 }  // namespace
 
 absl::StatusOr<std::vector<std::string>> Day16_2016::Part1(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  if (RunPart1("10000", 20) != "01100") return Error("Bad code");
+  if (input.size() != 1) return Error("Bad input size");
+  return std::vector<std::string>{RunPart1(input[0], 272)};
 }
 
 absl::StatusOr<std::vector<std::string>> Day16_2016::Part2(
