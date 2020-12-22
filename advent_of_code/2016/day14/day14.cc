56eefb9d543b469ad8980bcf5f954fbb45c4b768
Keith Peters <keith@monkeynova.com>
2020-12-22 11:10:37 -0800

    2016/14 Part2

--- advent_of_code/2016/day14/day14.cc
+++ advent_of_code/2016/day14/day14.cc
@@ -89,7 +89,66 @@ absl::StatusOr<std::vector<std::string>> Day14_2016::Part1(
 
 absl::StatusOr<std::vector<std::string>> Day14_2016::Part2(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  if (input.size() != 1) return Error("Bad input size");
+
+  MD5_CTX ctx;
+  unsigned char md5_result[16];
+  absl::string_view md5_result_view(reinterpret_cast<char*>(md5_result), 16);
+  std::vector<std::string> window;
+  int found = 0;
+  for (int index = 0; true; ++index) {
+    std::string str = absl::StrCat(input[0], index);
+    for (int i = 0; i < 2017; ++i) {
+      MD5_Init(&ctx);
+      MD5_Update(&ctx, str.data(), str.size());
+      MD5_Final(md5_result, &ctx);
+      str = Hex(md5_result_view);
+    }
+    
+    std::string hex = Hex(md5_result_view);
+    window.push_back(hex);
+    if (window.size() < 1001) continue;
+
+    if (window.size() != index + 1) return Error("Bad append");
+
+    int test_index = index - 1000;
+    absl::flat_hash_set<char> hunt_set;
+    {
+      absl::string_view test = window[test_index];
+      for (int i = 0; i + 2 < test.size(); ++i) {
+        if (test[i] == test[i+1] &&
+            test[i] == test[i+2]) {
+          VLOG(2) << "Found 3-ple of " << test.substr(i, 1) << " in " << test << " @" << test_index << "," << i;
+          hunt_set.insert(test[i]);
+          break;
+        }
+      }
+    }  
+    for (char hunt : hunt_set) {
+      bool found_5ple = false;
+      for (int j = 1; j <= 1000; ++j) {
+        absl::string_view test = window[test_index + j];
+        for (int i = 0; i + 4 < test.size(); ++i) {
+          if (test[i] == hunt &&
+              test[i] == test[i+1] &&
+              test[i] == test[i+2] &&
+              test[i] == test[i+3] &&
+              test[i] == test[i+4]) {
+            VLOG(2) << "Found 5-ple of " << test.substr(i, 1) << " in " << test << " @" << test_index + j << "," << i;
+            found_5ple = true;
+          }
+        }
+      }
+      if (found_5ple) {
+        ++found;
+        VLOG(1) << "Found Key #" << found << " @" << test_index;
+        if (found == 64) return IntReturn(test_index);
+        break;
+      }
+    }
+  }
+
+  return Error("Impossible");
 }
 
 }  // namespace advent_of_code
