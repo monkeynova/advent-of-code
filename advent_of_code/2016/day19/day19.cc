ecab6564f6f12b8c7af129c74b6ba4bbf959af5a
Keith Peters <keith@monkeynova.com>
2020-12-22 20:07:05 -0800

    2016/19 Part2

--- advent_of_code/2016/day19/day19.cc
+++ advent_of_code/2016/day19/day19.cc
@@ -26,7 +26,7 @@ absl::StatusOr<std::vector<std::string>> Day19_2016::Part1(
   for (int i = 0; i < cycle.size(); ++i) cycle[i] = i + 1;
   int offset = 0;
   while (cycle.size() > 1) {
-    VLOG_IF(1, cycle.size() < 100) << absl::StrJoin(cycle, ",");
+    VLOG_IF(2, cycle.size() < 100) << absl::StrJoin(cycle, ",");
     std::vector<int> next_cycle;
     next_cycle.reserve(cycle.size() / 2 + 1);
     for (int i = offset; i < cycle.size(); i += 2) {
@@ -41,7 +41,32 @@ absl::StatusOr<std::vector<std::string>> Day19_2016::Part1(
 
 absl::StatusOr<std::vector<std::string>> Day19_2016::Part2(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  if (input.size() != 1) return Error("Bad input");
+  int size;
+  if (!absl::SimpleAtoi(input[0], &size)) return Error("Bad int");
+  std::vector<int> cycle(size, 0);
+  for (int i = 0; i < cycle.size(); ++i) cycle[i] = i + 1;
+  int offset = cycle.size() / 2;
+  int pos = cycle.size() % 2;
+  while (cycle.size() > 1) {
+    VLOG_IF(2, cycle.size() < 100) << absl::StrJoin(cycle, ",");
+    std::vector<int> next_cycle;
+    for (int i = 0; i < offset; ++i) {
+      next_cycle.push_back(cycle[i]);
+    }
+    for (int i = offset; i < cycle.size(); ++i) {
+      if (pos == 2) next_cycle.push_back(cycle[i]);
+      pos = (pos + 1) % 3;
+    }
+    offset = 0;
+    if (next_cycle.empty()) {
+      cycle = {cycle.back()};
+    } else {
+      cycle = next_cycle;
+    }
+  }
+  if (cycle.size() != 1) return Error("WTF?");
+  return IntReturn(cycle[0]);
 }
 
 }  // namespace advent_of_code
