3322b31364cfebf1d7a455ef4fb2c78d746ddaf2
Keith Peters <keith@monkeynova.com>
2020-12-23 09:47:41 -0800

    2016/20 Part1

--- advent_of_code/2016/day20/day20.cc
+++ advent_of_code/2016/day20/day20.cc
@@ -13,13 +13,39 @@ namespace advent_of_code {
 
 namespace {
 
-// Helper methods go here.
+struct Block {
+  int64_t start;
+  int64_t end;
+  bool operator<(const Block& o) const {
+    if (start != o.start) return start < o.start;
+    return end < o.end;
+  }
+};
 
 }  // namespace
 
 absl::StatusOr<std::vector<std::string>> Day20_2016::Part1(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  std::vector<Block> blocks;
+  for (absl::string_view s : input) {
+    Block b;
+    if (!RE2::FullMatch(s, "(\\d+)-(\\d+)", &b.start, &b.end)) {
+      return Error("Bad range: ", s);
+    }
+    blocks.push_back(b);
+  }
+  std::sort(blocks.begin(), blocks.end());
+  if (blocks[0].start != 0) return IntReturn(0);
+  int invalid_through = blocks[0].end;
+  for (Block b : blocks) {
+    if (b.start <= invalid_through + 1) {
+      if (b.end > invalid_through) invalid_through = b.end;
+    } else {
+      return IntReturn(invalid_through + 1);
+    }
+  }
+
+  return Error("Not found");
 }
 
 absl::StatusOr<std::vector<std::string>> Day20_2016::Part2(
