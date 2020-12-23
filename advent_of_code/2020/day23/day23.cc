021a7c0ccf5e97d6e036605bd8b572819991164f
Keith Peters <keith@monkeynova.com>
2020-12-23 07:51:33 -0800

    2020/23 Code Cleanup and mark long

--- advent_of_code/2020/day23/day23.cc
+++ advent_of_code/2020/day23/day23.cc
@@ -14,62 +14,6 @@ namespace advent_of_code {
 namespace {
 
 struct Cups {
-  int pos = 0;
-  absl::flat_hash_map<int, int> cup_to_pos;
-  absl::flat_hash_map<int, int> pos_to_cup;
-};
-
-std::ostream& operator<<(std::ostream& o, const Cups& c) {
-  for (int i = 0; c.pos_to_cup.contains(i); ++i) {
-    if (i > 0) o << ",";
-    if (i == c.pos) o << "(";
-    o << c.pos_to_cup.find(i)->second;
-    if (i == c.pos) o << ")";
-  }
-  return o;
-}
-
-absl::StatusOr<Cups> RunMove(Cups& cups) {
-  int size = cups.pos_to_cup.size();
-  int cur_cup = cups.pos_to_cup[cups.pos];
-  int dest_cup = cur_cup - 1;
-  if (dest_cup < 1) dest_cup = size;
-  while ((size + cups.cup_to_pos[dest_cup] - (cups.pos + 1)) % size < 3) {
-    dest_cup = (dest_cup - 1);
-    if (dest_cup < 1) dest_cup = size;
-  }
-  VLOG(2) << "  dest_cup=" << dest_cup;
-  Cups ret;
-  ret.pos = -1;
-  int out_pos = 0;
-  for (int in_pos = 0; in_pos < size; ++in_pos) {
-    if ((size + in_pos - (cups.pos + 1)) % size < 3) continue;
-    ret.pos_to_cup[out_pos] = cups.pos_to_cup[in_pos];
-    ret.cup_to_pos[cups.pos_to_cup[in_pos]] = out_pos;
-    VLOG(2) << "  Putting " << cups.pos_to_cup[in_pos] << " at " << out_pos;
-    ++out_pos;
-    if (cups.pos_to_cup[in_pos] == dest_cup) {
-      for (int i = 0; i < 3; ++i) {
-        int src_pos = (size + cups.pos + 1 + i) % size;
-        ret.pos_to_cup[out_pos] = cups.pos_to_cup[src_pos];
-        ret.cup_to_pos[cups.pos_to_cup[src_pos]] = out_pos;
-        VLOG(2) << "  Putting " << cups.pos_to_cup[src_pos] << " at " << out_pos;
-        ++out_pos;
-      }
-    }
-    if (in_pos == cups.pos) {
-      // We've advanced out_pos for the next loop already.
-      ret.pos = out_pos % size;
-    }
-  }
-  if (out_pos != size)return AdventDay::Error("Bad out_pos");
-  if (ret.pos_to_cup.size() != size) return AdventDay::Error("Bad pos_to_cup size");
-  if (ret.cup_to_pos.size() != size) return AdventDay::Error("Bad cup_to_pos size");
-  if (ret.pos == -1) return AdventDay::Error("Could not find next position");
-  return ret;
-}
-
-struct Cups2 {
   struct LLCup {
     int val;
     int next;
@@ -80,7 +24,7 @@ struct Cups2 {
   std::vector<LLCup> cups;
 };
 
-std::ostream& operator<<(std::ostream& o, const Cups2& c) {
+std::ostream& operator<<(std::ostream& o, const Cups& c) {
   o << "1";
   for (int idx = c.cups[c.cup_to_index.find(1)->second].next;
        c.cups[idx].val != 1;
@@ -93,7 +37,7 @@ std::ostream& operator<<(std::ostream& o, const Cups2& c) {
   return o;
 }
 
-void RunMove2(Cups2* cups) {
+void RunMove2(Cups* cups) {
   int cur_cup = cups->cups[cups->cur_idx].val;
   int dest_cup = cur_cup - 1;
   if (dest_cup < 1) dest_cup = cups->cups.size();
@@ -128,8 +72,7 @@ void RunMove2(Cups2* cups) {
 absl::StatusOr<std::vector<std::string>> Day23_2020::Part1(
     absl::Span<absl::string_view> input) const {
   if (input.size() != 1) return Error("Bad size");
-#if 1
-  Cups2 cups;
+  Cups cups;
   cups.cur_idx = 0;
   for (int i = 0; i < input[0].size(); ++i) {
     int cup_num = input[0][i] - '0';
@@ -152,34 +95,12 @@ absl::StatusOr<std::vector<std::string>> Day23_2020::Part1(
     ++ret_i;
   }
   return std::vector<std::string>{ret};
-#else
-  Cups cups;
-  for (int i = 0; i < input[0].size(); ++i) {
-    int cup_num = input[0][i] - '0';
-    cups.pos_to_cup[i] = cup_num;
-    cups.cup_to_pos[cup_num] = i;
-  }
-  for (int i = 0; i < 100; ++i) {
-    VLOG(1) << cups;
-    absl::StatusOr<Cups> next = RunMove(cups);
-    if (!next.ok()) return next.status();
-    cups = std::move(*next);
-  }
-  VLOG(1) << cups;
-  std::string ret;
-  int size = cups.pos_to_cup.size();
-  ret.resize(size - 1);
-  for (int i = 0; i < size - 1; ++i) {
-    ret[i] = cups.pos_to_cup[(size + i + 1 + cups.cup_to_pos[1]) % size] + '0';
-  }
-  return std::vector<std::string>{ret};
-#endif
 }
 
 absl::StatusOr<std::vector<std::string>> Day23_2020::Part2(
     absl::Span<absl::string_view> input) const {
   if (input.size() != 1) return Error("Bad size");
-  Cups2 cups;
+  Cups cups;
   cups.cur_idx = 0;
   for (int i = 0; i < input[0].size(); ++i) {
     int cup_num = input[0][i] - '0';
@@ -197,7 +118,7 @@ absl::StatusOr<std::vector<std::string>> Day23_2020::Part2(
     RunMove2(&cups);
   }
   int next_idx = cups.cups[cups.cup_to_index[1]].next;
-  int product = 1;
+  int64_t product = 1;
   product *= cups.cups[next_idx].val;
   next_idx = cups.cups[next_idx].next;
   product *= cups.cups[next_idx].val;
