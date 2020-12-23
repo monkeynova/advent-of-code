1a7a3d9308adb43f004b2250f37e9cd4174600b8
Keith Peters <keith@monkeynova.com>
2020-12-22 17:39:20 -0800

    2016/18 Part1

--- advent_of_code/2016/day18/day18.cc
+++ advent_of_code/2016/day18/day18.cc
@@ -6,6 +6,7 @@
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
 #include "absl/strings/str_split.h"
+#include "advent_of_code/char_board.h"
 #include "glog/logging.h"
 #include "re2/re2.h"
 
@@ -19,7 +20,30 @@ namespace {
 
 absl::StatusOr<std::vector<std::string>> Day18_2016::Part1(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  if (input.size() != 1) return Error("Bad input size");
+  CharBoard b(input[0].size(), 40);
+  absl::flat_hash_set<int> next_traps = {6, 3, 4, 1};
+  for (Point p : b.range()) {
+    if (p.y == 0) {
+      b[p] = input[0][p.x];
+    } else {
+      int prev_traps = 0;
+      for (Point dir : {Cardinal::kNorthWest, Cardinal::kNorth, Cardinal::kNorthEast}) {
+        prev_traps <<= 1;
+        Point prev = p + dir;
+        if (b.OnBoard(prev) && b[prev] == '^') {
+          prev_traps |= 1;
+        }
+      }
+      b[p] = next_traps.contains(prev_traps) ? '^' : '.';
+    }
+  }
+  LOG(INFO) << "Board:\n" << b.DebugString();
+  int safe_count = 0;
+  for (Point p : b.range())  {
+    if (b[p] == '.') ++safe_count;
+  }
+  return IntReturn(safe_count);
 }
 
 absl::StatusOr<std::vector<std::string>> Day18_2016::Part2(
