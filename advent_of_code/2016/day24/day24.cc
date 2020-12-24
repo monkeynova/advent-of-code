55111718b2f8faad4a892d260e36cccd1050e265
Keith Peters <keith@monkeynova.com>
2020-12-24 10:50:10 -0800

    Migrate 2016/24 to a usable BFS Interface

--- advent_of_code/2016/day24/day24.cc
+++ advent_of_code/2016/day24/day24.cc
@@ -7,6 +7,7 @@
 #include "absl/strings/str_join.h"
 #include "absl/strings/str_split.h"
 #include "advent_of_code/char_board.h"
+#include "advent_of_code/bfs.h"
 #include "glog/logging.h"
 #include "re2/re2.h"
 
@@ -14,103 +15,76 @@ namespace advent_of_code {
 
 namespace {
 
-struct PathState {
-  Point cur;
-  int have_keys;
-  int num_steps;
-  bool operator==(const PathState& o) const {
-    return cur == o.cur && have_keys == o.have_keys;
+class PathWalk : public BFSInterface<PathWalk> {
+ public:
+  PathWalk(const CharBoard& board, bool back_to_origin)
+   : board_(board),
+     start_(FindStart(board)),
+     cur_(start_),
+     need_keys_(NeedKeys(board)),
+     have_keys_(0),
+     back_to_origin_(back_to_origin) {}
+
+  void AddNextSteps(State* state) final {
+    for (Point dir : Cardinal::kFourDirs) {
+      Point next_point = cur_ + dir;
+      if (board_.OnBoard(next_point) && board_[next_point] != '#') {
+        PathWalk next = *this;
+        next.cur_ = next_point;
+        if (board_[next_point] > '0' && board_[next_point] <= '9') {
+          next.have_keys_ |= 1 << (board_[next_point] - '1');
+        }
+        state->AddNextStep(std::move(next));
+      }
+    }
   }
+
+  bool IsFinal() final {
+    if (need_keys_ != have_keys_) return false;
+    if (back_to_origin_ && cur_ != start_) return false;
+    return true;
+  }
+
+  bool operator==(const PathWalk& o) const {
+    return cur_ == o.cur_ && have_keys_ == o.have_keys_;
+  }
+
+  template <typename H>
+  friend H AbslHashValue(H h, const PathWalk& pw) {
+    return H::combine(std::move(h), pw.cur_, pw.have_keys_);
+  }
+
+  friend std::ostream& operator<<(std::ostream& o, const PathWalk& pw) {
+    return o << "{" << pw.cur_ << "," << std::hex << pw.have_keys_ << std::dec
+             << "}";
+  }
+
+ private:
+  Point FindStart(const CharBoard& b) {
+    Point start;
+    for (Point p : b.range()) {
+      if (b[p] == '0') start = p;
+    }
+    return start;
+  }
+  
+  int NeedKeys(const CharBoard& b) {
+    int need_keys = 0;
+    for (Point p : b.range()) {
+      if (b[p] > '0' && b[p] <= '9') {
+        need_keys |= 1 << (b[p] - '1');
+      }
+    }
+    return need_keys;
+  }
+
+  const CharBoard& board_;
+  Point start_;
+  Point cur_;
+  int need_keys_;
+  int have_keys_;
+  bool back_to_origin_;
 };
-
-template <typename H>
-H AbslHashValue(H h, const PathState& s) {
-  return H::combine(std::move(h), s.cur, s.have_keys);
-}
-
-absl::StatusOr<int> MinPathAllNums(const CharBoard& b) {
-  Point start;
-  int need_keys = 0;
-  for (Point p : b.range()) {
-    if (b[p] == '0')
-      start = p;
-    else if (b[p] > '0' && b[p] <= '9') {
-      need_keys |= 1 << (b[p] - '1');
-    }
-  }
-  std::deque<PathState> frontier = {
-      PathState{.cur = start, .have_keys = 0, .num_steps = 0}};
-  absl::flat_hash_set<PathState> hist;
-  hist.insert(frontier.front());
-  while (!frontier.empty()) {
-    const PathState& cur_state = frontier.front();
-    for (Point dir : Cardinal::kFourDirs) {
-      Point next_point = cur_state.cur + dir;
-      if (b.OnBoard(next_point) && b[next_point] != '#') {
-        PathState next_state = cur_state;
-        ++next_state.num_steps;
-        next_state.cur = next_point;
-        if (b[next_point] > '0' && b[next_point] <= '9') {
-          next_state.have_keys |= 1 << (b[next_point] - '1');
-          if (next_state.have_keys == need_keys) {
-            return next_state.num_steps;
-          }
-        }
-        if (!hist.contains(next_state)) {
-          hist.insert(next_state);
-          frontier.push_back(next_state);
-        }
-      }
-    }
-
-    frontier.pop_front();
-  }
-
-  return AdventDay::Error("No path found");
-}
-
-absl::StatusOr<int> MinPathAllNumsReturn(const CharBoard& b) {
-  Point start;
-  int need_keys = 0;
-  for (Point p : b.range()) {
-    if (b[p] == '0')
-      start = p;
-    else if (b[p] > '0' && b[p] <= '9') {
-      need_keys |= 1 << (b[p] - '1');
-    }
-  }
-  std::deque<PathState> frontier = {
-      PathState{.cur = start, .have_keys = 0, .num_steps = 0}};
-  absl::flat_hash_set<PathState> hist;
-  hist.insert(frontier.front());
-  while (!frontier.empty()) {
-    const PathState& cur_state = frontier.front();
-    for (Point dir : Cardinal::kFourDirs) {
-      Point next_point = cur_state.cur + dir;
-      if (b.OnBoard(next_point) && b[next_point] != '#') {
-        PathState next_state = cur_state;
-        ++next_state.num_steps;
-        next_state.cur = next_point;
-        if (b[next_point] > '0' && b[next_point] <= '9') {
-          next_state.have_keys |= 1 << (b[next_point] - '1');
-        }
-        if (next_state.have_keys == need_keys && next_state.cur == start) {
-          return next_state.num_steps;
-        }
-        if (!hist.contains(next_state)) {
-          hist.insert(next_state);
-          frontier.push_back(next_state);
-        }
-      }
-    }
-
-    frontier.pop_front();
-  }
-
-  return AdventDay::Error("No path found");
-}
-
-// Helper methods go here.
 
 }  // namespace
 
@@ -119,7 +93,7 @@ absl::StatusOr<std::vector<std::string>> Day24_2016::Part1(
   absl::StatusOr<CharBoard> b = CharBoard::Parse(input);
   if (!b.ok()) return b.status();
 
-  return IntReturn(MinPathAllNums(*b));
+  return IntReturn(PathWalk(*b, false).FindMinSteps());
 }
 
 absl::StatusOr<std::vector<std::string>> Day24_2016::Part2(
@@ -127,7 +101,7 @@ absl::StatusOr<std::vector<std::string>> Day24_2016::Part2(
   absl::StatusOr<CharBoard> b = CharBoard::Parse(input);
   if (!b.ok()) return b.status();
 
-  return IntReturn(MinPathAllNumsReturn(*b));
+  return IntReturn(PathWalk(*b, true).FindMinSteps());
 }
 
 }  // namespace advent_of_code
---
+++ advent_of_code/bfs.h
@@ -0,0 +1,67 @@
+#ifndef ADVENT_OF_CODE_BFS_H
+#define ADVENT_OF_CODE_BFS_H
+
+#include <deque>
+#include <memory>
+#include <vector>
+
+namespace advent_of_code {
+
+template <typename BFSImpl>
+class BFSInterface {
+ public:
+  class State {
+   public:
+    explicit State(BFSImpl start) {
+     hist_.insert(start);
+     frontier_.push_back(std::move(start));
+    }
+    void AddNextStep(BFSImpl next) {
+      ++next.num_steps_;
+      if (next.IsFinal()) ret = next.num_steps_;
+      if (hist_.contains(next)) return;
+      hist_.insert(next);
+      frontier_.push_back(std::move(next));
+    }
+
+   private:
+    std::deque<BFSImpl> frontier_;
+    absl::flat_hash_set<BFSImpl> hist_;
+    absl::optional<int> ret;
+
+    friend class BFSInterface;
+  };
+
+  virtual ~BFSInterface() = default;
+  virtual void AddNextSteps(State* state) = 0;
+  virtual bool IsFinal() = 0;
+
+  absl::optional<int> FindMinSteps();
+
+ protected:
+  BFSInterface() = default;
+
+ private:
+  int num_steps_ = 0;
+
+  friend class State;
+};
+
+template <typename BFSImpl>
+absl::optional<int> BFSInterface<BFSImpl>::FindMinSteps() {
+  static_assert(std::is_base_of<BFSInterface<BFSImpl>, BFSImpl>(), 
+                "BFSInterface must be templated with a subclass");
+  State state(*dynamic_cast<BFSImpl*>(this));
+  while (!state.frontier_.empty()) {
+    BFSImpl& cur = state.frontier_.front();
+    VLOG(3) << "@" << cur.num_steps_ << "; " << cur;
+    cur.AddNextSteps(&state);
+    if (state.ret) return *state.ret;
+    state.frontier_.pop_front();
+  }
+  return absl::nullopt;
+}
+
+}  // namespace advent_of_code
+
+#endif  // ADVENT_OF_CODE_BFS_H
