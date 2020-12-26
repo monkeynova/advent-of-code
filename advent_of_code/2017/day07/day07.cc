6b7a9fcabf80c5ca39aafd51a2b168e8e8060fd3
Keith Peters <keith@monkeynova.com>
2020-12-26 15:53:22 -0800

    2017/07 Part1

--- advent_of_code/2017/day07/day07.cc
+++ advent_of_code/2017/day07/day07.cc
@@ -19,7 +19,29 @@ namespace {
 
 absl::StatusOr<std::vector<std::string>> Day07_2017::Part1(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  absl::flat_hash_set<absl::string_view> parents;
+  absl::flat_hash_set<absl::string_view> children;
+  for (absl::string_view str : input) {
+    std::vector<absl::string_view> p_and_c_list = absl::StrSplit(str, " -> ");
+    if (p_and_c_list.size() > 2) return Error("Bad input: ", str);
+    if (p_and_c_list.size() == 1) continue;
+    for (absl::string_view child : absl::StrSplit(p_and_c_list[1], ", ")) {
+      children.insert(child);
+    }
+    absl::string_view parent;
+    if (!RE2::FullMatch(p_and_c_list[0], "(.*) \\(\\d+\\)", &parent)) {
+      return Error("Bad parent: ", p_and_c_list[0]);
+    }
+    parents.insert(parent);
+  }
+  absl::string_view root;
+  for (absl::string_view p : parents) {
+    if (!children.contains(p)) {
+      if (root != "") return Error("Dupe roots");
+      root = p;
+    }
+  }
+  return std::vector<std::string>{std::string(root)};
 }
 
 absl::StatusOr<std::vector<std::string>> Day07_2017::Part2(
