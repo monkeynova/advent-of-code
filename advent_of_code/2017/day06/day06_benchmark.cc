a5469ecf675976e9c24bd33411ab51a596b3a31c
Keith Peters <keith@monkeynova.com>
2020-12-26 15:27:45 -0800

    Empty 2017/06

---
+++ advent_of_code/2017/day06/day06_benchmark.cc
@@ -0,0 +1,20 @@
+#include "advent_of_code/infra/file_benchmark.h"
+#include "advent_of_code/2017/day06/day06.h"
+#include "gmock/gmock.h"
+
+namespace advent_of_code {
+
+namespace advent_of_code {
+
+constexpr int kMaxTestCount = 2;
+
+TEST(DayBenchmarkTest, Consistency) {
+  absl::StatusOr<int> test_count = FileBenchmarkTestCount();
+  ASSERT_TRUE(test_count.ok()) << test_count.status().message();
+  EXPECT_EQ(kMaxTestCount, *test_count);
+}
+
+BENCHMARK_TEMPLATE(BM_Day, Day06_2017)->DenseRange(0, kMaxTestCount - 1);
+
+}  // namespace advent_of_code
+}  // namespace advent_of_code
