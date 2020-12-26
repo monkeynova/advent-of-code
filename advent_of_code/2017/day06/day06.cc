21e459163a105069cca38887e6d9825525df1907
Keith Peters <keith@monkeynova.com>
2020-12-26 15:40:21 -0800

    2017/06 Part1

--- advent_of_code/2017/day06/day06.cc
+++ advent_of_code/2017/day06/day06.cc
@@ -13,13 +13,44 @@ namespace advent_of_code {
 
 namespace {
 
-// Helper methods go here.
+std::vector<int64_t> RunStep(std::vector<int64_t> in) {
+  int64_t max_idx;
+  int64_t max_value = std::numeric_limits<int64_t>::min();
+  for (int i = 0; i < in.size(); ++i) {
+    if (in[i] > max_value) {
+      max_value = in[i];
+      max_idx = i;
+    }
+  }
+  int64_t q = in[max_idx];
+  in[max_idx] = 0;
+  int64_t per = q / in.size();
+  int64_t rem = q % in.size();
+  for (int i = 0; i < in.size(); ++i) {
+    in[i] += per;
+  }
+  for (int j = 0; j < rem; ++j) {
+    ++in[(max_idx + j + 1) % in.size()];
+  }
+  return in;
+}
 
 }  // namespace
 
 absl::StatusOr<std::vector<std::string>> Day06_2017::Part1(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  if (input.size() != 1) return Error("Bad size");
+  std::vector<absl::string_view> split = absl::StrSplit(input[0], "\t");
+  absl::StatusOr<std::vector<int64_t>> list = ParseAsInts(split);
+  if (!list.ok()) return list.status();
+  std::vector<int64_t> tmp = *list;
+  absl::flat_hash_set<std::vector<int64_t>> hist;
+  while (!hist.contains(tmp)) {
+    VLOG(2) << absl::StrJoin(tmp, ",");
+    hist.insert(tmp);
+    tmp = RunStep(std::move(tmp));
+  }
+  return IntReturn(hist.size());
 }
 
 absl::StatusOr<std::vector<std::string>> Day06_2017::Part2(
