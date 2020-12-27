388733382119f2c76bd0a190ec811158caaa96a0
Keith Peters <keith@monkeynova.com>
2020-12-27 08:58:51 -0800

    2017/11 Part2

--- advent_of_code/2017/day11/day11.cc
+++ advent_of_code/2017/day11/day11.cc
@@ -37,7 +37,21 @@ absl::StatusOr<std::vector<std::string>> Day11_2017::Part1(
 
 absl::StatusOr<std::vector<std::string>> Day11_2017::Part2(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  if (input.size() != 1) return Error("Bad size");
+  Point p = Cardinal::kOrigin;
+  int max_dist = std::numeric_limits<int>::min();
+  std::vector<absl::string_view> dirs = absl::StrSplit(input[0], ",");
+  for (absl::string_view dir : dirs) {
+    if (dir == "nw") p += Cardinal::kNorthWest;
+    else if (dir == "ne") p += Cardinal::kNorthEast;
+    else if (dir == "sw") p += Cardinal::kSouthWest;
+    else if (dir == "se") p += Cardinal::kSouthEast;
+    else if (dir == "n") p += 2 * Cardinal::kNorth;
+    else if (dir == "s") p += 2 * Cardinal::kSouth;
+    else return Error("Bad direction: ", dir);
+    max_dist = std::max(max_dist, p.dist() / 2);
+  }
+  return IntReturn(max_dist);
 }
 
 }  // namespace advent_of_code
