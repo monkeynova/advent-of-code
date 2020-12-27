8b3b55fe213dc6315375155760ae35440c9d48be
Keith Peters <keith@monkeynova.com>
2020-12-26 18:27:43 -0800

    Benchmark task (and update)

--- benchmark/run_all_benchmarks.sh
+++ benchmark/run_all_benchmarks.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
  
-echo > ~/benchmark_out.txt
+TMP_FILE=/tmp/benchmark_out.txt.$$
+
+echo > $TMP_FILE
 for t in `bazel query 'filter(".*day\d+_benchmark",...)'`
 do
-  bazel run -c opt $t -- --benchmark --run_long_tests=10s| grep BM_ >> ~/benchmark_out.txt
+  bazel run -c opt $t -- --benchmark --run_long_tests=20s | grep BM_ >> $TMP_FILE
 done
+benchmark/sort_benchmark.pl < $TMP_FILE > benchmark/benchmark_out.txt
