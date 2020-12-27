9abaf6878c6cec4784502a53fed20de6aa85ef09
Keith Peters <keith@monkeynova.com>
2020-12-27 13:14:12 -0800

    Create a helper DAG interface.
    Move 2017/07 to use it.

--- advent_of_code/2017/day07/day07.cc
+++ advent_of_code/2017/day07/day07.cc
@@ -6,6 +6,7 @@
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
 #include "absl/strings/str_split.h"
+#include "advent_of_code/dag.h"
 #include "glog/logging.h"
 #include "re2/re2.h"
 
@@ -15,33 +16,30 @@ namespace {
 
 absl::optional<int> FindBadWeight(
     absl::string_view root,
-    const absl::flat_hash_map<absl::string_view,
-                              std::vector<absl::string_view>>& tree,
-    const absl::flat_hash_map<absl::string_view, int>& weights,
-    int* this_weight = nullptr) {
-  auto weight_it = weights.find(root);
-  CHECK(weight_it != weights.end());
+    const DAG<int>& dag,
+    int* this_weight_out = nullptr) {
+  const int* weight = dag.GetData(root);
+  CHECK(weight != nullptr);
 
-  auto it = tree.find(root);
-  if (it == tree.end()) {
-    if (this_weight != nullptr) {
-      *this_weight = weight_it->second;
+  const std::vector<absl::string_view>* children = dag.Outgoing(root);
+  if (children == nullptr) {
+    if (this_weight_out != nullptr) {
+      *this_weight_out = *weight;
     }
     return absl::nullopt;
   }
-  const std::vector<absl::string_view>& children = it->second;
   absl::flat_hash_map<int, int> sub_weight_counts;
   absl::flat_hash_map<int, int> sub_weight_example;
-  for (int i = 0; i < children.size(); ++i) {
+  for (int i = 0; i < children->size(); ++i) {
     int this_weight;
     absl::optional<int> bad =
-        FindBadWeight(children[i], tree, weights, &this_weight);
-    VLOG(1) << children[i] << ": " << this_weight;
+        FindBadWeight((*children)[i], dag, &this_weight);
+    VLOG(1) << (*children)[i] << ": " << this_weight;
     if (bad) return bad;
     ++sub_weight_counts[this_weight];
-    auto child_weight_it = weights.find(children[i]);
-    CHECK(child_weight_it != weights.end());
-    sub_weight_example[this_weight] = child_weight_it->second;
+    const int* child_weight = dag.GetData((*children)[i]);
+    CHECK(child_weight != nullptr);
+    sub_weight_example[this_weight] = *child_weight;
   }
   CHECK(sub_weight_counts.size() < 3);
   CHECK(sub_weight_counts.size() > 0);
@@ -59,37 +57,45 @@ absl::optional<int> FindBadWeight(
     }
     return target_weight - miss_target_weight + miss_weight;
   }
-  if (this_weight != nullptr) {
-    *this_weight =
-        weight_it->second + children.size() * sub_weight_counts.begin()->first;
+  if (this_weight_out != nullptr) {
+    *this_weight_out =
+        *weight + children->size() * sub_weight_counts.begin()->first;
   }
   return absl::nullopt;
+}
+
+absl::StatusOr<DAG<int>> Parse(absl::Span<absl::string_view> input) {
+  DAG<int> dag;
+  for (absl::string_view str : input) {
+    std::vector<absl::string_view> p_and_c_list = absl::StrSplit(str, " -> ");
+    if (p_and_c_list.size() > 2) return AdventDay::Error("Bad input: ", str);
+    absl::string_view parent;
+    int weight;
+    if (!RE2::FullMatch(p_and_c_list[0], "(.*) \\((\\d+)\\)", &parent, &weight)) {
+      return AdventDay::Error("Bad parent: ", p_and_c_list[0]);
+    }
+    dag.AddNode(parent, weight);
+
+    if (p_and_c_list.size() == 1) continue;
+    for (absl::string_view child : absl::StrSplit(p_and_c_list[1], ", ")) {
+      dag.AddEdge(parent, child);
+    }
+  }
+  return dag;
 }
 
 }  // namespace
 
 absl::StatusOr<std::vector<std::string>> Day07_2017::Part1(
     absl::Span<absl::string_view> input) const {
-  absl::flat_hash_set<absl::string_view> parents;
-  absl::flat_hash_set<absl::string_view> children;
-  for (absl::string_view str : input) {
-    std::vector<absl::string_view> p_and_c_list = absl::StrSplit(str, " -> ");
-    if (p_and_c_list.size() > 2) return Error("Bad input: ", str);
-    if (p_and_c_list.size() == 1) continue;
-    for (absl::string_view child : absl::StrSplit(p_and_c_list[1], ", ")) {
-      children.insert(child);
-    }
-    absl::string_view parent;
-    if (!RE2::FullMatch(p_and_c_list[0], "(.*) \\(\\d+\\)", &parent)) {
-      return Error("Bad parent: ", p_and_c_list[0]);
-    }
-    parents.insert(parent);
-  }
+  absl::StatusOr<DAG<int>> dag = Parse(input);
+  if (!dag.ok()) return dag.status();
   absl::string_view root;
-  for (absl::string_view p : parents) {
-    if (!children.contains(p)) {
+  for (absl::string_view node : dag->nodes()) {
+    const std::vector<absl::string_view>* incoming = dag->Incoming(node);
+    if (incoming == nullptr) {
       if (root != "") return Error("Dupe roots");
-      root = p;
+      root = node;
     }
   }
   return std::vector<std::string>{std::string(root)};
@@ -97,34 +103,17 @@ absl::StatusOr<std::vector<std::string>> Day07_2017::Part1(
 
 absl::StatusOr<std::vector<std::string>> Day07_2017::Part2(
     absl::Span<absl::string_view> input) const {
-  absl::flat_hash_map<absl::string_view, std::vector<absl::string_view>> tree;
-  absl::flat_hash_map<absl::string_view, int> weights;
-  absl::flat_hash_set<absl::string_view> children;
-  for (absl::string_view str : input) {
-    std::vector<absl::string_view> p_and_c_list = absl::StrSplit(str, " -> ");
-    if (p_and_c_list.size() > 2) return Error("Bad input: ", str);
-    absl::string_view parent;
-    int weight;
-    if (!RE2::FullMatch(p_and_c_list[0], "(.*) \\((\\d+)\\)", &parent,
-                        &weight)) {
-      return Error("Bad parent: ", p_and_c_list[0]);
-    }
-    weights[parent] = weight;
-    if (p_and_c_list.size() == 1) continue;
-    std::vector<absl::string_view>& child_vec = tree[parent];
-    for (absl::string_view child : absl::StrSplit(p_and_c_list[1], ", ")) {
-      child_vec.push_back(child);
-      children.insert(child);
-    }
-  }
+  absl::StatusOr<DAG<int>> dag = Parse(input);
+  if (!dag.ok()) return dag.status();
   absl::string_view root;
-  for (const auto& [p, _] : tree) {
-    if (!children.contains(p)) {
+  for (absl::string_view node : dag->nodes()) {
+    const std::vector<absl::string_view>* incoming = dag->Incoming(node);
+    if (incoming == nullptr) {
       if (root != "") return Error("Dupe roots");
-      root = p;
+      root = node;
     }
   }
-  return IntReturn(FindBadWeight(root, tree, weights));
+  return IntReturn(FindBadWeight(root, *dag));
 }
 
 }  // namespace advent_of_code
---
+++ advent_of_code/dag.h
@@ -0,0 +1,58 @@
+#ifndef ADVENT_OF_CODE_DAG_H
+#define ADVENT_OF_CODE_DAG_H
+
+#include <vector>
+
+#include "absl/container/flat_hash_map.h"
+
+namespace advent_of_code {
+
+template <typename Storage>
+class DAG {
+ public:
+  DAG() = default;
+
+  void AddNode(absl::string_view node_name, Storage s) {
+    storage_[node_name] = std::move(s);
+    nodes_.insert(node_name);
+  }
+
+  void AddEdge(absl::string_view src, absl::string_view dst) {
+    node_to_outgoing_[src].push_back(dst);
+    node_to_incoming_[dst].push_back(src);
+    nodes_.insert(src);
+    nodes_.insert(dst);
+  }
+
+  const absl::flat_hash_set<absl::string_view>& nodes() const { return nodes_; }
+
+  const std::vector<absl::string_view>* Outgoing(absl::string_view node) const {
+    auto it = node_to_outgoing_.find(node);
+    if (it == node_to_outgoing_.end()) return nullptr;
+    return &it->second;
+  }
+
+  const std::vector<absl::string_view>* Incoming(absl::string_view node) const {
+    auto it = node_to_incoming_.find(node);
+    if (it == node_to_incoming_.end()) return nullptr;
+    return &it->second;
+  }
+
+  const Storage* GetData(absl::string_view node) const {
+    auto it = storage_.find(node);
+    if (it == storage_.end()) return nullptr;
+    return &it->second;
+  }
+
+ private:  
+  absl::flat_hash_set<absl::string_view> nodes_;
+  absl::flat_hash_map<absl::string_view, std::vector<absl::string_view>>
+    node_to_outgoing_;
+  absl::flat_hash_map<absl::string_view, std::vector<absl::string_view>>
+    node_to_incoming_;
+  absl::flat_hash_map<absl::string_view, Storage> storage_;
+};
+
+}  // namespace advent_of_code
+
+#endif  // ADVENT_OF_CODE_DAG_H
