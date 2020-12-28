2df98ea1d4c0aca3ade8f7da18e42e5d745eb68d
Keith Peters <keith@monkeynova.com>
2020-12-28 09:23:17 -0800

    2017/13 Part1

--- advent_of_code/2017/day13/day13.cc
+++ advent_of_code/2017/day13/day13.cc
@@ -19,7 +19,28 @@ namespace {
 
 absl::StatusOr<std::vector<std::string>> Day13_2017::Part1(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  int severity = 0;
+  for (absl::string_view row : input) {
+    int depth;
+    int range;
+    if (!RE2::FullMatch(row, "(\\d+): (\\d+)", &depth, &range)) {
+      return Error("Bad row: ", row);
+    }
+    int pos = 0;
+    int to_move = depth;
+    while (to_move > 0) {
+      int delta = std::min(to_move, range - 1);
+      if (pos == 0) pos += delta;
+      else if (pos == range - 1) pos -= delta;
+      else return Error("Consistency error");
+      to_move -= delta;
+    }
+    VLOG(2) << "depth=" << depth << "; range=" << range << "; pos=" << pos;
+    if (pos == 0) {
+      severity += depth * range;
+    }
+  }
+  return IntReturn(severity);
 }
 
 absl::StatusOr<std::vector<std::string>> Day13_2017::Part2(
