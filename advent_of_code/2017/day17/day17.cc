0f05ebed5ea362a8d066d26e833d2558ebb5b65b
Keith Peters <keith@monkeynova.com>
2020-12-28 16:53:01 -0800

    2017/17 Part1

--- advent_of_code/2017/day17/day17.cc
+++ advent_of_code/2017/day17/day17.cc
@@ -19,7 +19,22 @@ namespace {
 
 absl::StatusOr<std::vector<std::string>> Day17_2017::Part1(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  if (input.size() != 1) return Error("Bad size");
+  int n;
+  if (!absl::SimpleAtoi(input[0], &n)) return Error("Bad int:", input[0]);
+  int pos = 0;
+  std::vector<int> buf = {0};
+  for (int i = 0; i < 2017; ++i) {
+    VLOG(1) << absl::StrJoin(buf, ",");
+    pos = (pos + n) % buf.size();
+    buf.push_back(0);
+    ++pos;
+    for (int j = buf.size() - 1; j > pos; --j) {
+      buf[j] = buf[j - 1];
+    }
+    buf[pos] = i + 1;
+  }
+  return IntReturn(buf[(pos + 1) % buf.size()]);
 }
 
 absl::StatusOr<std::vector<std::string>> Day17_2017::Part2(
