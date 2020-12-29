92932e5dc9e0becaf82f1501f17d5020e2130860
Keith Peters <keith@monkeynova.com>
2020-12-29 11:22:14 -0800

    Empty 2017/24

---
+++ advent_of_code/2017/day24/day24.cc
@@ -0,0 +1,30 @@
+#include "advent_of_code/2017/day24/day24.h"
+
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/strings/numbers.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_join.h"
+#include "absl/strings/str_split.h"
+#include "glog/logging.h"
+#include "re2/re2.h"
+
+namespace advent_of_code {
+
+namespace {
+
+// Helper methods go here.
+
+}  // namespace
+
+absl::StatusOr<std::vector<std::string>> Day24_2017::Part1(
+    absl::Span<absl::string_view> input) const {
+  return Error("Not implemented");
+}
+
+absl::StatusOr<std::vector<std::string>> Day24_2017::Part2(
+    absl::Span<absl::string_view> input) const {
+  return Error("Not implemented");
+}
+
+}  // namespace advent_of_code
