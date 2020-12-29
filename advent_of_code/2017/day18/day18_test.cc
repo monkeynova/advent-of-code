0855f3f739e0dd346b12783208429e93030cce89
Keith Peters <keith@monkeynova.com>
2020-12-28 16:59:16 -0800

    Empty 2017/18

---
+++ advent_of_code/2017/day18/day18_test.cc
@@ -0,0 +1,15 @@
+#include "advent_of_code/2017/day18/day18.h"
+
+#include "advent_of_code/infra/file_test.h"
+#include "gmock/gmock.h"
+
+namespace advent_of_code {
+
+namespace advent_of_code {
+
+using DayTest = FileTest<Day18_2017>;
+
+TEST_F(DayTest, FileBasedTest) { EXPECT_TRUE(RunTest()); }
+
+}  // namespace advent_of_code
+}  // namespace advent_of_code
