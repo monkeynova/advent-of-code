8eecb73a93366891e6b0a97ef484450d946853d0
Keith Peters <keith@monkeynova.com>
2020-12-31 11:13:24 -0800

    cleanup.sh

--- advent_of_code/2018/day07/day07_benchmark.cc
+++ advent_of_code/2018/day07/day07_benchmark.cc
@@ -1,5 +1,5 @@
-#include "advent_of_code/infra/file_benchmark.h"
 #include "advent_of_code/2018/day07/day07.h"
+#include "advent_of_code/infra/file_benchmark.h"
 #include "gmock/gmock.h"
 
 namespace advent_of_code {
