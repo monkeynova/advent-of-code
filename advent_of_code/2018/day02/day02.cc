7a7aedd8fd9c341cbe451ec05c674658c3cd1688
Keith Peters <keith@monkeynova.com>
2020-12-31 09:18:23 -0800

    cleanup.sh

--- advent_of_code/2018/day02/day02.cc
+++ advent_of_code/2018/day02/day02.cc
@@ -45,7 +45,8 @@ absl::StatusOr<std::vector<std::string>> Day02_2018::Part2(
       for (int c = 0; c < input[i].size(); ++c) {
         if (input[i][c] == input[j][c]) tmp.append(input[i].substr(c, 1));
       }
-      if (tmp.size() == input[i].size() - 1) return std::vector<std::string>{tmp};
+      if (tmp.size() == input[i].size() - 1)
+        return std::vector<std::string>{tmp};
     }
   }
   return Error("Not found");
