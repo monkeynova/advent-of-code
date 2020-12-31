8a34262071f81c8e2e450309d192e74078bb33c4
Keith Peters <keith@monkeynova.com>
2020-12-31 09:26:36 -0800

    2018/03 Part1

--- advent_of_code/2018/day03/day03.cc
+++ advent_of_code/2018/day03/day03.cc
@@ -6,6 +6,7 @@
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
 #include "absl/strings/str_split.h"
+#include "advent_of_code/char_board.h"
 #include "glog/logging.h"
 #include "re2/re2.h"
 
@@ -19,7 +20,32 @@ namespace {
 
 absl::StatusOr<std::vector<std::string>> Day03_2018::Part1(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  // #1372 @ 119,13: 29x16
+  CharBoard board(1'000, 1'000);
+  for (Point p : board.range()) board[p] = '.';
+  int dupes = 0;
+  for (absl::string_view row : input) {
+    PointRectangle r;
+    if (!RE2::FullMatch(row, "#\\d+ @ (\\d+),(\\d+): (\\d+)x(\\d+)",
+                        &r.min.x, &r.min.y, &r.max.x, &r.max.y)) {
+      return Error("Bad input: ", row);
+    }
+    r.max.x += r.min.x - 1;
+    r.max.y += r.min.y - 1;
+    if (!board.OnBoard(r.min)) return Error("Min off board");
+    if (!board.OnBoard(r.max)) return Error("Max off board");
+    for (Point p : r) {
+      if (board[p] == '+') {
+        ++dupes;
+        board[p] = '#';
+      } else if (board[p] == '#') {
+        // Already counted.
+      } else {
+        board[p] = '+';
+      }
+    }
+  }
+  return IntReturn(dupes);
 }
 
 absl::StatusOr<std::vector<std::string>> Day03_2018::Part2(
