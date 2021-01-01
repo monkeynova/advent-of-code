e44c3929e5e5dd3c8d33297a6f780564fbd4ef72
Keith Peters <keith@monkeynova.com>
2021-01-01 09:57:16 -0800

    Function extract advancer

--- advent_of_code/2018/day12/day12.cc
+++ advent_of_code/2018/day12/day12.cc
@@ -13,7 +13,38 @@ namespace advent_of_code {
 
 namespace {
 
-// Helper methods go here.
+std::string Advance(
+  const absl::flat_hash_map<absl::string_view, absl::string_view>& rules,
+  absl::string_view cur_view, int64_t* offset) {
+  constexpr absl::string_view kEmpty = ".....";
+  std::string next;
+  int cur_size = cur_view.size();  // Not an unsigned int!
+  for (int j = -4; j < cur_size; ++j) {
+    int start_need = -std::min(j, 0);
+    int end_need = std::max(j + 5 - cur_size, 0);
+    std::string test = absl::StrCat(
+      kEmpty.substr(0, start_need),
+      cur_view.substr(std::max(j, 0), 5 - start_need - end_need),
+      kEmpty.substr(0, end_need));
+    CHECK(test.size() == 5);
+    auto it = rules.find(test);
+    if (it == rules.end()) {
+      //return Error("No rule for: ", test);
+      next.append(std::string(kEmpty.substr(0, 1)));
+    } else {
+      next.append(std::string(it->second));
+    }
+    VLOG(2) << j << ": " << test << " => " << next.back();
+  }
+  *offset -= 2;
+  absl::string_view next_view = next;
+  while (next_view[0] == '.') {
+    ++*offset;
+    next_view = next_view.substr(1);
+  }
+  while (next_view.back() == '.') next_view = next_view.substr(0, next_view.size() - 1);
+  return std::string(next_view);
+}
 
 }  // namespace
 
@@ -36,37 +67,10 @@ absl::StatusOr<std::vector<std::string>> Day12_2018::Part1(
   }
   if (initial_state.empty()) return Error("No initial state");
   std::string cur = std::string(initial_state);
-  absl::string_view empty = ".....";
-  int offset = 0;
+  int64_t offset = 0;
   for (int i = 0; i < 20; ++i) {
     VLOG(1) << "@[" << offset << "]: " << cur;
-    absl::string_view cur_view = cur;
-    std::string next;
-    int cur_size = cur.size();  // Not an unsigned int!
-    for (int j = -4; j < cur_size; ++j) {
-      int start_need = -std::min(j, 0);
-      int end_need = std::max(j + 5 - cur_size, 0);
-      std::string test = absl::StrCat(
-        empty.substr(0, start_need),
-        cur_view.substr(std::max(j, 0), 5 - start_need - end_need),
-        empty.substr(0, end_need));
-      if (test.size() != 5) return Error("Bad read: ", j);
-      if (!rules.contains(test)) {
-        //return Error("No rule for: ", test);
-        next.append(std::string(empty.substr(0, 1)));
-      } else {
-        next.append(std::string(rules[test]));
-      }
-      VLOG(2) << j << ": " << test << " => " << next.back();
-    }
-    offset -= 2;
-    absl::string_view next_view = next;
-    while (next_view[0] == '.') {
-      ++offset;
-      next_view = next_view.substr(1);
-    }
-    while (next_view.back() == '.') next_view = next_view.substr(0, next_view.size() - 1);
-    cur = std::string(next_view);
+    cur = Advance(rules, cur, &offset);
   }
   VLOG(1) << "@[" << offset << "]: " << cur;
   int count = 0;
@@ -98,46 +102,20 @@ absl::StatusOr<std::vector<std::string>> Day12_2018::Part2(
   }
   if (initial_state.empty()) return Error("No initial state");
   std::string cur = std::string(initial_state);
-  absl::string_view empty = ".....";
   int64_t offset = 0;
   constexpr int64_t kGenerations = 50'000'000'000;
   for (int64_t i = 0; i < kGenerations; ++i) {
     VLOG(1) << "@[" << offset << "]: " << cur;
-    absl::string_view cur_view = cur;
-    std::string next;
-    int cur_size = cur.size();  // Not an unsigned int!
-    for (int j = -4; j < cur_size; ++j) {
-      int start_need = -std::min(j, 0);
-      int end_need = std::max(j + 5 - cur_size, 0);
-      std::string test = absl::StrCat(
-        empty.substr(0, start_need),
-        cur_view.substr(std::max(j, 0), 5 - start_need - end_need),
-        empty.substr(0, end_need));
-      if (test.size() != 5) return Error("Bad read: ", j);
-      if (!rules.contains(test)) {
-        //return Error("No rule for: ", test);
-        next.append(std::string(empty.substr(0, 1)));
-      } else {
-        next.append(std::string(rules[test]));
-      }
-      VLOG(2) << j << ": " << test << " => " << next.back();
-    }
     int64_t last_offset = offset;
-    offset -= 2;
-    absl::string_view next_view = next;
-    while (next_view[0] == '.') {
-      ++offset;
-      next_view = next_view.substr(1);
-    }
-    while (next_view.back() == '.') next_view = next_view.substr(0, next_view.size() - 1);
-    if (cur == next_view) {
+    std::string next = Advance(rules, cur, &offset);
+    if (cur == next) {
       LOG(INFO) << "Found translation @" << i;
       int64_t offset_delta = offset - last_offset;
       offset += offset_delta * (kGenerations - i - 1);
       LOG(INFO) << "Upping offset by " << offset_delta * (kGenerations - i - 1);
       break;
     }
-    cur = std::string(next_view);
+    cur = std::string(next);
   }
   VLOG(1) << "@[" << offset << "]: " << cur;
   int64_t count = 0;
