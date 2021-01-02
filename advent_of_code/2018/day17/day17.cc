9373b5fbae8dfd22bcbf63a6e85677378d03c6b4
Keith Peters <keith@monkeynova.com>
2021-01-02 11:12:37 -0800

    Empty 2018/17

---
+++ advent_of_code/2018/day17/day17.cc
@@ -0,0 +1,30 @@
+#include "advent_of_code/2018/day17/day17.h"
+
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/strings/numbers.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_join.h"
+#include "absl/strings/str_split.h"
+#include "glog/logging.h"
+#include "re2/re2.h"
+
+namespace advent_of_code {
+
+namespace {
+
+// Helper methods go here.
+
+}  // namespace
+
+absl::StatusOr<std::vector<std::string>> Day17_2018::Part1(
+    absl::Span<absl::string_view> input) const {
+  return Error("Not implemented");
+}
+
+absl::StatusOr<std::vector<std::string>> Day17_2018::Part2(
+    absl::Span<absl::string_view> input) const {
+  return Error("Not implemented");
+}
+
+}  // namespace advent_of_code
---
+++ advent_of_code/2018/day17/day17_test.cc
@@ -0,0 +1,15 @@
+#include "advent_of_code/2018/day17/day17.h"
+
+#include "advent_of_code/infra/file_test.h"
+#include "gmock/gmock.h"
+
+namespace advent_of_code {
+
+namespace advent_of_code {
+
+using DayTest = FileTest<Day17_2018>;
+
+TEST_F(DayTest, FileBasedTest) { EXPECT_TRUE(RunTest()); }
+
+}  // namespace advent_of_code
+}  // namespace advent_of_code
