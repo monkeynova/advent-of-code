4af7e2136cd803d9656d166232424e3a1282bb69
Keith Peters <keith@monkeynova.com>
2021-01-02 12:53:47 -0800

    2018/17 Part1

--- advent_of_code/2018/day17/day17.cc
+++ advent_of_code/2018/day17/day17.cc
@@ -6,6 +6,7 @@
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
 #include "absl/strings/str_split.h"
+#include "advent_of_code/char_board.h"
 #include "glog/logging.h"
 #include "re2/re2.h"
 
@@ -13,13 +14,167 @@ namespace advent_of_code {
 
 namespace {
 
+absl::Status DropFrom(CharBoard& b, Point start) {
+  VLOG(1) << "Drop From: " << start;
+  Point cur = start;
+  if (b[cur] != '.') return AdventDay::Error("Drop from non-soil");
+  while (b[cur] == '.') {
+    b[cur] = '|';
+    cur += Point{0, 1};
+    if (!b.OnBoard(cur)) {
+      // Ran off the board. Done.
+      return absl::OkStatus();
+    }
+  }
+  switch (b[cur]) {
+    case '|': {
+      // Hit previous drop fill.
+      return absl::OkStatus();
+    }
+    case '~':
+    case '#': {
+      // Need to fill, fall through to work.
+      break;
+    }
+    default: return AdventDay::Error("Drop to unhandled");
+  }
+  cur -= Point{0, 1};
+  VLOG(1) << "Drop to: " << cur;
+  bool filling = true;
+  while (filling) {
+    VLOG(3) << b.DebugString();
+    PointRectangle fill = {cur, cur};
+    while (b[fill.min - Point{1, 0}] != '#') {
+      if (b[fill.min + Point{0, 1}] == '.') {
+        if (absl::Status st = DropFrom(b, fill.min + Point{0, 1}); !st.ok()) {
+          return st;
+        }
+        if (b[fill.min + Point{0, 1}] != '~' &&
+            b[fill.min + Point{0, 1}] != '#') {
+          filling = false;
+          break;
+        }
+      }
+      if (b[fill.min - Point{1, 0}] == '|') {
+        // Hit an existing drop.
+        filling = false;
+        break;
+      }
+      fill.min -= Point{1, 0};
+      if (!b.OnBoard(fill.min)) return AdventDay::Error("Off edge");
+    }
+    while (b[fill.max + Point{1, 0}] != '#') {
+      if (b[fill.max + Point{0, 1}] == '.') {
+        if (absl::Status st = DropFrom(b, fill.max + Point{0, 1}); !st.ok()) {
+          return st;
+        }
+        if (b[fill.max + Point{0, 1}] != '~' &&
+            b[fill.max + Point{0, 1}] != '#') {
+          filling = false;
+          break;
+        }
+      }
+      if (b[fill.max + Point{1, 0}] == '|') {
+        // Hit an existing drop.
+        filling = false;
+        break;
+      }
+      fill.max += Point{1, 0};
+      if (!b.OnBoard(fill.max)) return AdventDay::Error("Off edge");
+    }
+    if (filling) {
+      VLOG(1) << "Fill range: " << fill.min << "-" << fill.max;
+      for (Point p : fill) b[p] = '~';
+    } else {
+      VLOG(1) << "Drop range: " << fill.min << "-" << fill.max;
+      for (Point p : fill) b[p] = '|';
+    }
+    if (filling) {
+      cur -= Point{0, 1};
+      if (cur.y < start.y) {
+        filling = false; 
+      }
+    }
+  }
+  return absl::OkStatus();
+}
+
+absl::Status FillWithWater(CharBoard& b) {
+  Point start;
+  for (int x = 0; x < b.width(); ++x) {
+    start = Point{x, 0};
+    if (b[start] == '+') break;
+  }
+  if (!b.OnBoard(start)) return AdventDay::Error("Can't find spring");
+  return DropFrom(b, start + Point{0, 1});
+}
+
 // Helper methods go here.
 
 }  // namespace
 
 absl::StatusOr<std::vector<std::string>> Day17_2018::Part1(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  std::vector<PointRectangle> strips;
+  PointRectangle grid = {{500, 0}, {500, 0}};
+  int min_y = std::numeric_limits<int>::max();
+  for (absl::string_view row : input) {
+    int fixed;
+    int r1;
+    int r2;
+    PointRectangle next;
+    if (RE2::FullMatch(row, "x=(\\d+), y=(\\d+)\\.\\.(\\d+)", &fixed, &r1, &r2)) {
+      next = {{fixed, r1}, {fixed, r2}};
+      min_y = std::min(min_y, r1);
+      min_y = std::min(min_y, r2);
+    } else if (RE2::FullMatch(row, "y=(\\d+), x=(\\d+)\\.\\.(\\d+)", &fixed, &r1, &r2)) {
+      next = {{r1, fixed}, {r2, fixed}};
+      min_y = std::min(min_y, fixed);
+    } else {
+      return Error("Bad input: ", row);
+    }
+    grid.ExpandInclude(next.min);
+    grid.ExpandInclude(next.max);
+    strips.push_back(next);
+  }
+  --grid.min.x;
+  ++grid.max.x;
+  CharBoard b(grid.max.x - grid.min.x + 1,
+              grid.max.y - grid.min.y + 1);
+  for (Point p : b.range()) b[p] = '.';
+  b[Point{500, 0} - grid.min] = '+';
+  for (PointRectangle r : strips) {
+    for (Point p : r) {
+      b[p - grid.min] = '#';
+    }
+  }
+  VLOG_IF(1, b.height() < 100) << "Init:\n" << b.DebugString();
+  if (VLOG_IS_ON(2) && b.height() >= 100) {
+    VLOG(2) << "Init:";
+    for (absl::string_view row : b.rows) {
+      VLOG(2) << row;
+    }
+  }
+  if (absl::Status st = FillWithWater(b); !st.ok()) {
+    VLOG(2) << "Final:";
+    for (absl::string_view row : b.rows) {
+      VLOG(2) << row;
+    }
+    return st;
+  }
+  VLOG_IF(1, b.height() < 100) << "Final:\n" << b.DebugString();
+  if (VLOG_IS_ON(2) && b.height() >= 100) {
+    VLOG(2) << "Final:";
+    for (absl::string_view row : b.rows) {
+      VLOG(2) << row;
+    }
+  }
+  int count = 0;
+  for (Point p : b.range()) {
+    if (p.y < min_y) continue;
+    if (b[p] == '~' || b[p] == '|') ++count;
+  }
+  return IntReturn(count);
 }
 
 absl::StatusOr<std::vector<std::string>> Day17_2018::Part2(
