c7f7ac77d67153e332084e1d2558b732336aab87
Keith Peters <keith@monkeynova.com>
2021-01-03 13:21:04 -0800

    2018/20 Part1

--- advent_of_code/2018/day20/day20.cc
+++ advent_of_code/2018/day20/day20.cc
@@ -6,6 +6,8 @@
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
 #include "absl/strings/str_split.h"
+#include "advent_of_code/bfs.h"
+#include "advent_of_code/char_board.h"
 #include "glog/logging.h"
 #include "re2/re2.h"
 
@@ -13,13 +15,243 @@ namespace advent_of_code {
 
 namespace {
 
-// Helper methods go here.
+struct ParsedRE {
+  enum Type {
+    kLiteral = 1,
+    kBranch = 2,
+    kSequence = 3,
+  } type;
+  absl::string_view val;
+  std::vector<std::unique_ptr<ParsedRE>> children;
+
+  std::string DebugString(std::string prefix = "") {
+    if (type == kLiteral) return prefix.append(std::string(val));
+    std::string sub_prefix = prefix;
+    sub_prefix.append(2, ' ');
+    return absl::StrCat(
+      prefix, type == kBranch ? "<branch>\n" : "<sequence\n",
+      absl::StrJoin(children, "\n",
+                    [sub_prefix](std::string* out, const std::unique_ptr<ParsedRE>& p) {
+                      absl::StrAppend(out, p->DebugString(sub_prefix));
+                    }));
+  }
+};
+
+std::vector<absl::string_view> Tokenize(absl::string_view re) {
+  std::vector<absl::string_view> ret;
+  int tok_start = 0;
+  for (int i = 0; i < re.size(); ++i) {
+    if (re[i] == '(' || re[i] == ')' || re[i] == '|') {
+      ret.push_back(re.substr(tok_start, i - tok_start));
+      ret.push_back(re.substr(i, 1));
+      tok_start = i + 1;
+    }
+  }
+  ret.push_back(re.substr(tok_start, re.size() - tok_start));
+  return ret;
+}
+
+absl::StatusOr<ParsedRE> Parse(absl::string_view re) {
+  std::vector<absl::string_view> tokens = Tokenize(re);
+  ParsedRE ret;
+  ret.type = ParsedRE::kSequence;
+  std::vector<ParsedRE*> stack = {&ret};
+  for (absl::string_view tok : tokens) {
+    if (stack.empty()) return AdventDay::Error("Empty stack");
+    if (tok == "(") {
+      auto next = absl::make_unique<ParsedRE>();
+      next->type = ParsedRE::kBranch;
+      stack.back()->children.push_back(std::move(next));
+      stack.push_back(stack.back()->children.back().get());
+      next = absl::make_unique<ParsedRE>();
+      next->type = ParsedRE::kSequence;
+      stack.back()->children.push_back(std::move(next));
+      stack.push_back(stack.back()->children.back().get());
+    } else if (tok == ")") {
+      if (stack.back()->type != ParsedRE::kSequence) {
+        return AdventDay::Error("Bad type at ')'");
+      }
+      stack.pop_back();
+      if (stack.empty()) return AdventDay::Error("Empty stack");
+      if (stack.back()->type != ParsedRE::kBranch) {
+        return AdventDay::Error("Bad type at ')' (2)");
+      }
+      stack.pop_back();
+    } else if (tok == "|") {
+      if (stack.back()->type != ParsedRE::kSequence) {
+        return AdventDay::Error("Bad type at ')'");
+      }
+      stack.pop_back();
+      if (stack.empty()) return AdventDay::Error("Empty stack");
+      auto next = absl::make_unique<ParsedRE>();
+      next->type = ParsedRE::kSequence;
+      stack.back()->children.push_back(std::move(next));
+      stack.push_back(stack.back()->children.back().get());
+    } else {
+      auto next = absl::make_unique<ParsedRE>();
+      next->type = ParsedRE::kLiteral;
+      next->val = tok;
+      stack.back()->children.push_back(std::move(next));
+    }
+  }
+  if (stack.size() != 1) return AdventDay::Error("Bad parse");
+  if (stack.back() != &ret) return AdventDay::Error("Bad parse 2");
+  return ret;
+}
+
+absl::StatusOr<absl::flat_hash_set<Point>> WalkAllPaths(
+    const ParsedRE& re, Point cur, absl::flat_hash_set<Point>* sparse_board) {
+  absl::flat_hash_set<Point> ret;
+  switch (re.type) {
+    case ParsedRE::kLiteral: {
+      if (re.children.size() != 0) {
+        return AdventDay::Error("Literal with non-empty children");
+      }
+      for (char c : re.val) {
+        Point dir;
+        switch (c) {
+          case 'N': dir = Cardinal::kNorth; break;
+          case 'S': dir = Cardinal::kSouth; break;
+          case 'W': dir = Cardinal::kWest; break;
+          case 'E': dir = Cardinal::kEast; break;
+          default: return AdventDay::Error("Bad direction in: ", re.val);
+        }
+        // Advance twice to include door.
+        cur += dir;
+        sparse_board->insert(cur);
+        cur += dir;
+        sparse_board->insert(cur);
+      }
+      ret = {cur};
+      break;
+    }
+    case ParsedRE::kSequence: {
+      if (!re.val.empty()) {
+        return AdventDay::Error("Sequence with non-empty value");
+      }
+      ret = {cur};
+      for (const auto& child : re.children) {
+        absl::flat_hash_set<Point> next;
+        for (Point p : ret) {
+          absl::StatusOr<absl::flat_hash_set<Point>> this_next = WalkAllPaths(*child, p, sparse_board);
+          if (!this_next.ok()) return this_next.status();
+          for (Point p : *this_next) next.insert(p);
+        }
+        ret = std::move(next);
+      }
+      break;
+    }
+    case ParsedRE::kBranch: {
+      if (!re.val.empty()) {
+        return AdventDay::Error("Branch with non-empty value");
+      }
+      ret = {};
+      for (const auto& child : re.children) {
+        absl::StatusOr<absl::flat_hash_set<Point>> this_next = WalkAllPaths(*child, cur, sparse_board);
+        if (!this_next.ok()) return this_next.status();
+        for (Point p : *this_next) ret.insert(p);
+      }
+      break;
+    }
+    default: return AdventDay::Error("Bad type: ", re.type);
+  }
+  return ret;
+}
+
+absl::StatusOr<CharBoard> ConstructRoom(absl::string_view re, Point* start_ret) {
+  Point start = {0, 0};
+
+  absl::StatusOr<ParsedRE> parsed_re = Parse(re);
+  if (!parsed_re.ok()) return parsed_re.status();
+
+  VLOG(1) << parsed_re->DebugString();
+
+  absl::flat_hash_set<Point> sparse_board = {start};
+  LOG(INFO) << "Walking paths...";
+  absl::StatusOr<absl::flat_hash_set<Point>> final_points =
+    WalkAllPaths(*parsed_re, start, &sparse_board);
+  LOG(INFO) << "  ...Done";
+  if (!final_points.ok()) return final_points.status();
+
+  PointRectangle grid = {start, start};
+  for (Point p : sparse_board) {
+    VLOG(2) << "Point @" << p;
+    grid.ExpandInclude(p);
+  }
+
+  CharBoard board(grid.max.x - grid.min.x + 1 + 2,
+                  grid.max.y - grid.min.y + 1 + 2);
+  for (Point p : board.range()) {
+    Point test_point = p + grid.min + Point{-1, -1};
+    if (sparse_board.contains(test_point)) {
+      if (test_point.x % 2 == 0 && test_point.y % 2 == 0) {
+        // Room.
+        board[p] = '.';
+      } else if (test_point.x % 2 && test_point.y % 2 == 0) {
+        board[p] = '|';
+      } else if (test_point.x % 2 == 0 && test_point.y % 2) {
+        board[p] = '-';
+      } else {
+        return AdventDay::Error("Sparse board contains corner: ", test_point.DebugString());
+      }
+    } else {
+      board[p] = '#';
+    }
+  }
+  *start_ret = -grid.min + Point{1, 1};
+  return board;
+}
+
+
+class RoomWalk : public BFSInterface<RoomWalk, Point> {
+ public:
+  RoomWalk(const CharBoard& b, Point start, int* max_dist)
+   : board_(b), max_dist_(max_dist), cur_(start) {}
+
+  Point identifier() const override { return cur_; }
+  bool IsFinal() override { return false; }
+
+  void AddNextSteps(State* state) override {
+    *max_dist_ = std::max(num_steps(), *max_dist_);
+
+    for (Point dir : Cardinal::kFourDirs) {
+      Point door_p = cur_ + dir;
+      if (board_.OnBoard(door_p) &&
+          (board_[door_p] == '|' || board_[door_p] == '-')) {
+        RoomWalk next = *this;
+        next.cur_ = cur_ + 2 * dir;
+        state->AddNextStep(next);
+      }
+    }
+  }
+
+ private:
+  const CharBoard& board_;
+  int* max_dist_;
+  Point cur_;
+};
 
 }  // namespace
 
 absl::StatusOr<std::vector<std::string>> Day20_2018::Part1(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  if (input.size() != 1) return Error("Bad size");
+  if (input[0][0] != '^') return Error("No front anchor");
+  input[0] = input[0].substr(1);
+  if (input[0][input[0].size() - 1] != '$') return Error("No back anchor");
+  input[0] = input[0].substr(0, input[0].size() - 1);
+
+  Point start;
+  absl::StatusOr<CharBoard> room = ConstructRoom(input[0], &start);
+  if (!room.ok()) return room.status();
+
+  VLOG(1) << "Start @" << start << " in Room:\n" << *room;
+
+  int max_path = -1;
+  absl::optional<int> null_dist = RoomWalk(*room, start, &max_path).FindMinSteps();
+  if (null_dist) return Error("Path walk terminated?!?");
+
+  return IntReturn(max_path);
 }
 
 absl::StatusOr<std::vector<std::string>> Day20_2018::Part2(
