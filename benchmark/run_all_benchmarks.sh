3e20034d54adf0da789ebd7a8e5a9d48bfa82d88
Keith Peters <keith@monkeynova.com>
2021-01-04 15:22:48 -0800

    Update benchmarks after error cleanup
    I've also increased the "long" option to be 30s and
    it appears 1m might be sufficient to run everything

--- benchmark/run_all_benchmarks.sh
+++ benchmark/run_all_benchmarks.sh
@@ -5,6 +5,6 @@ TMP_FILE=/tmp/benchmark_out.txt.$$
 echo > $TMP_FILE
 for t in `bazel query 'filter(".*day\d+_benchmark",...)'`
 do
-  bazel run -c opt $t -- --benchmark --run_long_tests=20s | grep BM_ >> $TMP_FILE
+  bazel run -c opt $t -- --benchmark --run_long_tests=30s | grep BM_ >> $TMP_FILE
 done
 benchmark/sort_benchmark.pl < $TMP_FILE > benchmark/benchmark_out.txt
