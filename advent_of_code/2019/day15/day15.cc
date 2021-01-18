acba8f31569670b8800b283adb414772fe351621
Keith Peters <keith@monkeynova.com>
2021-01-18 14:00:00 -0800

    Move 2019/15 to use BFSInterface
    Rather than ad-hoc BFS implemenations
    use the library standard.

--- advent_of_code/2019/day15/day15.cc
+++ advent_of_code/2019/day15/day15.cc
@@ -7,6 +7,7 @@
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_split.h"
 #include "advent_of_code/2019/int_code.h"
+#include "advent_of_code/bfs.h"
 #include "advent_of_code/point.h"
 #include "glog/logging.h"
 
@@ -43,129 +44,130 @@ class Droid : public IntCode::IOModule {
     InputDirection dir = current_path_.front();
     current_path_.pop_front();
     return dir;
-    // return static_cast<InputDirection>(absl::Uniform(bitgen_, 1, 5));
   }
 
   absl::Status ComputePathToNearestUnknown() {
-    struct Path {
-      Point location;
-      std::vector<InputDirection> directions;
-    };
-    absl::flat_hash_set<Point> computed_path = {pos_};
-    std::deque<Path> frontier;
-    frontier.push_back({.location = pos_});
-    for (int depth = 1; !frontier.empty(); ++depth) {
-      const Path& to_extend = frontier.front();
-      for (InputDirection dir :
-           {InputDirection::kNorthInput, InputDirection::kSouthInput,
-            InputDirection::kWestInput, InputDirection::kEastInput}) {
-        Point new_location =
-            to_extend.location + kInputDirectionMap[static_cast<int>(dir)];
-        if (computed_path.contains(new_location)) continue;
-        if (!board_.contains(new_location)) {
-          // Hit an unknown. Head here.
-          current_path_.insert(current_path_.end(),
-                               to_extend.directions.begin(),
-                               to_extend.directions.end());
-          current_path_.push_back(dir);
-          return absl::OkStatus();
-        }
-        if (board_[new_location] == 0) {
-          // Wall.
-          continue;
-        }
-        frontier.push_back(
-            {.location = new_location, .directions = to_extend.directions});
-        frontier.back().directions.push_back(dir);
-        computed_path.insert(new_location);
+    class PathWalk : public BFSInterface<PathWalk, Point> {
+     public:
+      PathWalk(const absl::flat_hash_map<Point, int>& board, Point start,
+               std::deque<InputDirection>* out_directions)
+       : board_(board), cur_(start), out_directions_(out_directions) {}
+
+      Point identifier() const override { return cur_; }
+
+      bool IsFinal() override {
+        if (board_.contains(cur_)) return false;
+        out_directions_->clear();
+        out_directions_->insert(out_directions_->end(), directions_.begin(), directions_.end());
+        return true;
       }
-      frontier.pop_front();
+
+      void AddNextSteps(State* state) override {
+        for (InputDirection dir :
+             {InputDirection::kNorthInput, InputDirection::kSouthInput,
+              InputDirection::kWestInput, InputDirection::kEastInput}) {
+          Point new_location = cur_ + kInputDirectionMap[static_cast<int>(dir)];
+          if (auto it = board_.find(new_location); it != board_.end() && it->second == 0) {
+            // Wall.
+            continue;
+          }
+          PathWalk next = *this;
+          next.cur_ = new_location;
+          next.directions_.push_back(dir);
+          state->AddNextStep(next);
+        }
+      }
+
+     private:
+      const absl::flat_hash_map<Point, int>& board_;
+      Point cur_;
+      std::vector<InputDirection> directions_;
+      std::deque<InputDirection>* out_directions_;
+    };
+
+    absl::optional<int> dist =
+        PathWalk(board_, pos_, &current_path_).FindMinSteps();
+    if (!dist) {
+      // Fully explored the map.
+      done_ = true;
+      current_path_ = {InputDirection::kNorthInput};
     }
-    // Fully explored the map.
-    done_ = true;
-    current_path_ = {InputDirection::kNorthInput};
     return absl::OkStatus();
   }
 
-  absl::StatusOr<int> DistanceToO2() {
-    struct Path {
-      Point location;
-      std::vector<InputDirection> directions;
-    };
-    absl::flat_hash_set<Point> computed_path = {Point{.x = 0, .y = 0}};
-    std::deque<Path> frontier;
-    frontier.push_back({.location = Point{.x = 0, .y = 0}});
-    for (int depth = 1; !frontier.empty(); ++depth) {
-      const Path& to_extend = frontier.front();
-      for (InputDirection dir :
-           {InputDirection::kNorthInput, InputDirection::kSouthInput,
-            InputDirection::kWestInput, InputDirection::kEastInput}) {
-        Point new_location =
-            to_extend.location + kInputDirectionMap[static_cast<int>(dir)];
-        if (computed_path.contains(new_location)) continue;
-        if (!board_.contains(new_location)) {
-          return absl::InternalError(
-              "Path to unknown location finding O2 distaince");
-        }
-        if (board_[new_location] == 0) {
-          // Wall.
-          continue;
-        }
-        if (board_[new_location] == 2) {
-          VLOG(1) << "Path = "
-                  << absl::StrJoin(to_extend.directions, "",
-                                   [](std::string* out, InputDirection dir) {
-                                     constexpr absl::string_view rec[] = {
-                                         "", "^", "\\/", "<", ">"};
-                                     absl::StrAppend(
-                                         out, rec[static_cast<int>(dir)]);
-                                   });
-          return to_extend.directions.size() + 1;
-        }
-        frontier.push_back(
-            {.location = new_location, .directions = to_extend.directions});
-        frontier.back().directions.push_back(dir);
-        computed_path.insert(new_location);
+  absl::optional<int> DistanceToO2() {
+    class PathWalk : public BFSInterface<PathWalk, Point> {
+     public:
+      PathWalk(const absl::flat_hash_map<Point, int>& board, Point start)
+       : board_(board), cur_(start) {}
+
+      Point identifier() const override { return cur_; }
+
+      bool IsFinal() override {
+        auto it = board_.find(cur_);
+        CHECK(it != board_.end());
+        return it->second == 2;
       }
-      frontier.pop_front();
-    }
-    return absl::InternalError("Ran out of paths");
+
+      void AddNextSteps(State* state) override {
+        for (Point dir : Cardinal::kFourDirs) {
+          Point next_cur = cur_ + dir;
+          auto it = board_.find(next_cur);
+          CHECK(it != board_.end());
+          if (it->second == 0) continue;
+          PathWalk next = *this;
+          next.cur_ = next_cur;
+          state->AddNextStep(next);
+        }
+      }
+      
+     private:
+      const absl::flat_hash_map<Point, int>& board_;
+      Point cur_;
+    };
+    return PathWalk(board_, Cardinal::kOrigin).FindMinSteps();
   }
 
   absl::StatusOr<int> GreatestDistanceFromO2() {
-    struct Path {
-      Point location;
-      std::vector<InputDirection> directions;
-    };
-    absl::flat_hash_set<Point> computed_path = {o2_pos_};
-    std::deque<Path> paths;
-    paths.push_back({.location = o2_pos_});
-    int max_path = 0;
-    for (int depth = 0; !paths.empty(); ++depth) {
-      const Path& to_extend = paths.front();
-      max_path = std::max<int>(max_path, to_extend.directions.size());
-      for (InputDirection dir :
-           {InputDirection::kNorthInput, InputDirection::kSouthInput,
-            InputDirection::kWestInput, InputDirection::kEastInput}) {
-        Point new_location =
-            to_extend.location + kInputDirectionMap[static_cast<int>(dir)];
-        if (computed_path.contains(new_location)) continue;
-        if (!board_.contains(new_location)) {
-          return absl::InternalError(
-              "Path to unknown location finding O2 distaince");
-        }
-        if (board_[new_location] == 0) {
-          // Wall.
-          continue;
-        }
-        paths.push_back(
-            {.location = new_location, .directions = to_extend.directions});
-        paths.back().directions.push_back(dir);
-        computed_path.insert(new_location);
+    class PathWalk : public BFSInterface<PathWalk, Point> {
+     public:
+      PathWalk(const absl::flat_hash_map<Point, int>& board, Point start, int* max_dist)
+       : board_(board), cur_(start), max_dist_(max_dist) {}
+
+      Point identifier() const override { return cur_; }
+
+      bool IsFinal() override {
+        return false;
       }
-      paths.pop_front();
+
+      void AddNextSteps(State* state) override {
+        // TODO(@monkeynova): Move max_dist check to IsFinal once BFSInterface
+        // checks history before calling IsFinal.
+        *max_dist_ = std::max(*max_dist_, num_steps());
+        for (Point dir : Cardinal::kFourDirs) {
+          Point next_cur = cur_ + dir;
+          auto it = board_.find(next_cur);
+          CHECK(it != board_.end());
+          if (it->second == 0) continue;
+          PathWalk next = *this;
+          next.cur_ = next_cur;
+          state->AddNextStep(next);
+        }
+      }
+      
+     private:
+      const absl::flat_hash_map<Point, int>& board_;
+      Point cur_;
+      int* max_dist_;
+    };
+
+    int max_dist = -1;
+    absl::optional<int> should_be_empty =
+        PathWalk(board_, o2_pos_, &max_dist).FindMinSteps();
+    if (should_be_empty) {
+      return AdventDay::Error("Internal error: GreatestDistanceFromO2 (found path)");
     }
-    return max_path;
+    return max_dist;
   }
 
   enum class OutputStatus {
@@ -266,8 +268,8 @@ absl::StatusOr<std::vector<std::string>> Day15_2019::Part1(
   if (absl::Status st = codes->Run(&droid); !st.ok()) {
     return st;
   }
-  LOG(WARNING) << droid.min() << "-" << droid.max();
-  LOG(WARNING) << droid.DebugBoard();
+  VLOG(1) << droid.min() << "-" << droid.max();
+  VLOG(1) << droid.DebugBoard();
 
   return IntReturn(droid.DistanceToO2());
 }
@@ -281,8 +283,8 @@ absl::StatusOr<std::vector<std::string>> Day15_2019::Part2(
   if (absl::Status st = codes->Run(&droid); !st.ok()) {
     return st;
   }
-  LOG(WARNING) << droid.min() << "-" << droid.max();
-  LOG(WARNING) << droid.DebugBoard();
+  VLOG(1) << droid.min() << "-" << droid.max();
+  VLOG(1) << droid.DebugBoard();
 
   return IntReturn(droid.GreatestDistanceFromO2());
 }
