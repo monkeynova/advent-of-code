1c7bc9cda18c575c97334dcf64f4c58ecabacf54
Keith Peters <keith@monkeynova.com>
2021-12-02 21:15:44 -0800

    2021/03

--- advent_of_code/2021/day03/day03.cc
+++ advent_of_code/2021/day03/day03.cc
@@ -19,12 +19,80 @@ namespace {
 
 absl::StatusOr<std::string> Day_2021_03::Part1(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  int64_t gamma = 0;
+  int64_t epsilon = 0;
+  for (int i = 0; i < input[0].size(); ++i) {
+    int64_t ones = 0;
+    int64_t zeros = 0;
+    for (absl::string_view rec : input) {
+      if (rec.size() <= i) return Error("Bad line: ", rec);
+      if (rec[i] == '1') ++ones;
+      else if (rec[i] == '0') ++zeros;
+      else return Error("Bad bit");
+    }
+    if (ones == zeros) return Error("Same rate");
+    if (ones > zeros) {
+      gamma = gamma * 2 + 1;
+      epsilon *= 2;
+    } else {
+      gamma *= 2;
+      epsilon = epsilon * 2 + 1;
+    }
+  }
+  return IntReturn(gamma * epsilon);
 }
 
 absl::StatusOr<std::string> Day_2021_03::Part2(
     absl::Span<absl::string_view> input) const {
-  return Error("Not implemented");
+  absl::flat_hash_set<absl::string_view> most(input.begin(), input.end());
+  for (int i = 0; i < input[0].size(); ++i) {
+    int64_t ones = 0;
+    int64_t zeros = 0;
+    for (absl::string_view rec : most) {
+      if (rec.size() <= i) return Error("Bad line: ", rec);
+      if (rec[i] == '1') ++ones;
+      else if (rec[i] == '0') ++zeros;
+      else return Error("Bad bit");
+    }
+    char delete_char = ones >= zeros ? '0' : '1';
+    for (auto it = most.begin(); it != most.end();) {
+      if ((*it)[i] == delete_char) most.erase(it++);
+      else ++it;
+    }
+
+    if (most.size() == 1) break;    
+  }
+  if (most.size() != 1) return Error("Cannot find most");
+
+  absl::flat_hash_set<absl::string_view> least(input.begin(), input.end());
+  for (int i = 0; i < input[0].size(); ++i) {
+    int64_t ones = 0;
+    int64_t zeros = 0;
+    for (absl::string_view rec : least) {
+      if (rec.size() <= i) return Error("Bad line: ", rec);
+      if (rec[i] == '1') ++ones;
+      else if (rec[i] == '0') ++zeros;
+      else return Error("Bad bit");
+    }
+    char delete_char = ones >= zeros ? '1' : '0';
+    for (auto it = least.begin(); it != least.end();) {
+      if ((*it)[i] == delete_char) least.erase(it++);
+      else ++it;
+    }
+
+    if (least.size() == 1) break;    
+  }
+  if (least.size() != 1) return Error("Cannot find least");
+
+  int64_t most_val = 0;
+  for (char c : *most.begin()) {
+    most_val = most_val * 2 + (c == '1' ? 1 : 0);
+  }
+  int64_t least_val = 0;
+  for (char c : *least.begin()) {
+    least_val = least_val * 2 + (c == '1' ? 1 : 0);
+  }
+  return IntReturn(most_val * least_val);
 }
 
 }  // namespace advent_of_code
