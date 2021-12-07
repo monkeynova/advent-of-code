a5034222c9f4a50fb706531874ce35c6e3052117
Keith Peters <keith@monkeynova.com>
2021-12-06 21:30:28 -0800

    2021/07 Performance and cleanup
    Use c_minmax_element for extracting
    the range, and use concavity to stop
    iterating once we found the minimum

--- advent_of_code/2021/day07/day07.cc
+++ advent_of_code/2021/day07/day07.cc
@@ -21,28 +21,21 @@ namespace {
 absl::StatusOr<std::string> Day_2021_07::Part1(
     absl::Span<absl::string_view> input) const {
   if (input.size() != 1) return Error("Bad input");
-  std::vector<int64_t> nums;
-  for (absl::string_view num_str : absl::StrSplit(input[0], ",")) {
-    int64_t num;
-    if (!absl::SimpleAtoi(num_str, &num)) return Error("Bad num: ", num_str);
-    nums.push_back(num);
-  }
+  absl::StatusOr<std::vector<int64_t>> nums = ParseAsInts(absl::StrSplit(input[0], ","));
+  if (!nums.ok()) return nums.status();
 
-  int64_t min = nums[0];
-  int64_t max = nums[0];
-  for (int64_t n : nums) {
-    min = std::min(n, min);
-    max = std::max(n, max);
-  }
+  const auto& [min_it, max_it] = absl::c_minmax_element(*nums);
   int64_t best_cost = std::numeric_limits<int64_t>::max();
-  for (int i = min; i <= max; ++i) {
+  for (int i = *min_it; i <= *max_it; ++i) {
     int64_t cost = 0;
-    for (int64_t n : nums) {
+    for (int64_t n : *nums) {
       cost += abs(n - i);
     }
     if (cost < best_cost) {
       best_cost = cost;
     }
+    // Cost curve is concave up. Once we bounce, stop.
+    if (cost > best_cost) break;
   }
   
   return IntReturn(best_cost);
@@ -51,29 +44,22 @@ absl::StatusOr<std::string> Day_2021_07::Part1(
 absl::StatusOr<std::string> Day_2021_07::Part2(
     absl::Span<absl::string_view> input) const {
   if (input.size() != 1) return Error("Bad input");
-  std::vector<int64_t> nums;
-  for (absl::string_view num_str : absl::StrSplit(input[0], ",")) {
-    int64_t num;
-    if (!absl::SimpleAtoi(num_str, &num)) return Error("Bad num: ", num_str);
-    nums.push_back(num);
-  }
+  absl::StatusOr<std::vector<int64_t>> nums = ParseAsInts(absl::StrSplit(input[0], ","));
+  if (!nums.ok()) return nums.status();
 
-  int64_t min = nums[0];
-  int64_t max = nums[0];
-  for (int64_t n : nums) {
-    min = std::min(n, min);
-    max = std::max(n, max);
-  }
+  const auto& [min_it, max_it] = absl::c_minmax_element(*nums);
   int64_t best_cost = std::numeric_limits<int64_t>::max();
-  for (int i = min; i <= max; ++i) {
+  for (int i = *min_it; i <= *max_it; ++i) {
     int64_t cost = 0;
-    for (int64_t n : nums) {
+    for (int64_t n : *nums) {
       int64_t d = abs(n - i);
       cost += d * (d + 1) / 2;
     }
     if (cost < best_cost) {
       best_cost = cost;
     }
+    // Cost curve is concave up. Once we bounce, stop.
+    if (cost > best_cost) break;
   }
   
   return IntReturn(best_cost);
