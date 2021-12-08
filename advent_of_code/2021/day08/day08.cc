1b5fafcc7089705575b9f23c5dadbe5be125effe
Keith Peters <keith@monkeynova.com>
2021-12-07 22:08:33 -0800

    2021/08 Futzing
    More logical deduction and less brute
    force

--- advent_of_code/2021/day08/day08.cc
+++ advent_of_code/2021/day08/day08.cc
@@ -21,7 +21,6 @@ std::string DebugSet(const absl::flat_hash_set<char>& possible) {
 }
 
 absl::flat_hash_set<char> Restrict(const absl::flat_hash_set<char>& possible, absl::string_view set) {
-  VLOG(1) << DebugSet(possible) << " - " << set;
   absl::flat_hash_set<char> new_possible;
   for (char c : set) {
     if (possible.contains(c)) new_possible.insert(c);
@@ -66,21 +65,30 @@ bool TryMap(
   return false;
 }
 
-absl::StatusOr<absl::flat_hash_map<char, char>> FindMap(const std::vector<absl::string_view>& left, const std::vector<absl::string_view>& right) {
-  absl::flat_hash_map<char, char> ret;
-  if (!TryMap(ret, 'a', left)) return AdventDay::Error("Could not find");
-  return ret;
+absl::StatusOr<absl::flat_hash_map<char, char>> FindMap(const std::vector<absl::string_view>& left) {
+  if (left.size() != 10) return AdventDay::Error("Bad exemplars");
 
+  absl::flat_hash_map<int64_t, absl::flat_hash_set<char>> countToSegments = {
+    {8, {'a', 'c'}}, {6, {'b'}}, {7, {'d', 'g'}}, {4, {'e'}}, {9, {'f'}},
+  };
+
+  absl::flat_hash_map<char, int64_t> observed;
+  for (absl::string_view in : left) {
+    for (char c : in) {
+      ++observed[c];
+    }
+  }
 
   absl::flat_hash_map<char, absl::flat_hash_set<char>> possible;
 
   for (char c1 : {'a', 'b', 'c', 'd', 'e', 'f', 'g'}) {
-    for (char c2 : {'a', 'b', 'c', 'd', 'e', 'f', 'g'}) {
-      possible[c1].insert(c2);
-    }
+    auto it = observed.find(c1);
+    if (it == observed.end()) return AdventDay::Error("Bad observed");
+    auto it2 = countToSegments.find(it->second);
+    if (it2 == countToSegments.end()) return AdventDay::Error("Bad count");
+    possible[c1] = it2->second;
   }
   for (absl::string_view test : left) {
-    VLOG(1) << "test: " << test;
     switch (test.size()) {
       case 2: {
         possible[test[0]] = Restrict(possible[test[0]], "cf");
@@ -102,31 +110,25 @@ absl::StatusOr<absl::flat_hash_map<char, char>> FindMap(const std::vector<absl::
       }
     }
   }
-  for (absl::string_view test : right) {
-    switch (test.size()) {
-      case 2: {
-        possible[test[0]] = Restrict(possible[test[0]], "cf");
-        possible[test[1]] = Restrict(possible[test[1]], "cf");
-        break;
-      }
-      case 3: {
-        possible[test[0]] = Restrict(possible[test[0]], "acf");
-        possible[test[1]] = Restrict(possible[test[1]], "acf");
-        possible[test[2]] = Restrict(possible[test[2]], "acf");
-        break;
-      }
-      case 4: {
-        possible[test[0]] = Restrict(possible[test[0]], "bcdf");
-        possible[test[1]] = Restrict(possible[test[1]], "bcdf");
-        possible[test[2]] = Restrict(possible[test[2]], "bcdf");
-        possible[test[3]] = Restrict(possible[test[3]], "bcdf");
-        break;
-      }
-    }
-  }
+  absl::flat_hash_set<char> definitive;
   for (const auto& [c1, set] : possible) {
-    VLOG(1) << c1 << ": " << DebugSet(set);
+    if (set.size() == 1) definitive.insert(*set.begin());
   }
+  bool did_work = true;
+  while (did_work) {
+    did_work = false;
+    for (auto& [c1, set] : possible) {
+      if (set.size() > 1) {
+        did_work = true;
+        absl::flat_hash_set<char> new_set;
+        for (char c : set) {
+          if (!definitive.contains(c)) new_set.insert(c);
+        }
+        set = std::move(new_set);
+      }
+    }
+  }
+  absl::flat_hash_map<char, char> ret;
   for (const auto& [c, set] : possible) {
     if (set.size() != 1) return AdventDay::Error("Bad possible");
     ret[c] = *set.begin();
@@ -162,7 +164,7 @@ absl::StatusOr<std::string> Day_2021_08::Part2(
     std::pair<absl::string_view, absl::string_view> split = absl::StrSplit(line, " | ");
     std::vector<absl::string_view> left = absl::StrSplit(split.first, " ");
     std::vector<absl::string_view> right = absl::StrSplit(split.second, " ");
-    absl::StatusOr<absl::flat_hash_map<char, char>> map = FindMap(left, right);
+    absl::StatusOr<absl::flat_hash_map<char, char>> map = FindMap(left);
     if (!map.ok()) return Error("Could not make map ", map.status().message(), " for ", line);
     int64_t right_val = 0;
     for (absl::string_view digit : right) {
