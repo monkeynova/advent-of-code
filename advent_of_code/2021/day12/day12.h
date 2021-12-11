34bd2bc40c527275049854f9a0e1b32fa9949b6e
Keith Peters <keith@monkeynova.com>
2021-12-11 05:58:33 -0800

    Empty 2021/12

---
+++ advent_of_code/2021/day12/day12.h
@@ -0,0 +1,18 @@
+#ifndef ADVENT_OF_CODE_2021_DAY12_H
+#define ADVENT_OF_CODE_2021_DAY12_H
+
+#include "advent_of_code/advent_day.h"
+
+namespace advent_of_code {
+
+class Day_2021_12 : public AdventDay {
+ public:
+  absl::StatusOr<std::string> Part1(
+      absl::Span<absl::string_view> input) const override;
+  absl::StatusOr<std::string> Part2(
+      absl::Span<absl::string_view> input) const override;
+};
+
+}  // namespace advent_of_code
+
+#endif  // ADVENT_OF_CODE_2021_DAY12_H
