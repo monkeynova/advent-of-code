47b71cfbeb94cc1168ae41585b497304964adffd
Keith Peters <keith@monkeynova.com>
2021-12-11 21:31:14 -0800

    2021/12 Futzing
    Vastly simplify the cruft in the original
    solution. Brings runtime from 3.6s -> 0.5s (most of that from the big
    example)

--- advent_of_code/2021/day12/day12.cc
+++ advent_of_code/2021/day12/day12.cc
@@ -16,82 +16,38 @@ namespace advent_of_code {
 namespace {
 
 struct State {
-  std::vector<absl::string_view> path;
+  absl::string_view cur;
   absl::flat_hash_set<absl::string_view> small_visit;
-  absl::string_view small_visit2;
-
-  const bool operator==(const State& o) const {
-    return path == o.path;
-  }
-
-  template <typename H>
-  friend H AbslHashValue(H h, const State& o) {
-    return H::combine(std::move(h), o.path);
-  }
+  bool has_visit2;
 };
 
-int64_t CountPaths(const DirectedGraph<int64_t>& map) {
-  absl::flat_hash_set<State> hist;
-  absl::flat_hash_set<State> end;
-  State start{.path = {"start"}, .small_visit = {"start"}};
+int64_t CountPaths(const DirectedGraph<int64_t>& map, bool allow_small_visit2) {
+  int64_t path_count = 0;
+  State start{.cur = "start", .small_visit = {"start"}};
   for (std::deque<State> queue = {start}; !queue.empty(); queue.pop_front()) {
     const State& cur = queue.front();
-    VLOG(3) << "cur: " << absl::StrJoin(cur.path, "->");
-    const std::vector<absl::string_view>* out = map.Outgoing(cur.path.back());
+    const std::vector<absl::string_view>* out = map.Outgoing(cur.cur);
     if (!out) continue;
     for (absl::string_view next : *out) {
-      if (cur.small_visit.contains(next)) continue;
-      State new_state = cur;
-      new_state.path.push_back(next);
-      if (next[0] >= 'a' && next[0] <= 'z') {
-        VLOG(3) << "Small: " << next;
-        new_state.small_visit.insert(next);
-      }
       if (next == "end") {
-        end.insert(new_state);
-        VLOG(3) << "end: " << absl::StrJoin(new_state.path, "->");
+        ++path_count;
         continue;
       }
-      if (hist.contains(new_state)) continue;
-      hist.insert(new_state);
-      queue.push_back(new_state);
-    }
-  }
-  return end.size();
-}
-
-int64_t CountPaths2(const DirectedGraph<int64_t>& map) {
-  absl::flat_hash_set<State> hist;
-  absl::flat_hash_set<State> end;
-  State start{.path = {"start"}, .small_visit = {"start"}};
-  for (std::deque<State> queue = {start}; !queue.empty(); queue.pop_front()) {
-    const State& cur = queue.front();
-    VLOG(3) << "cur: " << absl::StrJoin(cur.path, "->");
-    const std::vector<absl::string_view>* out = map.Outgoing(cur.path.back());
-    if (!out) continue;
-    for (absl::string_view next : *out) {
       State new_state = cur;
-      new_state.path.push_back(next);
+      new_state.cur = next;
       if (cur.small_visit.contains(next)) {
-        if (!cur.small_visit2.empty()) continue;
+        if (!allow_small_visit2) continue;
+        if (cur.has_visit2) continue;
         if (next == "start") continue;
-        new_state.small_visit2 = next;
+        new_state.has_visit2 = true;
       }
       if (next[0] >= 'a' && next[0] <= 'z') {
-        VLOG(3) << "Small: " << next;
         new_state.small_visit.insert(next);
       }
-      if (next == "end") {
-        end.insert(new_state);
-        VLOG(3) << "end: " << absl::StrJoin(new_state.path, "->");
-        continue;
-      }
-      if (hist.contains(new_state)) continue;
-      hist.insert(new_state);
-      queue.push_back(new_state);
+      queue.push_back(std::move(new_state));
     }
   }
-  return end.size();
+  return path_count;
 }
 
 }  // namespace
@@ -105,7 +61,7 @@ absl::StatusOr<std::string> Day_2021_12::Part1(
     map.AddEdge(dst, src);
   }
 
-  return IntReturn(CountPaths(map));
+  return IntReturn(CountPaths(map, /*allow_small_visit2=*/false));
 }
 
 absl::StatusOr<std::string> Day_2021_12::Part2(
@@ -117,7 +73,7 @@ absl::StatusOr<std::string> Day_2021_12::Part2(
     map.AddEdge(dst, src);
   }
 
-  return IntReturn(CountPaths2(map));
+  return IntReturn(CountPaths(map, /*allow_small_visit2=*/true));
 }
 
 }  // namespace advent_of_code
