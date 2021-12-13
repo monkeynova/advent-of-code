e0a4955458f99fb8382ea51181c300bd0c546b2d
Keith Peters <keith@monkeynova.com>
2021-12-13 10:07:32 -0800

    Empty 2021/14

---
+++ advent_of_code/2021/day14/day14_test.cc
@@ -0,0 +1,15 @@
+#include "advent_of_code/2021/day14/day14.h"
+
+#include "advent_of_code/infra/file_test.h"
+#include "gmock/gmock.h"
+
+namespace advent_of_code {
+
+namespace advent_of_code {
+
+using DayTest = FileTest<Day_2021_14>;
+
+TEST_F(DayTest, FileBasedTest) { EXPECT_TRUE(RunTest()); }
+
+}  // namespace advent_of_code
+}  // namespace advent_of_code
