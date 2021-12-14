36d00cd6fb5dbf817c3e015fd35e5a3d4cf2ab89
Keith Peters <keith@monkeynova.com>
2021-12-13 21:39:02 -0800

    2021/14 Futzing
    Extract Expand methods.

--- advent_of_code/2021/day14/day14.cc
+++ advent_of_code/2021/day14/day14.cc
@@ -31,6 +31,32 @@ absl::StatusOr<Problem> Parse(absl::Span<absl::string_view> input) {
   return ret;
 }
 
+absl::StatusOr<std::string> Expand(absl::string_view rec, const Problem& p) {
+  std::string next;
+  for (int j = 0; j < rec.size() - 1; ++j) {
+    next.append(rec.substr(j, 1));
+    auto it = p.rules.find(rec.substr(j, 2));
+    if (it == p.rules.end()) return Error("Could not rule");
+    next.append(it->second);
+  }
+  next.append(1, rec.back());
+  return next;
+}
+
+absl::StatusOr<absl::flat_hash_map<std::string, int64_t>> Expand(
+    const absl::flat_hash_map<std::string, int64_t>& pair_counts, const Problem& p) {
+  absl::flat_hash_map<std::string, int64_t> new_pair_counts;
+  for (const auto& [str, count] : pair_counts) {
+    auto it = p.rules.find(str);
+    if (it == p.rules.end()) return Error("Bad");
+    std::string left = {str[0], it->second[0]};
+    std::string right = {it->second[0], str[1]};
+    new_pair_counts[left] += count;
+    new_pair_counts[right] += count;
+  }
+  return new_pair_counts;
+}
+
 }  // namespace
 
 absl::StatusOr<std::string> Day_2021_14::Part1(
@@ -40,16 +66,9 @@ absl::StatusOr<std::string> Day_2021_14::Part1(
 
   std::string rec = std::string(p->start);
   for (int i = 0; i < 10; ++i) {
-    absl::string_view rec_view = rec;
-    std::string next;
-    for (int j = 0; j < rec.size() - 1; ++j) {
-      next.append(rec_view.substr(j, 1));
-      auto it = p->rules.find(rec_view.substr(j, 2));
-      if (it == p->rules.end()) return Error("Could not rule");
-      next.append(it->second);
-    }
-    next.append(1, rec_view.back());
-    rec = next;
+    absl::StatusOr<std::string> next = Expand(rec, *p);
+    if (!next.ok()) return next.status();
+    rec = std::move(*next);
   }
   absl::flat_hash_map<absl::string_view, int64_t> counts;
   absl::string_view rec_view = rec;
@@ -75,32 +94,24 @@ absl::StatusOr<std::string> Day_2021_14::Part2(
     ++pair_counts[std::string(p->start.substr(i, 2))];
   }
   for (int i = 0; i < 40; ++i) {
-    absl::flat_hash_map<std::string, int64_t> new_pair_counts;
-    for (const auto& [str, count] : pair_counts) {
-      auto it = p->rules.find(str);
-      if (it == p->rules.end()) return Error("Bad");
-      std::string left = {str[0], it->second[0]};
-      std::string right = {it->second[0], str[1]};
-      new_pair_counts[left] += count;
-      new_pair_counts[right] += count;
-    }
-    pair_counts = std::move(new_pair_counts);
+    absl::StatusOr<absl::flat_hash_map<std::string, int64_t>> new_pair_counts =
+        Expand(pair_counts, *p);
+    pair_counts = std::move(*new_pair_counts);
   }
-  absl::flat_hash_map<std::string, int64_t> counts;
+  absl::flat_hash_map<std::string, int64_t> single_char_counts;
   for (const auto& [str, count] : pair_counts) {
-    counts[str.substr(0, 1)] += count;
-    counts[str.substr(1, 1)] += count;
-  }
-  for (auto& [str, count] : pair_counts) {
+    single_char_counts[str.substr(0, 1)] += count;
+    single_char_counts[str.substr(1, 1)] += count;
   }
 
   int64_t max = 0;
   int64_t min = std::numeric_limits<int64_t>::max();
-  for (const auto& [str, c] : counts) {
+  for (const auto& [str, c] : single_char_counts) {
     int64_t count = c;
     // End characters missed one count in pairs.
     if (str == p->start.substr(0, 1)) ++count;
     if (str == p->start.substr(p->start.size() - 1, 1)) ++count;
+    if (count % 2 != 0) return Error("MOD 2");
     // All characters now counted twice (once in each pair).
     count /= 2;
     if (count > max) max = count;
