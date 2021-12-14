3fdba5aba6f471c60c45f320e77082e339b57fcc
Keith Peters <keith@monkeynova.com>
2021-12-13 21:30:35 -0800

    2021/14 Futzing
    Extract a problem parser

--- advent_of_code/2021/day14/day14.cc
+++ advent_of_code/2021/day14/day14.cc
@@ -14,28 +14,38 @@ namespace advent_of_code {
 
 namespace {
 
-// Helper methods go here.
+struct Problem {
+  absl::string_view start;
+  absl::flat_hash_map<absl::string_view, absl::string_view> rules;
+};
+
+absl::StatusOr<Problem> Parse(absl::Span<absl::string_view> input) {
+  Problem ret;
+  if (input.size() < 3) return Error("Bad input");
+  ret.start = input[0];
+  if (input[1] != "") return Error("Bad input");
+  for (int i = 2; i < input.size(); ++i) {
+    auto [pair, insert] = PairSplit(input[i], " -> ");
+    ret.rules.emplace(pair, insert);
+  }
+  return ret;
+}
 
 }  // namespace
 
 absl::StatusOr<std::string> Day_2021_14::Part1(
     absl::Span<absl::string_view> input) const {
-  if (input.size() < 3) return Error("Bad input");
-  absl::string_view start = input[0];
-  if (input[1] != "") return Error("Bad input");
-  absl::flat_hash_map<absl::string_view, absl::string_view> rules;
-  for (int i = 2; i < input.size(); ++i) {
-    auto [pair, insert] = PairSplit(input[i], " -> ");
-    rules.emplace(pair, insert);
-  }
-  std::string rec = std::string(start);
+  absl::StatusOr<Problem> p = Parse(input);
+  if (!p.ok()) return p.status();
+
+  std::string rec = std::string(p->start);
   for (int i = 0; i < 10; ++i) {
     absl::string_view rec_view = rec;
     std::string next;
     for (int j = 0; j < rec.size() - 1; ++j) {
       next.append(rec_view.substr(j, 1));
-      auto it = rules.find(rec_view.substr(j, 2));
-      if (it == rules.end()) return Error("Could not rule");
+      auto it = p->rules.find(rec_view.substr(j, 2));
+      if (it == p->rules.end()) return Error("Could not rule");
       next.append(it->second);
     }
     next.append(1, rec_view.back());
@@ -57,26 +67,18 @@ absl::StatusOr<std::string> Day_2021_14::Part1(
 
 absl::StatusOr<std::string> Day_2021_14::Part2(
     absl::Span<absl::string_view> input) const {
-  if (input.size() < 3) return Error("Bad input");
-  absl::string_view start = input[0];
-  if (input[1] != "") return Error("Bad input");
-  absl::flat_hash_map<absl::string_view, absl::string_view> rules;
-  for (int i = 2; i < input.size(); ++i) {
-    auto [pair, insert] = PairSplit(input[i], " -> ");
-    rules.emplace(pair, insert);
-  }
+  absl::StatusOr<Problem> p = Parse(input);
+  if (!p.ok()) return p.status();
   
   absl::flat_hash_map<std::string, int64_t> pair_counts;
-  for (int i = 0; i < start.size() - 1; ++i) {
-    ++pair_counts[std::string(start.substr(i, 2))];
+  for (int i = 0; i < p->start.size() - 1; ++i) {
+    ++pair_counts[std::string(p->start.substr(i, 2))];
   }
   for (int i = 0; i < 40; ++i) {
-    //VLOG(1) << i;
     absl::flat_hash_map<std::string, int64_t> new_pair_counts;
     for (const auto& [str, count] : pair_counts) {
-      //VLOG(1) << "  " << str << ": " << count;
-      auto it = rules.find(str);
-      if (it == rules.end()) return Error("Bad");
+      auto it = p->rules.find(str);
+      if (it == p->rules.end()) return Error("Bad");
       std::string left = {str[0], it->second[0]};
       std::string right = {it->second[0], str[1]};
       new_pair_counts[left] += count;
@@ -96,11 +98,11 @@ absl::StatusOr<std::string> Day_2021_14::Part2(
   int64_t min = std::numeric_limits<int64_t>::max();
   for (const auto& [str, c] : counts) {
     int64_t count = c;
-    if (str == start.substr(0, 1) || str == start.substr(start.size() - 1, 1)) {
-      ++count;
-    }
+    // End characters missed one count in pairs.
+    if (str == p->start.substr(0, 1)) ++count;
+    if (str == p->start.substr(p->start.size() - 1, 1)) ++count;
+    // All characters now counted twice (once in each pair).
     count /= 2;
-    VLOG(1) << str << ": " << count;
     if (count > max) max = count;
     if (count < min) min = count;
   }
