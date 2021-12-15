4d61db0076fa64a104713c67ff6cb61fe31edf4a
Keith Peters <keith@monkeynova.com>
2021-12-15 12:59:35 -0800

    BFS Futzing
    Single FindMinPath (templated) implementation
    Remove "friend" class references

--- advent_of_code/bfs.h
+++ advent_of_code/bfs.h
@@ -15,10 +15,10 @@ class AStarGT {
  public:
   bool operator()(const BFSImpl& a, const BFSImpl& b) {
     // Tie break with num_steps sp we can't add to the history a backtrack.
-    int a_steps = a.num_steps_ + a.min_steps_to_final();
-    int b_steps = b.num_steps_ + b.min_steps_to_final();
+    int a_steps = a.num_steps() + a.min_steps_to_final();
+    int b_steps = b.num_steps() + b.min_steps_to_final();
     if (a_steps != b_steps) return a_steps > b_steps;
-    return a.num_steps_ > b.num_steps_;
+    return a.num_steps() > b.num_steps();
   }
 };
 
@@ -48,6 +48,8 @@ template <typename BFSImpl, typename HistType = BFSImpl>
 class BFSInterface {
  public:
   class State;
+  class DequeState;
+  class QueueState;
 
   virtual ~BFSInterface() = default;
 
@@ -57,8 +59,15 @@ class BFSInterface {
 
   virtual int min_steps_to_final() const { return 0; }
 
-  absl::optional<int> FindMinSteps();
-  absl::optional<int> FindMinStepsAStar();
+  absl::optional<int> FindMinSteps() {
+    return FindMinStepsImpl<DequeState>();
+  }
+  absl::optional<int> FindMinStepsAStar() {
+    static_assert(std::is_copy_assignable_v<BFSImpl>,
+                  "BFSImpl must be copy-assignable for AStar");
+    VLOG(3) << "New AStar: " << identifier();
+    return FindMinStepsImpl<QueueState>();
+  }
 
   int num_steps() const { return num_steps_; }
 
@@ -68,10 +77,23 @@ class BFSInterface {
   void add_steps(int num_steps) { num_steps_ += num_steps; }
 
  private:
-  int num_steps_ = 0;
+  template <typename QueueType>
+  absl::optional<int> FindMinStepsImpl() {
+    static_assert(std::is_base_of<BFSInterface<BFSImpl, HistType>, BFSImpl>(),
+                  "BFSInterface must be templated with a subclass");
+    if (IsFinal()) return 0;
+    BFSImpl& start = *dynamic_cast<BFSImpl*>(this);
+    for (QueueType state(start); !state.empty(); state.pop()) {
+      const BFSImpl& cur = state.next();
+      if (cur.IsFinal()) return cur.num_steps();
+      VLOG(3) << "@" << cur.num_steps_ << "; " << cur.identifier();
+      cur.AddNextSteps(&state);
+      if (state.ret()) return *state.ret();
+    }
+    return absl::nullopt;
+  }
 
-  friend class State;
-  friend class AStarGT<BFSImpl>;
+  int num_steps_ = 0;
 };
 
 template <typename BFSImpl, typename HistType>
@@ -83,21 +105,24 @@ class BFSInterface<BFSImpl, HistType>::State {
     ++next.num_steps_;
     auto it = hist_.find(next.identifier());
     if (it != hist_.end() && it->second <= next.num_steps()) return;
-    if (next.IsFinal()) ret = next.num_steps_;
     hist_[next.identifier()] = next.num_steps();
     AddToFrontier(std::move(next));
   }
 
   virtual void AddToFrontier(BFSImpl next) = 0;
 
+  absl::optional<int> ret() const { return ret_; }
+
+ protected:
+  void set_ret(int ret) { ret_ = ret; }
+
  private:
   absl::flat_hash_map<HistType, int> hist_;
-  absl::optional<int> ret;
-  friend class BFSInterface;
+  absl::optional<int> ret_;
 };
 
 template <typename BFSImpl, typename HistType>
-class DequeState : public BFSInterface<BFSImpl, HistType>::State {
+class BFSInterface<BFSImpl, HistType>::DequeState : public BFSInterface<BFSImpl, HistType>::State {
  public:
   explicit DequeState(BFSImpl start)
       : BFSInterface<BFSImpl, HistType>::State(start) {
@@ -105,16 +130,20 @@ class DequeState : public BFSInterface<BFSImpl, HistType>::State {
   }
 
   void AddToFrontier(BFSImpl next) final {
+    if (next.IsFinal()) this->set_ret(next.num_steps());
     frontier_.push_back(std::move(next));
   }
 
+  bool empty() const { return frontier_.empty(); }
+  const BFSImpl& next() const { return frontier_.front(); }
+  void pop() { return frontier_.pop_front(); }
+
  private:
   std::deque<BFSImpl> frontier_;
-  friend class BFSInterface<BFSImpl, HistType>;
 };
 
 template <typename BFSImpl, typename HistType>
-class QueueState : public BFSInterface<BFSImpl, HistType>::State {
+class BFSInterface<BFSImpl, HistType>::QueueState : public BFSInterface<BFSImpl, HistType>::State {
  public:
   explicit QueueState(BFSImpl start)
       : BFSInterface<BFSImpl, HistType>::State(start) {
@@ -127,49 +156,14 @@ class QueueState : public BFSInterface<BFSImpl, HistType>::State {
     frontier_.push(std::move(next));
   }
 
+  bool empty() const { return frontier_.empty(); }
+  BFSImpl next() const { return frontier_.top(); }
+  void pop() { return frontier_.pop(); }
+
  private:
   std::priority_queue<BFSImpl, std::vector<BFSImpl>, AStarGT<BFSImpl>>
       frontier_;
-  friend class BFSInterface<BFSImpl, HistType>;
 };
-
-template <typename BFSImpl, typename HistType>
-absl::optional<int> BFSInterface<BFSImpl, HistType>::FindMinSteps() {
-  static_assert(std::is_base_of<BFSInterface<BFSImpl, HistType>, BFSImpl>(),
-                "BFSInterface must be templated with a subclass");
-  if (IsFinal()) return 0;
-  DequeState<BFSImpl, HistType> state(*dynamic_cast<BFSImpl*>(this));
-  while (!state.frontier_.empty()) {
-    const BFSImpl& cur = state.frontier_.front();
-    VLOG(3) << "@" << cur.num_steps_ << "; " << cur.identifier();
-    cur.AddNextSteps(&state);
-    if (state.ret) return *state.ret;
-    state.frontier_.pop_front();
-  }
-  return absl::nullopt;
-}
-
-template <typename BFSImpl, typename HistType>
-absl::optional<int> BFSInterface<BFSImpl, HistType>::FindMinStepsAStar() {
-  static_assert(std::is_base_of<BFSInterface<BFSImpl, HistType>, BFSImpl>(),
-                "BFSInterface must be templated with a subclass");
-  static_assert(std::is_copy_assignable_v<BFSImpl>,
-                "BFSImpl must be copy-assignable for AStar");
-  VLOG(3) << "New AStar: " << identifier();
-  if (IsFinal()) return 0;
-  QueueState<BFSImpl, HistType> state(*dynamic_cast<BFSImpl*>(this));
-  while (!state.frontier_.empty()) {
-    // Copy (as opposed to a const reference) because mutations to the queue
-    // can invalidate the reference.
-    BFSImpl cur = state.frontier_.top();
-    if (cur.IsFinal()) return cur.num_steps();
-    VLOG(3) << "  Next: " << cur.identifier() << " (" << cur.num_steps() << "+"
-            << cur.min_steps_to_final() << ")";
-    state.frontier_.pop();
-    cur.AddNextSteps(&state);
-  }
-  return absl::nullopt;
-}
 
 }  // namespace advent_of_code
 
