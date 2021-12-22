85c48e71e8bb4aa7339ac65c894e59bbd6fe3420
Keith Peters <keith@monkeynova.com>
2021-12-22 06:09:30 -0800

    2021/22 Futzing
    Mostly Cube iteration interface.

--- advent_of_code/2021/day22/day22.cc
+++ advent_of_code/2021/day22/day22.cc
@@ -18,6 +18,36 @@ namespace {
 struct Cube {
   Point3 min;
   Point3 max;
+
+  struct iterator {
+    Point3 cur = Point3{0, 0, 0};
+    const Cube* base = nullptr;
+
+    iterator operator++() {
+      if (++cur.x > base->max.x) {
+        cur.x = base->min.x;
+        if (++cur.y > base->max.y) {
+          cur.y = base->min.y;
+          if (++cur.z > base->max.z) {
+            base = nullptr;
+            cur = Point3{0, 0, 0};
+          }
+        }
+      }
+      return *this;
+    }
+
+    bool operator==(const iterator& o) const {
+      return base == o.base && cur == o.cur;
+    }
+    bool operator!=(const iterator& o) const { return !operator==(o); }
+
+    Point3 operator*() const { return cur; }
+  };
+
+  iterator begin() const { return iterator{.cur = min, .base = this}; }
+  iterator end() const { return iterator{}; }
+
   int64_t Volume() const {
     // 1ll forces the addition to return int64 before multiplying.
     return (max.x - min.x + 1ll) * (max.y - min.y + 1ll) *
@@ -32,7 +62,7 @@ struct Cube {
     if (p.z > max.z) return false; 
     return true;
   }
-  bool Contains(const Cube& o) const {
+  bool FullyContains(const Cube& o) const {
     if (o.min.x < min.x) return false;
     if (o.max.x > max.x) return false;
     if (o.min.y < min.y) return false;
@@ -51,17 +81,15 @@ struct Cube {
     return true;
   }
 
-  std::vector<Cube> Remove(const Cube& o) const {
+  std::vector<Cube> SetDifference(const Cube& o) const {
     std::vector<Cube> ret = {*this};
     {
       // Split this cube along o.min.x if appropriate.
       std::vector<Cube> new_ret;
       for (const Cube& c : ret) {
         if (o.min.x > c.min.x && o.min.x <= c.max.x) {
-          new_ret.push_back(
-              {{c.min.x, c.min.y, c.min.z}, {o.min.x - 1, c.max.y, c.max.z}});
-          new_ret.push_back(
-              {{o.min.x, c.min.y, c.min.z}, {c.max.x, c.max.y, c.max.z}});
+          new_ret.push_back({c.min, {o.min.x - 1, c.max.y, c.max.z}});
+          new_ret.push_back({{o.min.x, c.min.y, c.min.z}, c.max});
         } else {
           new_ret.push_back(c);
         }
@@ -73,10 +101,8 @@ struct Cube {
       std::vector<Cube> new_ret;
       for (const Cube& c : ret) {
         if (o.max.x >= c.min.x && o.max.x < c.max.x) {
-          new_ret.push_back(
-              {{c.min.x, c.min.y, c.min.z}, {o.max.x, c.max.y, c.max.z}});
-          new_ret.push_back(
-              {{o.max.x + 1, c.min.y, c.min.z}, {c.max.x, c.max.y, c.max.z}});
+          new_ret.push_back({c.min, {o.max.x, c.max.y, c.max.z}});
+          new_ret.push_back({{o.max.x + 1, c.min.y, c.min.z}, c.max});
         } else {
           new_ret.push_back(c);
         }
@@ -88,10 +114,8 @@ struct Cube {
       std::vector<Cube> new_ret;
       for (const Cube& c : ret) {
         if (o.min.y > c.min.y && o.min.y <= c.max.y) {
-          new_ret.push_back(
-              {{c.min.x, c.min.y, c.min.z}, {c.max.x, o.min.y - 1, c.max.z}});
-          new_ret.push_back(
-              {{c.min.x, o.min.y, c.min.z}, {c.max.x, c.max.y, c.max.z}});
+          new_ret.push_back({c.min, {c.max.x, o.min.y - 1, c.max.z}});
+          new_ret.push_back({{c.min.x, o.min.y, c.min.z}, c.max});
         } else {
           new_ret.push_back(c);
         }
@@ -103,10 +127,8 @@ struct Cube {
       std::vector<Cube> new_ret;
       for (const Cube& c : ret) {
         if (o.max.y >= c.min.y && o.max.y < c.max.y) {
-          new_ret.push_back(
-              {{c.min.x, c.min.y, c.min.z}, {c.max.x, o.max.y, c.max.z}});
-          new_ret.push_back(
-              {{c.min.x, o.max.y + 1, c.min.z}, {c.max.x, c.max.y, c.max.z}});
+          new_ret.push_back({c.min, {c.max.x, o.max.y, c.max.z}});
+          new_ret.push_back({{c.min.x, o.max.y + 1, c.min.z}, c.max});
         } else {
           new_ret.push_back(c);
         }
@@ -118,10 +140,8 @@ struct Cube {
       std::vector<Cube> new_ret;
       for (const Cube& c : ret) {
         if (o.min.z > c.min.z && o.min.z <= c.max.z) {
-          new_ret.push_back(
-              {{c.min.x, c.min.y, c.min.z}, {c.max.x, c.max.y, o.min.z - 1}});
-          new_ret.push_back(
-              {{c.min.x, c.min.y, o.min.z}, {c.max.x, c.max.y, c.max.z}});
+          new_ret.push_back({c.min, {c.max.x, c.max.y, o.min.z - 1}});
+          new_ret.push_back({{c.min.x, c.min.y, o.min.z}, c.max});
         } else {
           new_ret.push_back(c);
         }
@@ -133,10 +153,8 @@ struct Cube {
       std::vector<Cube> new_ret;
       for (const Cube& c : ret) {
         if (o.max.z >= c.min.z && o.max.z < c.max.z) {
-          new_ret.push_back(
-              {{c.min.x, c.min.y, c.min.z}, {c.max.x, c.max.y, o.max.z}});
-          new_ret.push_back(
-              {{c.min.x, c.min.y, o.max.z + 1}, {c.max.x, c.max.y, c.max.z}});
+          new_ret.push_back({c.min, {c.max.x, c.max.y, o.max.z}});
+          new_ret.push_back({{c.min.x, c.min.y, o.max.z + 1}, c.max});
         } else {
           new_ret.push_back(c);
         }
@@ -146,7 +164,8 @@ struct Cube {
     {
       std::vector<Cube> new_ret;
       for (const Cube& c : ret) {
-        if (o.Contains(c)) continue;
+        if (o.FullyContains(c)) continue;
+        CHECK(!o.Overlaps(c));
         new_ret.push_back(c);
       }
       ret = std::move(new_ret);
@@ -155,60 +174,67 @@ struct Cube {
   }
 };
 
-struct Space {
+struct CubeSet {
  public:
-  Space() = default;
+  CubeSet() = default;
 
-  void AddOn(Cube in) {
-    std::vector<Cube> new_on;
-    for (const Cube& c : on) {
-      if (c.Overlaps(in)) {
-        std::vector<Cube> new_c = c.Remove(in);
-        for (const Cube& c2 : new_c) {
-          new_on.push_back(c2);
-        }
-      } else {
-        new_on.push_back(c);
-      }
-    }
-    new_on.push_back(in);
-    on = std::move(new_on);
+  void Union(Cube in) {
+    SetDifference(in);
+    set_.push_back(in);
   }
 
-  void AddOff(Cube in) {
-    std::vector<Cube> new_on;
-    for (const Cube& c : on) {
+  void SetDifference(Cube in) {
+    std::vector<Cube> new_set;
+    for (const Cube& c : set_) {
       if (c.Overlaps(in)) {
-        std::vector<Cube> new_c = c.Remove(in);
-        for (const Cube& c2 : new_c) {
-          new_on.push_back(c2);
-        }
+        std::vector<Cube> split_c = c.SetDifference(in);
+        new_set.insert(new_set.end(), split_c.begin(), split_c.end());
       } else {
-        new_on.push_back(c);
+        new_set.push_back(c);
       }
     }
-    on = std::move(new_on);
+    set_ = std::move(new_set);
   }
 
   int64_t Volume() const {
     int64_t sum = 0;
-    for (const Cube& c : on) {
+    for (const Cube& c : set_) {
       sum += c.Volume();
     }
     return sum;
   }
 
  private:
-  std::vector<Cube> on;
+  std::vector<Cube> set_;
 };
 
-// Helper methods go here.
+absl::Status Audit() {
+  Cube c1({{10, 10, 10}, {12, 12, 12}});
+  Cube c2({{11, 11, 11}, {13, 13, 13}});
+  std::vector<Cube> removed = c1.SetDifference(c2);
+  for (Point3 p : c1) {
+    bool should_in = c1.Contains(p) && !c2.Contains(p);
+    int in_count = 0;
+    for (const Cube& r : removed) {
+      if (r.Contains(p)) {
+        ++in_count;
+      }
+    }
+    if (in_count != should_in ? 1 : 0) {
+      VLOG(1) << "Bad point: " << p
+              << "; should=" << (should_in ? "in" : "out");
+      return Error("Bad point");
+    }
+  }
+  return absl::OkStatus();
+}
 
 }  // namespace
 
 absl::StatusOr<std::string> Day_2021_22::Part1(
     absl::Span<absl::string_view> input) const {
   absl::flat_hash_set<Point3> map;
+  Cube bound{{-50, -50, -50}, {50, 50, 50}};
   for (absl::string_view line : input) {
     absl::string_view type;
     int x0, x1, y0, y1, z0, z1;
@@ -219,24 +245,23 @@ absl::StatusOr<std::string> Day_2021_22::Part1(
             &type, &x0, &x1, &y0, &y1, &z0, &z1)) {
       return Error("Bad line: ", line);
     }
-    x0 = std::max(x0, -50);
-    x1 = std::min(x1, 50);
-    y0 = std::max(y0, -50);
-    y1 = std::min(y1, 50);
-    z0 = std::max(z0, -50);
-    z1 = std::min(z1, 50);
-    for (int x = x0; x <= x1; ++x) {
-      for (int y = y0; y <= y1; ++y) {
-        for (int z = z0; z <= z1; ++z) {
-          Point3 p{x, y, z};
-          if (type == "on") {
-            map.insert(p);
-          } else if (type == "off") {
-            map.erase(p);
-          } else {
-            return Error("Neither on nor off?");
-          }
-        }
+
+    Cube c{{x0, y0, z0}, {x1, y1, z1}};
+    if (!c.Overlaps(bound)) continue;
+    c.min.x = std::max(c.min.x, bound.min.x);
+    c.min.y = std::max(c.min.y, bound.min.y);
+    c.min.z = std::max(c.min.z, bound.min.z);
+    c.max.x = std::min(c.max.x, bound.max.x);
+    c.max.y = std::min(c.max.y, bound.max.y);
+    c.max.z = std::min(c.max.z, bound.max.z);
+    VLOG(1) << c.min << "-" << c.max;
+    for (Point3 p : c) {
+      if (type == "on") {
+        map.insert(p);
+      } else if (type == "off") {
+        map.erase(p);
+      } else {
+        return Error("Neither on nor off?");
       }
     }
   }
@@ -246,30 +271,9 @@ absl::StatusOr<std::string> Day_2021_22::Part1(
 absl::StatusOr<std::string> Day_2021_22::Part2(
     absl::Span<absl::string_view> input) const {
   if (run_audit()) {
-    Cube c1({{10, 10, 10}, {12, 12, 12}});
-    Cube c2({{11, 11, 11}, {13, 13, 13}});
-    std::vector<Cube> removed = c1.Remove(c2);
-    for (int x = 10; x <= 12; ++x) {
-      for (int y = 10; y <= 12; ++y) {
-        for (int z = 10; z <= 12; ++z) {
-          Point3 p{x, y, z};
-          bool should_in = c1.Contains(p) && !c2.Contains(p);
-          int in_count = 0;
-          for (const Cube& r : removed) {
-            if (r.Contains(p)) {
-              ++in_count;
-            }
-          }
-          if (in_count != should_in ? 1 : 0) {
-            VLOG(1) << "Bad point: " << p
-                    << "; should=" << (should_in ? "in" : "out");
-            return Error("Bad point");
-          }
-        }
-      }
-    }
+    if (auto st = Audit(); !st.ok()) return st;
   }
-  Space s;
+  CubeSet s;
   for (absl::string_view line : input) {
     absl::string_view type;
     int x0, x1, y0, y1, z0, z1;
@@ -280,10 +284,11 @@ absl::StatusOr<std::string> Day_2021_22::Part2(
             &type, &x0, &x1, &y0, &y1, &z0, &z1)) {
       return Error("Bad line: ", line);
     }
+    Cube c{{x0, y0, z0}, {x1, y1, z1}};
     if (type == "on") {
-      s.AddOn(Cube{{x0, y0, z0}, {x1, y1, z1}});
+      s.Union(c);
     } else if (type == "off") {
-      s.AddOff(Cube{{x0, y0, z0}, {x1, y1, z1}});
+      s.SetDifference(c);
     } else {
       return Error("Neither on nor off?");
     }
