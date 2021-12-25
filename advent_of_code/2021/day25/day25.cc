b61fa51d7f2743f0d27569cd9f04f7cf309be6c9
Keith Peters <keith@monkeynova.com>
2021-12-24 21:12:34 -0800

    Solution 2021/25

--- advent_of_code/2021/day25/day25.cc
+++ advent_of_code/2021/day25/day25.cc
@@ -7,6 +7,7 @@
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
 #include "absl/strings/str_split.h"
+#include "advent_of_code/char_board.h"
 #include "glog/logging.h"
 #include "re2/re2.h"
 
@@ -14,18 +15,73 @@ namespace advent_of_code {
 
 namespace {
 
-// Helper methods go here.
+absl::StatusOr<CharBoard> UpdateEast(const CharBoard& b) {
+  CharBoard out(b.width(), b.height());
+  absl::flat_hash_set<Point> skip;
+  for (Point p : b.range()) {
+    if (skip.contains(p)) continue;
+    if (b[p] == '>') {
+      Point n = p;
+      n.x = (n.x + 1) % b.width();
+      if (b[n] == '.') {
+        out[n] = '>';
+        out[p] = '.';
+        skip.insert(n);
+      } else {
+        out[p] = b[p];
+      }
+    } else {
+      out[p] = b[p];
+    }
+  }
+  return out;
+}
+absl::StatusOr<CharBoard> UpdateSouth(const CharBoard& b) {
+  CharBoard out(b.width(), b.height());
+  absl::flat_hash_set<Point> skip;
+  for (Point p : b.range()) {
+    if (skip.contains(p)) continue;
+    if (b[p] == 'v') {
+      Point n = p;
+      n.y = (n.y + 1) % b.height();
+      if (b[n] == '.') {
+        out[n] = 'v';
+        out[p] = '.';
+        skip.insert(n);
+      } else {
+        out[p] = b[p];
+      }
+    } else {
+      out[p] = b[p];
+    }
+  }
+  return out;
+}
 
 }  // namespace
 
 absl::StatusOr<std::string> Day_2021_25::Part1(
     absl::Span<absl::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  absl::StatusOr<CharBoard> b = CharBoard::Parse(input);
+  if (!b.ok()) return b.status();
+
+  for (int i = 1; true; ++i) {
+    absl::StatusOr<CharBoard> new_b = UpdateEast(*b);
+    if (!new_b.ok()) return new_b.status();
+    new_b = UpdateSouth(*new_b);
+    if (!new_b.ok()) return new_b.status();
+    if (*new_b == *b) {
+      return IntReturn(i);
+    }
+    b = std::move(*new_b);
+  }
+
+  return Error("Left infinite loop");
 }
 
 absl::StatusOr<std::string> Day_2021_25::Part2(
     absl::Span<absl::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  return "Merry Christmas!";
 }
 
 }  // namespace advent_of_code
