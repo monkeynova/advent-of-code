b25b54beca3df69865d571afb145e30b326cf355
Keith Peters <keith@monkeynova.com>
2022-11-18 15:14:19 -0800

    2016/23 Performance
    Manually decompile the assembunny into equivalent 'C++' code that
    can utilize multiplication (per the hint) which brings the runtime into
    milliseconds provided the assembly matches the expected pattern.

--- advent_of_code/2016/day23/day23.cc
+++ advent_of_code/2016/day23/day23.cc
@@ -12,6 +12,53 @@
 
 namespace advent_of_code {
 
+std::optional<int64_t> MatchesDecompiled(absl::Span<std::string_view> input) {
+  std::vector<absl::string_view> decompilable_res = {
+    "cpy a b",
+    "dec b",
+    "cpy a d",
+    "cpy 0 a",
+    "cpy b c",
+    "inc a",
+    "dec c",
+    "jnz c \\-2",
+    "dec d",
+    "jnz d \\-5",
+    "dec b",
+    "cpy b c",
+    "cpy c d",
+    "dec d",
+    "inc c",
+    "jnz d \\-2",
+    "tgl c",
+    "cpy \\-16 c",
+    "jnz 1 c",
+    "cpy (\\d+) c",
+    "jnz (\\d+) d",
+    "inc a",
+    "inc d",
+    "jnz d \\-2",
+    "inc c",
+    "jnz c \\-5"
+  };
+
+  std::string full_re = absl::StrJoin(decompilable_res, "\n");
+
+  int64_t p1, p2;
+  if (!RE2::FullMatch(absl::StrJoin(input, "\n"), full_re, &p1, &p2)) {
+    LOG(WARNING) << "Could not match re=\n" << full_re;
+    LOG(WARNING) << "Against input=\n" << absl::StrJoin(input, "\n");
+    return std::nullopt;
+  }
+
+  int64_t a = 12;
+  for (int64_t b = a - 1; b > 0; --b) {
+    a *= b;
+  }
+  a += p1 * p2;
+  return a;
+}
+
 absl::StatusOr<std::string> Day_2016_23::Part1(
     absl::Span<absl::string_view> input) const {
   absl::StatusOr<AssemBunny> vm = AssemBunny::Parse(input);
@@ -23,6 +70,13 @@ absl::StatusOr<std::string> Day_2016_23::Part1(
 
 absl::StatusOr<std::string> Day_2016_23::Part2(
     absl::Span<absl::string_view> input) const {
+
+  if (std::optional<int64_t> ret = MatchesDecompiled(input)) {
+    return IntReturn(ret);
+  }
+
+  LOG(ERROR) << "Falling back to full execution...";
+
   absl::StatusOr<AssemBunny> vm = AssemBunny::Parse(input);
   if (!vm.ok()) return vm.status();
   vm->registers().a = 12;
