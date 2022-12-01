829f9da7057a9a17e5d6922c192f500a52464c4a
Keith Peters <keith@monkeynova.com>
2022-11-30 21:18:04 -0800

    2022/02 - Empty

---
+++ advent_of_code/2022/day02/day02.cc
@@ -0,0 +1,33 @@
+// http://adventofcode.com/2022/day/02
+
+#include "advent_of_code/2022/day02/day02.h"
+
+#include "absl/algorithm/container.h"
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/strings/numbers.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_join.h"
+#include "absl/strings/str_split.h"
+#include "glog/logging.h"
+#include "re2/re2.h"
+
+namespace advent_of_code {
+
+namespace {
+
+// Helper methods go here.
+
+}  // namespace
+
+absl::StatusOr<std::string> Day_2022_02::Part1(
+    absl::Span<absl::string_view> input) const {
+  return absl::UnimplementedError("Problem not known");
+}
+
+absl::StatusOr<std::string> Day_2022_02::Part2(
+    absl::Span<absl::string_view> input) const {
+  return absl::UnimplementedError("Problem not known");
+}
+
+}  // namespace advent_of_code
