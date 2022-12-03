31c6b04f504bd9a29fe78777eab568ea79c57eaa
Keith Peters <keith@monkeynova.com>
2022-12-02 21:09:03 -0800

    2022/03 Answers

--- advent_of_code/2022/day03/day03.cc
+++ advent_of_code/2022/day03/day03.cc
@@ -22,12 +22,54 @@ namespace {
 
 absl::StatusOr<std::string> Day_2022_03::Part1(
     absl::Span<absl::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  int score = 0;
+  for (absl::string_view line : input) {
+    std::vector<int> counts(256);
+    for (int i = 0; i < line.size() / 2; ++i) {
+      ++counts[line[i]];
+    }
+    for (int i = line.size() / 2; i < line.size(); ++i) {
+      if (counts[line[i]]) {
+        if (line[i] >= 'A' && line[i] <= 'Z')
+          score += line[i] - 'A' + 27;
+        else if (line[i] >= 'a' && line[i] <= 'z')
+          score += line[i] - 'a' + 1;
+        else Error("Bad line");
+        break;
+      }
+    }
+  }
+  return IntReturn(score);
 }
 
 absl::StatusOr<std::string> Day_2022_03::Part2(
     absl::Span<absl::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  int score = 0;
+  for (int i = 0; i < input.size(); i += 3) {
+    absl::flat_hash_set<char> a_set;
+    absl::flat_hash_set<char> b_set;
+    absl::flat_hash_set<char> c_set;
+    for (int j = 0; j < input[i].size(); ++j) {
+      a_set.insert(input[i][j]);
+    }
+    for (int j = 0; j < input[i+1].size(); ++j) {
+      b_set.insert(input[i+1][j]);
+    }
+    for (int j = 0; j < input[i+2].size(); ++j) {
+      c_set.insert(input[i+2][j]);
+    }
+    for (char c : a_set) {
+      if (b_set.contains(c) && c_set.contains(c)) {
+        if (c >= 'A' && c <= 'Z')
+          score += c - 'A' + 27;
+        else if (c >= 'a' && c <= 'z')
+          score += c - 'a' + 1;
+        else Error("Bad line");
+        break;
+      }
+    }
+  }
+  return IntReturn(score);
 }
 
 }  // namespace advent_of_code
