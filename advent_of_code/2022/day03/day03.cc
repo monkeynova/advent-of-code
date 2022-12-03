3b4e09a494c9ea067d20db9dec2571d04eacdfba
Keith Peters <keith@monkeynova.com>
2022-12-03 08:25:47 -0800

    2022/03 Futzing

--- advent_of_code/2022/day03/day03.cc
+++ advent_of_code/2022/day03/day03.cc
@@ -14,63 +14,67 @@
 
 namespace advent_of_code {
 
+namespace {
+
+absl::StatusOr<char> UniqueCheck(absl::StatusOr<char> cur, char v) {
+  if (cur.status().code() == absl::StatusCode::kNotFound) return v;
+  if (!cur.ok()) return cur;
+  if (*cur != v) {
+    return Error(absl::StrFormat("Not the same: %c != %c", *cur, v));
+  }
+  return v;
+}
+
+absl::StatusOr<int> Score(char c) {
+  if (c >= 'A' && c <= 'Z') return c - 'A' + 27;
+  if (c >= 'a' && c <= 'z') return c - 'a' + 1;
+  return Error("Bad line");
+}
+
+}  // namespace
+
 absl::StatusOr<std::string> Day_2022_03::Part1(
     absl::Span<absl::string_view> input) const {
-  int score = 0;
+  int total_score = 0;
   for (absl::string_view line : input) {
     if (line.size() % 2 != 0) return Error("Line size isn't even");
 
-    std::vector<int> counts(256, 0);
-    for (int i = 0; i < line.size() / 2; ++i) {
-      ++counts[line[i]];
+    std::string_view first = line.substr(0, line.size() / 2);
+    std::string_view second = line.substr(line.size() / 2);
+
+    absl::flat_hash_set<char> set(first.begin(), first.end());
+    absl::StatusOr<char> found = absl::NotFoundError("Bad line: no duplicate");
+    for (char c : second) {
+      if (!set.contains(c)) continue;
+      found = UniqueCheck(found, c);
     }
-    std::optional<char> found;
-    for (int i = line.size() / 2; i < line.size(); ++i) {
-      if (counts[line[i]]) {
-        if (found) {
-          if (*found != line[i]) {
-            return Error("Double result in a line: \"", line, "\"");
-          }
-          continue;
-        }
-        found = line[i];
-        if (line[i] >= 'A' && line[i] <= 'Z') score += line[i] - 'A' + 27;
-        else if (line[i] >= 'a' && line[i] <= 'z') score += line[i] - 'a' + 1;
-        else return Error("Bad char");
-      }
-    }
-    if (!found) return Error("Bad line: no duplicate");
+    if (!found.ok()) return found.status();
+    absl::StatusOr<int> score = Score(*found);
+    if (!score.ok()) return score.status();
+    total_score += *score;
   }
-  return IntReturn(score);
+  return IntReturn(total_score);
 }
 
 absl::StatusOr<std::string> Day_2022_03::Part2(
     absl::Span<absl::string_view> input) const {
-  int score = 0;
+  int total_score = 0;
   if (input.size() % 3 != 0) return Error("Input isn't groups of 3");
   for (int i = 0; i < input.size(); i += 3) {
-    absl::flat_hash_set<char> a_set;
-    absl::flat_hash_set<char> b_set;
-    for (char c : input[i]) a_set.insert(c);
-    for (char c : input[i+1]) b_set.insert(c);
-    std::optional<char> found;
+    absl::flat_hash_set<char> a_set(input[i].begin(), input[i].end());
+    absl::flat_hash_set<char> b_set(input[i+1].begin(), input[i+1].end());
+    absl::StatusOr<char> found = absl::NotFoundError("Bad line: no duplicate");
     for (char c : input[i+2]) {
-      if (a_set.contains(c) && b_set.contains(c)) {
-        if (found) {
-          if (*found != c) {
-            return Error("Multiple duplicates");
-          }
-          continue;
-        }
-        found = c;
-        if (c >= 'A' && c <= 'Z') score += c - 'A' + 27;
-        else if (c >= 'a' && c <= 'z') score += c - 'a' + 1;
-        else return Error("Bad line");
-      }
+      if (!a_set.contains(c)) continue;
+      if (!b_set.contains(c)) continue;
+      found = UniqueCheck(found, c);
     }
-    if (!found) return Error("Bad set: no duplicate");
+    if (!found.ok()) return found.status();
+    absl::StatusOr<int> score = Score(*found);
+    if (!score.ok()) return score.status();
+    total_score += *score;
   }
-  return IntReturn(score);
+  return IntReturn(total_score);
 }
 
 }  // namespace advent_of_code
