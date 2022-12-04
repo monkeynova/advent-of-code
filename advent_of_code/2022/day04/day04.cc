f7aa161919f22cd09413913997ae4d2b5eb91eec
Keith Peters <keith@monkeynova.com>
2022-12-03 21:17:46 -0800

    2022/04 Futzing

--- advent_of_code/2022/day04/day04.cc
+++ advent_of_code/2022/day04/day04.cc
@@ -24,19 +24,13 @@ absl::StatusOr<std::string> Day_2022_04::Part1(
     absl::Span<absl::string_view> input) const {
   int count = 0;
   for (absl::string_view line : input) {
-    auto [p1, p2] = PairSplit(line, ",");
-    auto [p1s_str, p1e_str] = PairSplit(p1, "-");
-    auto [p2s_str, p2e_str] = PairSplit(p2, "-");
     int p1s, p1e, p2s, p2e;
-    if (!absl::SimpleAtoi(p1s_str, &p1s)) return Error("Bad int");
-    if (!absl::SimpleAtoi(p1e_str, &p1e)) return Error("Bad int");
-    if (!absl::SimpleAtoi(p2s_str, &p2s)) return Error("Bad int");
-    if (!absl::SimpleAtoi(p2e_str, &p2e)) return Error("Bad int");
-    if (p1s >= p2s && p1e <= p2e) {
-      ++count;
-    } else if (p2s >= p1s && p2e <= p1e) {
-      ++count;
+    if (!RE2::FullMatch(line, "(\\d+)-(\\d+),(\\d+)-(\\d+)", 
+                        &p1s, &p1e, &p2s, &p2e)) {
+      return Error("Bad line: ", line);
     }
+    if (p1s >= p2s && p1e <= p2e) ++count;
+    else if (p2s >= p1s && p2e <= p1e) ++count;
   }
 
   return IntReturn(count);
@@ -46,14 +40,11 @@ absl::StatusOr<std::string> Day_2022_04::Part2(
     absl::Span<absl::string_view> input) const {
   int count = input.size();
   for (absl::string_view line : input) {
-    auto [p1, p2] = PairSplit(line, ",");
-    auto [p1s_str, p1e_str] = PairSplit(p1, "-");
-    auto [p2s_str, p2e_str] = PairSplit(p2, "-");
     int p1s, p1e, p2s, p2e;
-    if (!absl::SimpleAtoi(p1s_str, &p1s)) return Error("Bad int");
-    if (!absl::SimpleAtoi(p1e_str, &p1e)) return Error("Bad int");
-    if (!absl::SimpleAtoi(p2s_str, &p2s)) return Error("Bad int");
-    if (!absl::SimpleAtoi(p2e_str, &p2e)) return Error("Bad int");
+    if (!RE2::FullMatch(line, "(\\d+)-(\\d+),(\\d+)-(\\d+)", 
+                        &p1s, &p1e, &p2s, &p2e)) {
+      return Error("Bad line: ", line);
+    }
     if (p1e < p2s) --count;
     else if (p2e < p1s) --count;
   }
