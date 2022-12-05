1b9e41e57c2a8390f8e420cf43983205ff94f53e
Keith Peters <keith@monkeynova.com>
2022-12-05 07:17:12 -0800

    2022/05 Futzing
    More morning-after architecture.
    Consolidate parsing. Also some of the changes made me squirrly about
    pointer stability, so unique_ptr them.

--- advent_of_code/2022/day05/day05.cc
+++ advent_of_code/2022/day05/day05.cc
@@ -18,18 +18,20 @@ namespace {
 
 using CargoStack = std::vector<char>;
 
-absl::StatusOr<std::vector<CargoStack>> ParseStacks(
+absl::StatusOr<std::vector<std::unique_ptr<CargoStack>>> ParseStacks(
     absl::Span<absl::string_view> lines) {
-  std::vector<CargoStack> stacks;
+  std::vector<std::unique_ptr<CargoStack>> stacks;
   for (absl::string_view line : lines) {
     if (line.size() % 4 != 3) return Error("Bad line");
     for (int i = 0; 4 * i < line.size(); ++i) {
-      while (stacks.size() <= i) stacks.push_back({});
+      while (stacks.size() <= i) {
+        stacks.push_back(absl::make_unique<CargoStack>());
+      }
       absl::string_view crate = line.substr(4 * i, 3);
       if (crate == "   ") {
-        if (!stacks[i].empty()) return Error("Hole in stack");
+        if (!stacks[i]->empty()) return Error("Hole in stack");
       } else if (crate[0] == '[' && crate[2] == ']') {
-        stacks[i].push_back(crate[1]);
+        stacks[i]->push_back(crate[1]);
       } else if (crate[0] == ' ' && crate[2] == ' ') {
         if (crate[1] != i + '1') return Error("Bad crate (index): ", crate);
       } else {
@@ -38,8 +40,10 @@ absl::StatusOr<std::vector<CargoStack>> ParseStacks(
     }
   }
 
-  for (CargoStack& s : stacks) {
-    absl::c_reverse(s);
+  // Stacks were added top to bottom. We want `back` to reflect the "top" so
+  // reverse each of the stacks.
+  for (std::unique_ptr<CargoStack>& s : stacks) {
+    absl::c_reverse(*s);
   }
   return stacks;
 }
@@ -52,7 +56,7 @@ struct Move {
 
 absl::StatusOr<std::vector<Move>> ParseMoves(
     absl::Span<absl::string_view> moves,
-    std::vector<CargoStack>& stacks) {
+    std::vector<std::unique_ptr<CargoStack>>& stacks) {
   std::vector<Move> ret;
   for (absl::string_view line : moves) {
     int count, from_idx, to_idx;
@@ -62,24 +66,26 @@ absl::StatusOr<std::vector<Move>> ParseMoves(
     }
     if (stacks.size() < from_idx) return Error("Bad move (from) ", from_idx);
     if (stacks.size() < to_idx) return Error("Bad move (to) ", to_idx);
-    ret.push_back(Move{.count = count, .from = stacks[from_idx - 1],
-                       .to = stacks[to_idx - 1]});
+    ret.push_back(Move{.count = count, .from = *stacks[from_idx - 1],
+                       .to = *stacks[to_idx - 1]});
   }
   return ret;
 }
 
-std::string StackTops(const std::vector<CargoStack>& stacks) {
-  std::string ret;
-  for (const CargoStack& s : stacks) {
-    ret.append(1, s.back());
+struct Day {
+  std::vector<std::unique_ptr<CargoStack>> stacks;
+  std::vector<Move> moves;
+
+  std::string Tops() {
+    std::string ret;
+    for (const std::unique_ptr<CargoStack>& s : stacks) {
+      ret.append(1, s->back());
+    }
+    return ret;
   }
-  return ret;
-}
+};
 
-}  // namespace
-
-absl::StatusOr<std::string> Day_2022_05::Part1(
-    absl::Span<absl::string_view> input) const {
+absl::StatusOr<Day> Parse(absl::Span<absl::string_view> input) {
   std::optional<int> split_at;
   for (int i = 0; i < input.size(); ++i) {
     if (input[i].empty()) {
@@ -89,14 +95,23 @@ absl::StatusOr<std::string> Day_2022_05::Part1(
   }
   if (!split_at) return Error("No empty line in input");
 
-  absl::StatusOr<std::vector<CargoStack>> stacks =
+  absl::StatusOr<std::vector<std::unique_ptr<CargoStack>>> stacks =
       ParseStacks(input.subspan(0, *split_at));
   if (!stacks.ok()) return stacks.status();
 
   absl::StatusOr<std::vector<Move>> moves =
       ParseMoves(input.subspan(*split_at + 1), *stacks);
 
-  for (const Move& m : *moves) {
+  return Day{.stacks = std::move(*stacks), .moves = std::move(*moves)};
+}
+
+}  // namespace
+
+absl::StatusOr<std::string> Day_2022_05::Part1(
+    absl::Span<absl::string_view> input) const {
+  absl::StatusOr<Day> day = Parse(input);
+
+  for (const Move& m : day->moves) {
     if (m.from.size() < m.count) return Error("Bad move: empty");
     for (int i = 0; i < m.count; ++i) {
       m.to.push_back(m.from[m.from.size() - i - 1]);
@@ -104,28 +119,14 @@ absl::StatusOr<std::string> Day_2022_05::Part1(
     m.from.resize(m.from.size() - m.count);
   }
 
-  return StackTops(*stacks);
+  return day->Tops();
 }
 
 absl::StatusOr<std::string> Day_2022_05::Part2(
     absl::Span<absl::string_view> input) const {
-  std::optional<int> split_at;
-  for (int i = 0; i < input.size(); ++i) {
-    if (input[i].empty()) {
-      split_at = i;
-      break;
-    }
-  }
-  if (!split_at) return Error("No empty line in input");
+  absl::StatusOr<Day> day = Parse(input);
 
-  absl::StatusOr<std::vector<CargoStack>> stacks =
-      ParseStacks(input.subspan(0, *split_at));
-  if (!stacks.ok()) return stacks.status();
-
-  absl::StatusOr<std::vector<Move>> moves =
-      ParseMoves(input.subspan(*split_at + 1), *stacks);
-
-  for (const Move& m : *moves) {
+  for (const Move& m : day->moves) {
     if (m.from.size() < m.count) return Error("Bad move: empty");
     for (int i = 0; i < m.count; ++i) {
       m.to.push_back(m.from[m.from.size() - m.count + i]);
@@ -133,7 +134,7 @@ absl::StatusOr<std::string> Day_2022_05::Part2(
     m.from.resize(m.from.size() - m.count);
   }
 
-  return StackTops(*stacks);
+  return day->Tops();
 }
 
 }  // namespace advent_of_code
