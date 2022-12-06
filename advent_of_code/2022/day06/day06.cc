143dc57e2f394541e00e2de34c85be2813da97a6
Keith Peters <keith@monkeynova.com>
2022-12-05 21:05:07 -0800

    2022/06 Answer

--- advent_of_code/2022/day06/day06.cc
+++ advent_of_code/2022/day06/day06.cc
@@ -22,11 +22,35 @@ namespace {
 
 absl::StatusOr<std::string> Day_2022_06::Part1(
     absl::Span<absl::string_view> input) const {
+  for (int i = 0; i < input[0].size(); ++i) {
+    bool found = true;
+    absl::flat_hash_set<char> set;
+    for (int j = 0; j < 4; ++j) {
+      if (set.contains(input[0][i + j])) {
+        found = false;
+        break;
+      }
+      set.insert(input[0][i + j]);
+    }
+    if (found) return IntReturn(i + 4);
+  }
   return absl::UnimplementedError("Problem not known");
 }
 
 absl::StatusOr<std::string> Day_2022_06::Part2(
     absl::Span<absl::string_view> input) const {
+  for (int i = 0; i < input[0].size(); ++i) {
+    bool found = true;
+    absl::flat_hash_set<char> set;
+    for (int j = 0; j < 14; ++j) {
+      if (set.contains(input[0][i + j])) {
+        found = false;
+        break;
+      }
+      set.insert(input[0][i + j]);
+    }
+    if (found) return IntReturn(i + 14);
+  }
   return absl::UnimplementedError("Problem not known");
 }
 
