e6fc8b0fa6f6bde7f89db0dd875da6a31fd5cb99
Keith Peters <keith@monkeynova.com>
2022-12-07 09:15:07 -0800

    :cleanup

--- advent_of_code/2015/day18/day18.cc
+++ advent_of_code/2015/day18/day18.cc
@@ -2,50 +2,50 @@
 //
 // --- Day 18: Like a GIF For Your Yard ---
 // ----------------------------------------
-// 
+//
 // After the million lights incident, the fire code has gotten stricter:
 // now, at most ten thousand lights are allowed. You arrange them in a
 // 100x100 grid.
-// 
+//
 // Never one to let you down, Santa again mails you instructions on the
 // ideal lighting configuration. With so few lights, he says, you'll have
 // to resort to animation.
-// 
+//
 // Start by setting your lights to the included initial configuration
 // (your puzzle input). A # means "on", and a . means "off".
-// 
+//
 // Then, animate your grid in steps, where each step decides the next
 // configuration based on the current one. Each light's next state
 // (either on or off) depends on its current state and the current states
 // of the eight lights adjacent to it (including diagonals). Lights on
 // the edge of the grid might have fewer than eight neighbors; the
 // missing ones always count as "off".
-// 
+//
 // For example, in a simplified 6x6 grid, the light marked A has the
 // neighbors numbered 1 through 8, and the light marked B, which is on an
 // edge, only has the neighbors marked 1 through 5:
-// 
+//
 // 1B5...
 // 234...
 // ......
 // ..123.
 // ..8A4.
 // ..765.
-// 
+//
 // The state a light should have next is based on its current state (on
 // or off) plus the number of neighbors that are on:
-// 
+//
 // * A light which is on stays on when 2 or 3 neighbors are on, and
 // turns off otherwise.
-// 
+//
 // * A light which is off turns on if exactly 3 neighbors are on, and
 // stays off otherwise.
-// 
+//
 // All of the lights update simultaneously; they all consider the same
 // current state before moving to the next.
-// 
+//
 // Here's a few steps from an example configuration of another 6x6 grid:
-// 
+//
 // Initial state:
 // .#.#.#
 // ...##.
@@ -53,7 +53,7 @@
 // ..#...
 // #.#..#
 // ####..
-// 
+//
 // After 1 step:
 // ..##..
 // ..##.#
@@ -61,7 +61,7 @@
 // ......
 // #.....
 // #.##..
-// 
+//
 // After 2 steps:
 // ..###.
 // ......
@@ -69,7 +69,7 @@
 // ......
 // .#....
 // .#....
-// 
+//
 // After 3 steps:
 // ...#..
 // ......
@@ -77,7 +77,7 @@
 // ..##..
 // ......
 // ......
-// 
+//
 // After 4 steps:
 // ......
 // ......
@@ -85,21 +85,21 @@
 // ..##..
 // ......
 // ......
-// 
+//
 // After 4 steps, this example has four lights on.
-// 
+//
 // In your grid of 100x100 lights, given your initial configuration, how
 // many lights are on after 100 steps?
 //
 // --- Part Two ---
 // ----------------
-// 
+//
 // You flip the instructions over; Santa goes on to point out that this
 // is all just an implementation of Conway's Game of Life. At least, it
 // was, until you notice that something's wrong with the grid of lights
 // you bought: four lights, one in each corner, are stuck on and can't be
 // turned off. The example above will actually run like this:
-// 
+//
 // Initial state:
 // ##.#.#
 // ...##.
@@ -107,7 +107,7 @@
 // ..#...
 // #.#..#
 // ####.#
-// 
+//
 // After 1 step:
 // #.##.#
 // ####.#
@@ -115,7 +115,7 @@
 // ......
 // #...#.
 // #.####
-// 
+//
 // After 2 steps:
 // #..#.#
 // #....#
@@ -123,7 +123,7 @@
 // ...##.
 // .#..##
 // ##.###
-// 
+//
 // After 3 steps:
 // #...##
 // ####.#
@@ -131,7 +131,7 @@
 // ......
 // ##....
 // ####.#
-// 
+//
 // After 4 steps:
 // #.####
 // #....#
@@ -139,7 +139,7 @@
 // .##...
 // #.....
 // #.#..#
-// 
+//
 // After 5 steps:
 // ##.###
 // .##..#
@@ -147,24 +147,23 @@
 // .##...
 // #.#...
 // ##...#
-// 
+//
 // After 5 steps, this example now has 17 lights on.
-// 
+//
 // In your grid of 100x100 lights, given your initial configuration, but
 // with the four corners always in the on state, how many lights are on
 // after 100 steps?
-
 
 #include "advent_of_code/2015/day18/day18.h"
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
+#include "absl/log/log.h"
 #include "absl/strings/numbers.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
 #include "absl/strings/str_split.h"
 #include "advent_of_code/conway.h"
-#include "absl/log/log.h"
 #include "re2/re2.h"
 
 namespace advent_of_code {
--- advent_of_code/2015/day24/day24.cc
+++ advent_of_code/2015/day24/day24.cc
@@ -2,14 +2,14 @@
 //
 // --- Day 24: It Hangs in the Balance ---
 // ---------------------------------------
-// 
+//
 // It's Christmas Eve, and Santa is loading up the sleigh for this year's
 // deliveries. However, there's one small problem: he can't get the
 // sleigh to balance. If it isn't balanced, he can't defy physics, and
 // nobody gets presents this year.
-// 
+//
 // No pressure.
-// 
+//
 // Santa has provided you a list of the weights of every package he needs
 // to fit on the sleigh. The packages need to be split into three groups
 // of exactly the same weight, and every package has to fit. The first
@@ -17,13 +17,13 @@
 // and third go in containers on either side. Only when all three groups
 // weigh exactly the same amount will the sleigh be able to fly. Defying
 // physics has rules, you know!
-// 
+//
 // Of course, that's not the only problem. The first group - the one
 // going in the passenger compartment - needs as few packages as possible
 // so that Santa has some legroom left over. It doesn't matter how many
 // packages are in either of the other two groups, so long as all of the
 // groups weigh the same.
-// 
+//
 // Furthermore, Santa tells you, if there are multiple ways to arrange
 // the packages such that the fewest possible are in the first group, you
 // need to choose the way where the first group has the smallest quantum
@@ -33,12 +33,12 @@
 // Only consider quantum entanglement if the first group has the fewest
 // possible number of packages in it and all groups weigh the same
 // amount.
-// 
+//
 // For example, suppose you have ten packages with weights 1 through 5
 // and 7 through 11. For this situation, some of the unique first groups,
 // their quantum entanglements, and a way to divide the remaining
 // packages are as follows:
-// 
+//
 // Group 1;             Group 2; Group 3
 // 11 9       (QE= 99); 10 8 2;  7 5 4 3 1
 // 10 9 1     (QE= 90); 11 7 2;  8 5 4 3
@@ -53,55 +53,54 @@
 // 8 7 5      (QE=280); 11 9;    10 4 3 2 1
 // 8 5 4 3    (QE=480); 11 9;    10 7 2 1
 // 7 5 4 3 1  (QE=420); 11 9;    10 8 2
-// 
+//
 // Of these, although 10 9 1 has the smallest quantum entanglement (90),
 // the configuration with only two packages, 11 9, in the passenger
 // compartment gives Santa the most legroom and wins. In this situation,
 // the quantum entanglement for the ideal configuration is therefore 99.
 // Had there been two configurations with only two packages in the first
 // group, the one with the smaller quantum entanglement would be chosen.
-// 
+//
 // What is the quantum entanglement of the first group of packages in the
 // ideal configuration?
 //
 // --- Part Two ---
 // ----------------
-// 
+//
 // That's weird... the sleigh still isn't balancing.
-// 
+//
 // "Ho ho ho", Santa muses to himself. "I forgot the trunk".
-// 
+//
 // Balance the sleigh again, but this time, separate the packages into
 // four groups instead of three. The other constraints still apply.
-// 
+//
 // Given the example packages above, this would be some of the new unique
 // first groups, their quantum entanglements, and one way to divide the
 // remaining packages:
-// 
+//
 // 11 4    (QE=44); 10 5;   9 3 2 1; 8 7
 // 10 5    (QE=50); 11 4;   9 3 2 1; 8 7
 // 9 5 1   (QE=45); 11 4;   10 3 2;  8 7
 // 9 4 2   (QE=72); 11 3 1; 10 5;    8 7
 // 9 3 2 1 (QE=54); 11 4;   10 5;    8 7
 // 8 7     (QE=56); 11 4;   10 5;    9 3 2 1
-// 
+//
 // Of these, there are three arrangements that put the minimum (two)
 // number of packages in the first group: 11 4, 10 5, and 8 7. Of these,
 // 11 4 has the lowest quantum entanglement, and so it is selected.
-// 
+//
 // Now, what is the quantum entanglement of the first group of packages
 // in the ideal configuration?
-
 
 #include "advent_of_code/2015/day24/day24.h"
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
+#include "absl/log/log.h"
 #include "absl/strings/numbers.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
 #include "absl/strings/str_split.h"
-#include "absl/log/log.h"
 #include "re2/re2.h"
 
 namespace advent_of_code {
