6453dcace2e66011ed72e37e183299ef30e27187
Keith Peters <keith@monkeynova.com>
2022-12-07 21:09:19 -0800

    2022/08 Answer

--- advent_of_code/2022/day08/day08.cc
+++ advent_of_code/2022/day08/day08.cc
@@ -22,12 +22,51 @@ namespace {
 
 absl::StatusOr<std::string> Day_2022_08::Part1(
     absl::Span<absl::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  auto board = ParseAsBoard(input);
+  if (!board.ok()) return board.status();
+
+  int count = 0;
+  for (Point p : board->range()) {
+    bool visible_any = false;
+    for (Point d : Cardinal::kFourDirs) {
+      bool visible_this = true;
+      for (Point t = p + d; board->OnBoard(t); t += d) {
+        if ((*board)[t] >= (*board)[p]) {
+          visible_this = false;
+          break;
+        }
+      }
+      if (visible_this) {
+        visible_any = true;
+        break;
+      }
+    }
+    if (visible_any) ++count;
+  }
+  return IntReturn(count);
 }
 
 absl::StatusOr<std::string> Day_2022_08::Part2(
     absl::Span<absl::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  auto board = ParseAsBoard(input);
+  if (!board.ok()) return board.status();
+
+  int best_score = 0;
+  for (Point p : board->range()) {
+    int score = 1;
+    for (Point d : Cardinal::kFourDirs) {
+      int dist = 0;
+      for (Point t = p + d; board->OnBoard(t); t += d) {
+        ++dist;
+        if ((*board)[t] >= (*board)[p]) {
+          break;
+        }
+      }
+      score *= dist;
+    }
+    best_score = std::max(best_score, score);
+  }
+  return IntReturn(best_score);
 }
 
 }  // namespace advent_of_code
