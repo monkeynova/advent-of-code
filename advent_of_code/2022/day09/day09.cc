41dcc6cbbf118dc17930c7d37e82cf489234bc99
Keith Peters <keith@monkeynova.com>
2022-12-08 21:35:43 -0800

    2022/09 Cleanup

--- advent_of_code/2022/day09/day09.cc
+++ advent_of_code/2022/day09/day09.cc
@@ -14,79 +14,68 @@ namespace advent_of_code {
 
 namespace {
 
-Point UpdateTail(Point head, Point tail) {
-    Point d = head - tail;
-    if (d.x == 2 && d.y == 0) ++tail.x;
-    else if (d.x == -2 && d.y == 0) --tail.x;
-    else if (d.y == 2 && d.x == 0) ++tail.y;
-    else if (d.y == -2 && d.x == 0) --tail.y;
-    else if (d.dist() > 2) {
-      d.x /= abs(d.x);
-      d.y /= abs(d.y);
-      tail += d;
+Point DragPoint(Point head, Point tail) {
+  static absl::flat_hash_map<Point, Point> dragdir = {
+    {Point{2, 0}, Point{1, 0}},
+    {Point{-2, 0}, Point{-1, 0}},
+    {Point{0, 2}, Point{0, 1}},
+    {Point{0, -2}, Point{0, -1}},
+
+    {Point{2, 1}, Point{1, 1}},
+    {Point{1, 2}, Point{1, 1}},
+    {Point{2, -1}, Point{1, -1}},
+    {Point{1, -2}, Point{1, -1}},
+    {Point{-2, 1}, Point{-1, 1}},
+    {Point{-1, 2}, Point{-1, 1}},
+    {Point{-2, -1}, Point{-1, -1}},
+    {Point{-1, -2}, Point{-1, -1}},
+  };
+  auto it = dragdir.find(head - tail);
+  if (it == dragdir.end()) {
+    CHECK(d.dist() < 2);
+    return tail;
+  }
+  return tail + it->second;
+}
+
+absl::StatusOr<int> DragRope(
+    absl::Span<absl::string_view> input, int ropesize) {
+  std::vector<Point> rope(ropesize, Point{0, 0});
+  absl::flat_hash_set<Point> tail_seen = {rope.back()};
+  static absl::flat_hash_map<char, Point> char2dir = {
+    {'U', Cardinal::kNorth}, {'D', Cardinal::kSouth}, {'L', Cardinal::kWest},
+    {'R', Cardinal::kEast},
+  };
+  for (absl::string_view line : input) {
+    char dir_char;
+    int dist;
+    if (!RE2::FullMatch(line, "([RULD]) (\\d+)", &dir_char, &dist)) {
+      return Error("Bad line: ", line);
     }
-  return tail;
+    CHECK(char2dir.contains(dir_char));
+    Point head_dir = char2dir[dir_char];
+
+    for (int i = 0; i < dist; ++i) {
+      rope[0] += head_dir;
+      for (int i = 1; i < rope.size(); ++i) {
+        rope[i] = DragPoint(rope[i - 1], rope[i]);
+      }
+      tail_seen.insert(rope.back());
+    }
+  }
+  return tail_seen.size();
 }
 
 }  // namespace
 
 absl::StatusOr<std::string> Day_2022_09::Part1(
     absl::Span<absl::string_view> input) const {
-  Point tail = {0, 0};
-  Point head = {0, 0};
-  absl::flat_hash_set<Point> tail_seen = {tail};
-  for (absl::string_view line : input) {
-    absl::string_view dir;
-    int dist;
-    if (!RE2::FullMatch(line, "([RULD]) (\\d+)", &dir, &dist)) {
-      return Error("Bad line: ", line);
-    }
-    for (int i = 0; i < dist; ++i) {
-      if (dir == "U") head = head + Cardinal::kNorth;
-      else if (dir == "D") head = head + Cardinal::kSouth;
-      else if (dir == "L") head = head + Cardinal::kWest;
-      else if (dir == "R") head = head + Cardinal::kEast;
-      else return Error("Bad dir!");
-  
-      tail = UpdateTail(head, tail);
-      //VLOG(1) << "H: " << head;
-      //VLOG(1) << "T: " << tail;
-      tail_seen.insert(tail);
-    }
-  }
-  return IntReturn(tail_seen.size());
+  return IntReturn(DragRope(input, 2));
 }
 
 absl::StatusOr<std::string> Day_2022_09::Part2(
     absl::Span<absl::string_view> input) const {
-  Point tail = {0, 0};
-  std::vector<Point> mid(8, Point{0, 0});
-  Point head = {0, 0};
-  absl::flat_hash_set<Point> tail_seen = {Point{0, 0}};
-  for (absl::string_view line : input) {
-    absl::string_view dir;
-    int dist;
-    if (!RE2::FullMatch(line, "([RULD]) (\\d+)", &dir, &dist)) {
-      return Error("Bad line: ", line);
-    }
-    for (int i = 0; i < dist; ++i) {
-      if (dir == "U") head = head + Cardinal::kNorth;
-      else if (dir == "D") head = head + Cardinal::kSouth;
-      else if (dir == "L") head = head + Cardinal::kWest;
-      else if (dir == "R") head = head + Cardinal::kEast;
-      else return Error("Bad dir!");
-  
-      mid[0] = UpdateTail(head, mid[0]);
-      for (int i = 1; i < 8; ++i) {
-        mid[i] = UpdateTail(mid[i - 1], mid[i]);
-      }
-      tail = UpdateTail(mid.back(), tail);
-      //VLOG(1) << "H: " << head;
-      //VLOG(1) << "T: " << tail;
-      tail_seen.insert(tail);
-    }
-  }
-  return IntReturn(tail_seen.size());
+  return IntReturn(DragRope(input, 10));
 }
 
 }  // namespace advent_of_code
