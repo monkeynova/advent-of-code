92aa6a470c9c8c224d66dacaf0f3139e7af47a64
Keith Peters <keith@monkeynova.com>
2022-12-08 21:22:40 -0800

    2022/09 Answer
    This time with mid-contest refactoring!

--- advent_of_code/2022/day09/day09.cc
+++ advent_of_code/2022/day09/day09.cc
@@ -14,18 +14,79 @@ namespace advent_of_code {
 
 namespace {
 
-// Helper methods go here.
+Point UpdateTail(Point head, Point tail) {
+    Point d = head - tail;
+    if (d.x == 2 && d.y == 0) ++tail.x;
+    else if (d.x == -2 && d.y == 0) --tail.x;
+    else if (d.y == 2 && d.x == 0) ++tail.y;
+    else if (d.y == -2 && d.x == 0) --tail.y;
+    else if (d.dist() > 2) {
+      d.x /= abs(d.x);
+      d.y /= abs(d.y);
+      tail += d;
+    }
+  return tail;
+}
 
 }  // namespace
 
 absl::StatusOr<std::string> Day_2022_09::Part1(
     absl::Span<absl::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  Point tail = {0, 0};
+  Point head = {0, 0};
+  absl::flat_hash_set<Point> tail_seen = {tail};
+  for (absl::string_view line : input) {
+    absl::string_view dir;
+    int dist;
+    if (!RE2::FullMatch(line, "([RULD]) (\\d+)", &dir, &dist)) {
+      return Error("Bad line: ", line);
+    }
+    for (int i = 0; i < dist; ++i) {
+      if (dir == "U") head = head + Cardinal::kNorth;
+      else if (dir == "D") head = head + Cardinal::kSouth;
+      else if (dir == "L") head = head + Cardinal::kWest;
+      else if (dir == "R") head = head + Cardinal::kEast;
+      else return Error("Bad dir!");
+  
+      tail = UpdateTail(head, tail);
+      //VLOG(1) << "H: " << head;
+      //VLOG(1) << "T: " << tail;
+      tail_seen.insert(tail);
+    }
+  }
+  return IntReturn(tail_seen.size());
 }
 
 absl::StatusOr<std::string> Day_2022_09::Part2(
     absl::Span<absl::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  Point tail = {0, 0};
+  std::vector<Point> mid(8, Point{0, 0});
+  Point head = {0, 0};
+  absl::flat_hash_set<Point> tail_seen = {Point{0, 0}};
+  for (absl::string_view line : input) {
+    absl::string_view dir;
+    int dist;
+    if (!RE2::FullMatch(line, "([RULD]) (\\d+)", &dir, &dist)) {
+      return Error("Bad line: ", line);
+    }
+    for (int i = 0; i < dist; ++i) {
+      if (dir == "U") head = head + Cardinal::kNorth;
+      else if (dir == "D") head = head + Cardinal::kSouth;
+      else if (dir == "L") head = head + Cardinal::kWest;
+      else if (dir == "R") head = head + Cardinal::kEast;
+      else return Error("Bad dir!");
+  
+      mid[0] = UpdateTail(head, mid[0]);
+      for (int i = 1; i < 8; ++i) {
+        mid[i] = UpdateTail(mid[i - 1], mid[i]);
+      }
+      tail = UpdateTail(mid.back(), tail);
+      //VLOG(1) << "H: " << head;
+      //VLOG(1) << "T: " << tail;
+      tail_seen.insert(tail);
+    }
+  }
+  return IntReturn(tail_seen.size());
 }
 
 }  // namespace advent_of_code
