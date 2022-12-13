1f60644b7d88172c138ae55203ac8213d89a765c
Keith Peters <keith@monkeynova.com>
2022-12-13 00:52:33 -0500

    2022/13 Night-of futzing

--- advent_of_code/2022/day13/day13.cc
+++ advent_of_code/2022/day13/day13.cc
@@ -30,30 +30,37 @@ struct Cmp {
     return o << "]";
   }
 
+  bool operator==(const Cmp& o) const {
+    return cmp(o) == 0;
+  }
+
   bool operator<(const Cmp& o) const {
-    VLOG(2) << *this << " <?< " << o;
+    return cmp(o) < 0;
+  }
+
+  int cmp(const Cmp& o) const {
     if (val != -1 && o.val != -1) {
-      return val < o.val;
+      return val < o.val ? -1 : val > o.val ? 1 : 0;
     }
     if (val != -1) {
       Cmp list_val;
       list_val.list.push_back(absl::make_unique<Cmp>());
       list_val.list.back()->val = val;
-      return list_val < o;
+      return list_val.cmp(o);
     }
     if (o.val != -1) {
       Cmp list_val;
       list_val.list.push_back(absl::make_unique<Cmp>());
       list_val.list.back()->val = o.val;
-      return *this < list_val;
+      return cmp(list_val);
     }
     for (int i = 0; i < list.size(); ++i) {
-      if (i >= o.list.size()) return false;
-      if (*list[i] < *o.list[i]) return true;
-      if (*o.list[i] < *list[i]) return false;
+      if (i >= o.list.size()) return 1;
+      int sub_cmp = list[i]->cmp(*o.list[i]);
+      if (sub_cmp != 0) return sub_cmp;
     }
-    if (o.list.size() > list.size()) return true;
-    return false;
+    if (o.list.size() > list.size()) return -1;
+    return 0;
   }
 };
 
@@ -62,11 +69,12 @@ std::unique_ptr<Cmp> Parse(absl::string_view& line) {
   if (line[0] == '[') {
     line = line.substr(1);
     while (true) {
+      if (line[0] == ']') {
+        line = line.substr(1);
+        break;
+      }
       ret->list.push_back(Parse(line));
-      char c = line[0];
-      line = line.substr(1);
-      if (c == ']') break;
-      CHECK_EQ(c, ',');
+      if (line[0] == ',') line = line.substr(1);
     }
   } else if (line[0] >= '0' && line[0] <= '9') {
     ret->val = 0;
@@ -78,6 +86,11 @@ std::unique_ptr<Cmp> Parse(absl::string_view& line) {
   return ret;
 }
 
+bool ByPointerLt(const std::unique_ptr<Cmp>& a,
+                 const std::unique_ptr<Cmp>& b) {
+  return *a < *b;
+}
+
 }  // namespace
 
 absl::StatusOr<std::string> Day_2022_13::Part1(
@@ -85,53 +98,36 @@ absl::StatusOr<std::string> Day_2022_13::Part1(
   int ret = 0;
   for (int i = 0; i < input.size(); i += 3) {
     if (!input[i+2].empty()) return Error("Bad input: empty");
-    auto left = Parse(input[i]);
-    if (!input[i].empty()) return Error("Bad line: ", input[i]);
-    auto right = Parse(input[i + 1]);
-    if (!input[i + 1].empty()) return Error("Bad line: ", input[i + 1]);
-    if (!(*right < *left)) {
-      VLOG(2) << "Found: " << (i/3) + 1;
-      ret += (i/3) + 1;
-    }
+    absl::string_view consume;
+    auto left = Parse(consume = input[i]);
+    if (!consume.empty()) return Error("Bad line: ", input[i]);
+    auto right = Parse(consume = input[i+1]);
+    if (!consume.empty()) return Error("Bad line: ", input[i + 1]);
+    if (!(*right < *left)) ret += (i/3) + 1;
   }
   return IntReturn(ret);
 }
 
 absl::StatusOr<std::string> Day_2022_13::Part2(
     absl::Span<absl::string_view> input) const {
-  int ret = 0;
-  std::unique_ptr<Cmp> l1 = absl::make_unique<Cmp>();
-  l1->list.push_back(absl::make_unique<Cmp>());
-  l1->list.back()->list.push_back(absl::make_unique<Cmp>());
-  l1->list.back()->list.back()->val = 2;
-  std::unique_ptr<Cmp> l2 = absl::make_unique<Cmp>();
-  l2->list.push_back(absl::make_unique<Cmp>());
-  l2->list.back()->list.push_back(absl::make_unique<Cmp>());
-  l2->list.back()->list.back()->val = 6;
-
   std::vector<std::unique_ptr<Cmp>> list;
-  list.push_back(std::move(l1));
-  list.push_back(std::move(l2));
+  absl::string_view consume;
+  list.push_back(Parse(consume = "[[2]]"));
+  list.push_back(Parse(consume = "[[6]]"));
 
   for (absl::string_view line : input) {
     if (line.empty()) continue;
     list.push_back(Parse(line));
   }
-  absl::c_sort(list, [](const std::unique_ptr<Cmp>& a, const std::unique_ptr<Cmp>& b) { return *a < *b; });
+  absl::c_sort(list, ByPointerLt);
 
-  std::unique_ptr<Cmp> f1 = absl::make_unique<Cmp>();
-  f1->list.push_back(absl::make_unique<Cmp>());
-  f1->list.back()->list.push_back(absl::make_unique<Cmp>());
-  f1->list.back()->list.back()->val = 2;
-  std::unique_ptr<Cmp> f2 = absl::make_unique<Cmp>();
-  f2->list.push_back(absl::make_unique<Cmp>());
-  f2->list.back()->list.push_back(absl::make_unique<Cmp>());
-  f2->list.back()->list.back()->val = 6;
+  std::unique_ptr<Cmp> f1 = Parse(consume = "[[2]]");
+  std::unique_ptr<Cmp> f2 = Parse(consume = "[[6]]");
 
   int idx1 = -1, idx2 = -1;
   for (int i = 0; i < list.size(); ++i) {
-    if (!(*list[i] < *f1) && !(*f1 < *list[i])) idx1 = i + 1;
-    if (!(*list[i] < *f2) && !(*f2 < *list[i])) idx2 = i + 1;
+    if (*list[i] == *f1) idx1 = i + 1;
+    if (*list[i] == *f2) idx2 = i + 1;
   }
   return IntReturn(idx1 * idx2);
 }
