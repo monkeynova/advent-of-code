e8a7ea059ce94c8fe1d3c1cbd705f3c5bd439ae7
Keith Peters <keith@monkeynova.com>
2022-12-14 00:42:07 -0500

    2022/14 Night-of Futzing

--- advent_of_code/2022/day14/day14.cc
+++ advent_of_code/2022/day14/day14.cc
@@ -16,20 +16,62 @@ namespace advent_of_code {
 
 namespace {
 
+constexpr Point kAddAt = {500, 0};
+
+absl::StatusOr<PointRectangle> FindGrid(absl::Span<absl::string_view> input) {
+  PointRectangle grid{kAddAt, kAddAt};
+  for (absl::string_view line : input) {
+    for (absl::string_view p_str : absl::StrSplit(line, " -> ")) {
+      Point p;
+      if (!Point::RE2Parse(p_str.data(), p_str.size(), &p)) {
+        return Error("Bad point");
+      } 
+      grid.ExpandInclude(p);
+    }
+  }
+  return grid;
+}
+
+absl::StatusOr<CharBoard> DrawGrid(PointRectangle grid,
+                                   absl::Span<absl::string_view> input) {
+  CharBoard b(grid.max.x - grid.min.x + 1, grid.max.y - grid.min.y + 1);
+  for (absl::string_view line : input) {
+    std::optional<Point> last;
+    for (absl::string_view p_str : absl::StrSplit(line, " -> ")) {
+      Point p;
+      if (!Point::RE2Parse(p_str.data(), p_str.size(), &p)) {
+        return Error("Bad point");
+      } 
+      if (last) {
+        Point d = (p - *last).min_step();
+        if (d.dist() != 1) return Error("Bad d");
+        for (Point t = *last; t != p; t += d) {
+          b[t - grid.min] = '#';
+        }
+      }
+      b[p - grid.min] = '#';
+      last = p;
+    }
+  }
+  return b;
+}
+
 bool AddSand(CharBoard& b, Point at) {
+  CHECK(b.OnBoard(at));
   if (b[at] != '.') return false;
-  while (true) {
-    CHECK(b.OnBoard(at));
-    if (!b.OnBoard(at + Cardinal::kSouth)) {
-      return false;
-    } if (b[at + Cardinal::kSouth] == '.') {
-      at = at + Cardinal::kSouth;
-    } else if (b[at + Cardinal::kSouthWest] == '.') {
-      at = at + Cardinal::kSouthWest;
-    } else if (b[at + Cardinal::kSouthEast] == '.') {
-      at = at + Cardinal::kSouthEast;
-    } else {
-      break;
+  bool fell = true;
+  while (fell) {
+    if (!b.OnBoard(at + Cardinal::kSouth)) return false;
+
+    fell = false;
+    for (Point d : {Cardinal::kSouth, Cardinal::kSouthWest, Cardinal::kSouthEast}) {
+      Point t = at + d;
+      CHECK(b.OnBoard(t));
+      if (b[t] == '.') {
+        at = t;
+        fell = true;
+        break;
+      }
     }
   }
   b[at] = 'o';
@@ -42,48 +84,23 @@ bool AddSand(CharBoard& b, Point at) {
 
 absl::StatusOr<std::string> Day_2022_14::Part1(
     absl::Span<absl::string_view> input) const {
-  std::optional<PointRectangle> grid;
-  for (absl::string_view line : input) {
-    for (absl::string_view p_str : absl::StrSplit(line, " -> ")) {
-      Point p;
-      if (!RE2::FullMatch(p_str, "(.*)", p.Capture())) {
-        return Error("Bad point");
-      } 
-      if (grid) {
-        grid->ExpandInclude(p);
-        grid->ExpandInclude(Point{p.x, 0});
-      }
-      else grid = PointRectangle{p, p};
-    }
-  }
+  absl::StatusOr<PointRectangle> grid = FindGrid(input);
+  if (!grid.ok()) return grid.status();
+
+  // Add space to fall off the end.
   --grid->min.x;
   ++grid->max.x;
   VLOG(1) << "Grid: " << *grid;
-  CharBoard b(grid->max.x - grid->min.x + 1, grid->max.y - grid->min.y + 1);
-  for (absl::string_view line : input) {
-    std::optional<Point> last;
-    for (absl::string_view p_str : absl::StrSplit(line, " -> ")) {
-      Point p;
-      if (!RE2::FullMatch(p_str, "(.*)", p.Capture())) {
-        return Error("Bad point");
-      } 
-      if (last) {
-        Point d = (p - *last).min_step();
-        if (d.dist() != 1) return Error("Bad d");
-        for (Point t = *last; t != p; t += d) {
-          b[t - grid->min] = '#';
-        }
-      }
-      b[p - grid->min] = '#';
-      last = p;
-    }
-  }
-  VLOG(2) << "Board:\n" << b;
-  Point add = Point{500, 0} - grid->min;
+
+  absl::StatusOr<CharBoard> b = DrawGrid(*grid, input);
+  if (!b.ok()) return b.status();
+  VLOG(2) << "Board:\n" << *b;
+
+  Point add = kAddAt - grid->min;
   int added = 0;
-  while (b[add] == '.') {
-    if (!AddSand(b, add)) break;
-    VLOG(2) << "Board:\n" << b;
+  while ((*b)[add] == '.') {
+    if (!AddSand(*b, add)) break;
+    VLOG(2) << "Board:\n" << *b;
     ++added;
   }
   return IntReturn(added);
@@ -91,52 +108,29 @@ absl::StatusOr<std::string> Day_2022_14::Part1(
 
 absl::StatusOr<std::string> Day_2022_14::Part2(
     absl::Span<absl::string_view> input) const {
-  std::optional<PointRectangle> grid;
-  for (absl::string_view line : input) {
-    for (absl::string_view p_str : absl::StrSplit(line, " -> ")) {
-      Point p;
-      if (!RE2::FullMatch(p_str, "(.*)", p.Capture())) {
-        return Error("Bad point");
-      } 
-      if (grid) {
-        grid->ExpandInclude(p);
-        grid->ExpandInclude(Point{p.x, 0});
-      }
-      else grid = PointRectangle{p, p};
-    }
-  }
-  grid->max.y += 2;
-  grid->min.x = std::min(grid->min.x, 498 - grid->max.y);
-  grid->max.x = std::max(grid->max.x, 502 + grid->max.y);
+  absl::StatusOr<PointRectangle> grid = FindGrid(input);
+  if (!grid.ok()) return grid.status();
+
+  // Expand to include the bottom line.
+  grid->ExpandInclude(Point{kAddAt.x, grid->max.y + 2});
+  // Extend the bottom line until we have space for a full pyramid.
+  grid->ExpandInclude(Point{kAddAt.x - grid->max.y, grid->max.y});
+  grid->ExpandInclude(Point{kAddAt.x + grid->max.y, grid->max.y});
   VLOG(1) << "Grid: " << *grid;
-  CharBoard b(grid->max.x - grid->min.x + 1, grid->max.y - grid->min.y + 1);
-  for (absl::string_view line : input) {
-    std::optional<Point> last;
-    for (absl::string_view p_str : absl::StrSplit(line, " -> ")) {
-      Point p;
-      if (!RE2::FullMatch(p_str, "(.*)", p.Capture())) {
-        return Error("Bad point");
-      } 
-      if (last) {
-        Point d = (p - *last).min_step();
-        if (d.dist() != 1) return Error("Bad d");
-        for (Point t = *last; t != p; t += d) {
-          b[t - grid->min] = '#';
-        }
-      }
-      b[p - grid->min] = '#';
-      last = p;
-    }
+
+  absl::StatusOr<CharBoard> b = DrawGrid(*grid, input);
+  if (!b.ok()) return b.status();
+  // Draw the bottom line.
+  for (int x = 0; x < b->width(); ++x) {
+    (*b)[Point{x, b->height() - 1}] = '#';
   }
-  for (int x = 0; x < b.width(); ++x) {
-    b[Point{x, b.height() - 1}] = '#';
-  }
-  VLOG(2) << "Board:\n" << b;
-  Point add = Point{500, 0} - grid->min;
+
+  VLOG(2) << "Board:\n" << *b;
+  Point add = kAddAt - grid->min;
   int added = 0;
-  while (b[add] == '.') {
-    if (!AddSand(b, add)) break;
-    VLOG(2) << "Board:\n" << b;
+  while ((*b)[add] == '.') {
+    if (!AddSand(*b, add)) break;
+    VLOG(2) << "Board:\n" << *b;
     ++added;
   }
   return IntReturn(added);
