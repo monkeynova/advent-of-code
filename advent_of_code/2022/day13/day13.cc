acaad5ffc45c5ddc4db093bf705d9a2119ba48c5
Keith Peters <keith@monkeynova.com>
2022-12-13 23:37:04 -0500

    2022/13 More futzing
    Remove unique_ptr mess. It's unnecessary.

--- advent_of_code/2022/day13/day13.cc
+++ advent_of_code/2022/day13/day13.cc
@@ -18,14 +18,14 @@ namespace {
 
 struct Cmp {
   int val = -1;
-  std::vector<std::unique_ptr<Cmp>> list;
+  std::vector<Cmp> list;
 
   friend std::ostream& operator<<(std::ostream& o, const Cmp& cmp) {
     if (cmp.val != -1) return o << cmp.val;
     o << "[";
     for (int i = 0; i < cmp.list.size(); ++i) {
       if (i > 0) o << ",";
-      o << *cmp.list[i];
+      o << cmp.list[i];
     }
     return o << "]";
   }
@@ -44,19 +44,19 @@ struct Cmp {
     }
     if (val != -1) {
       Cmp list_val;
-      list_val.list.push_back(absl::make_unique<Cmp>());
-      list_val.list.back()->val = val;
+      list_val.list.push_back(Cmp{});
+      list_val.list.back().val = val;
       return list_val.cmp(o);
     }
     if (o.val != -1) {
       Cmp list_val;
-      list_val.list.push_back(absl::make_unique<Cmp>());
-      list_val.list.back()->val = o.val;
+      list_val.list.push_back(Cmp{});
+      list_val.list.back().val = o.val;
       return cmp(list_val);
     }
     for (int i = 0; i < list.size(); ++i) {
       if (i >= o.list.size()) return 1;
-      int sub_cmp = list[i]->cmp(*o.list[i]);
+      int sub_cmp = list[i].cmp(o.list[i]);
       if (sub_cmp != 0) return sub_cmp;
     }
     if (o.list.size() > list.size()) return -1;
@@ -64,8 +64,8 @@ struct Cmp {
   }
 };
 
-std::unique_ptr<Cmp> Parse(absl::string_view& line) {
-  std::unique_ptr<Cmp> ret = absl::make_unique<Cmp>();
+Cmp Parse(absl::string_view& line) {
+  Cmp ret;
   if (line[0] == '[') {
     line = line.substr(1);
     while (true) {
@@ -73,13 +73,13 @@ std::unique_ptr<Cmp> Parse(absl::string_view& line) {
         line = line.substr(1);
         break;
       }
-      ret->list.push_back(Parse(line));
+      ret.list.push_back(Parse(line));
       if (line[0] == ',') line = line.substr(1);
     }
   } else if (line[0] >= '0' && line[0] <= '9') {
-    ret->val = 0;
+    ret.val = 0;
     while (line[0] >= '0' && line[0] <= '9') {
-      ret->val = ret->val * 10 + line[0] - '0';
+      ret.val = ret.val * 10 + line[0] - '0';
       line = line.substr(1);
     }
   }
@@ -91,10 +91,9 @@ bool ByPointerLt(const std::unique_ptr<Cmp>& a,
   return *a < *b;
 }
 
-absl::StatusOr<std::unique_ptr<Cmp>> ParseFull(
-    absl::string_view line) {
+absl::StatusOr<Cmp> ParseFull( absl::string_view line) {
   absl::string_view tmp = line;
-  std::unique_ptr<Cmp> ret = Parse(tmp);
+  Cmp ret = Parse(tmp);
   if (!tmp.empty()) return Error("Bad line: ", line);
   return ret;
 }
@@ -106,27 +105,28 @@ absl::StatusOr<std::string> Day_2022_13::Part1(
   int ret = 0;
   for (int i = 0; i < input.size(); i += 3) {
     if (!input[i+2].empty()) return Error("Bad input: empty");
-    absl::StatusOr<std::unique_ptr<Cmp>> left = ParseFull(input[i]);
+    absl::StatusOr<Cmp> left = ParseFull(input[i]);
     if (!left.ok()) return left.status();
-    absl::StatusOr<std::unique_ptr<Cmp>> right = ParseFull(input[i+1]);
+    absl::StatusOr<Cmp> right = ParseFull(input[i+1]);
     if (!right.ok()) return right.status();
-    if (!(**right < **left)) ret += (i/3) + 1;
+    if (!(*right < *left)) ret += (i/3) + 1;
   }
   return IntReturn(ret);
 }
 
 absl::StatusOr<std::string> Day_2022_13::Part2(
     absl::Span<absl::string_view> input) const {
-  absl::StatusOr<std::unique_ptr<Cmp>> m1 = ParseFull("[[2]]");
-  absl::StatusOr<std::unique_ptr<Cmp>> m2 = ParseFull("[[6]]");
+  absl::StatusOr<Cmp> m1 = ParseFull("[[2]]");
+  absl::StatusOr<Cmp> m2 = ParseFull("[[6]]");
 
   int idx1 = 1;
   int idx2 = 2;
   for (int i = 0; i < input.size(); ++i) {
     if (input[i].empty()) continue;
-    absl::StatusOr<std::unique_ptr<Cmp>> test = ParseFull(input[i]);
-    if (**test < **m1) { ++idx1; ++idx2; }
-    else if (**test < **m2) { ++idx2; }
+    absl::StatusOr<Cmp> test = ParseFull(input[i]);
+    if (!test.ok()) return test.status();
+    if (*test < *m1) { ++idx1; ++idx2; }
+    else if (*test < *m2) { ++idx2; }
   }
   return IntReturn(idx1 * idx2);
 }
