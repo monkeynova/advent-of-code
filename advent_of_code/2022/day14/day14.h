bfb9c52465e6a8fb1dc383e5c70541abef2e47b9
Keith Peters <keith@monkeynova.com>
2022-12-13 22:22:39 -0500

    2022.14 New

---
+++ advent_of_code/2022/day14/day14.h
@@ -0,0 +1,22 @@
+#ifndef ADVENT_OF_CODE_2022_DAY14_H
+#define ADVENT_OF_CODE_2022_DAY14_H
+
+#include "advent_of_code/advent_day.h"
+
+namespace advent_of_code {
+
+class Day_2022_14 : public AdventDay {
+ public:
+  virtual absl::string_view test_file() const override {
+    return "advent_of_code/2022/day14/day14.test";
+  }
+
+  absl::StatusOr<std::string> Part1(
+      absl::Span<absl::string_view> input) const override;
+  absl::StatusOr<std::string> Part2(
+      absl::Span<absl::string_view> input) const override;
+};
+
+}  // namespace advent_of_code
+
+#endif  // ADVENT_OF_CODE_2022_DAY14_H
