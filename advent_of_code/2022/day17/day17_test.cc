636731d1e8acb95cf5461a10ad9b30fc95be0a8f
Keith Peters <keith@monkeynova.com>
2022-12-16 18:52:35 -0800

    2022/17 New

---
+++ advent_of_code/2022/day17/day17_test.cc
@@ -0,0 +1,12 @@
+#include "advent_of_code/2022/day17/day17.h"
+
+#include "advent_of_code/infra/file_test.h"
+#include "gmock/gmock.h"
+
+namespace advent_of_code {
+
+using DayTest = FileTest<Day_2022_17>;
+
+TEST_F(DayTest, FileBasedTest) { EXPECT_TRUE(RunTest()); }
+
+}  // namespace advent_of_code
