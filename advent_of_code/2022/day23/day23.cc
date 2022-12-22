8157f584d3367174c9ad72c85d0db424706fe5ed
Keith Peters <keith@monkeynova.com>
2022-12-21 23:07:40 -0800

    2022/23 New

---
+++ advent_of_code/2022/day23/day23.cc
@@ -0,0 +1,33 @@
+// http://adventofcode.com/2022/day/23
+
+#include "advent_of_code/2022/day23/day23.h"
+
+#include "absl/algorithm/container.h"
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/log/log.h"
+#include "absl/strings/numbers.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_join.h"
+#include "absl/strings/str_split.h"
+#include "re2/re2.h"
+
+namespace advent_of_code {
+
+namespace {
+
+// Helper methods go here.
+
+}  // namespace
+
+absl::StatusOr<std::string> Day_2022_23::Part1(
+    absl::Span<absl::string_view> input) const {
+  return absl::UnimplementedError("Problem not known");
+}
+
+absl::StatusOr<std::string> Day_2022_23::Part2(
+    absl::Span<absl::string_view> input) const {
+  return absl::UnimplementedError("Problem not known");
+}
+
+}  // namespace advent_of_code
---
+++ advent_of_code/2022/day23/day23_benchmark.cc
@@ -0,0 +1,18 @@
+#include "advent_of_code/2022/day23/day23.h"
+#include "advent_of_code/infra/file_benchmark.h"
+#include "gmock/gmock.h"
+
+namespace advent_of_code {
+
+constexpr int kMaxTestCount = 5;
+
+TEST(DayBenchmarkTest_Day_2022_23, Consistency) {
+  Day_2022_23 day;
+  absl::StatusOr<int> test_count = FileBenchmarkTestCount(&day);
+  ASSERT_TRUE(test_count.ok()) << test_count.status().ToString();
+  EXPECT_EQ(kMaxTestCount, *test_count);
+}
+
+BENCHMARK_TEMPLATE(BM_Day, Day_2022_23)->DenseRange(0, kMaxTestCount - 1);
+
+}  // namespace advent_of_code
