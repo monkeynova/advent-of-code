93ec35c5af5eb6760148a5625595e2b44c06f8bb
Keith Peters <keith@monkeynova.com>
2022-12-23 22:03:19 -0800

    2022/24 Night-of futzing

--- advent_of_code/2022/day24/day24.cc
+++ advent_of_code/2022/day24/day24.cc
@@ -83,6 +83,9 @@ struct BlizzardHistory {
   }
 };
 
+// We use a Point3 for state because the world changes with each step, so we
+// use the step # as a z coordinate to consider {x,y},step1 distinct from
+// {x,y},step2.
 class BFSWalk : public BFSInterface<BFSWalk, Point3> { 
  public:
   BFSWalk(Point start, Point end, const CharBoard& b, BlizzardHistory& h)
@@ -94,9 +97,7 @@ class BFSWalk : public BFSInterface<BFSWalk, Point3> {
 
   bool IsFinal() const override { return cur() == end_; }
   void AddNextSteps(State* state) const override {
-    VLOG(2) << num_steps();
     while (h_.sets.size() <= num_steps() + 1) {
-      VLOG(1) << h_.sets.size();
       h_.BuildNext(b_);
     }
     const BlizzardSet& blizzards = h_.sets[num_steps() + 1];
@@ -126,84 +127,90 @@ class BFSWalk : public BFSInterface<BFSWalk, Point3> {
   BlizzardHistory& h_;
 };
 
+struct Start {
+  std::optional<Point> start;
+  std::optional<Point> end;
+  BlizzardSet blizzards;
+};
+
+Start ParseAndClearBoard(CharBoard& board) {
+  Start ret;
+  for (Point p : board.range()) {
+    switch (board[p]) {
+      case '^': {
+        ret.blizzards.up.insert(p);
+        board[p] = '.';
+        break;
+      }
+      case 'v': {
+        ret.blizzards.down.insert(p);
+        board[p] = '.';
+        break;
+      }
+      case '<': {
+        ret.blizzards.left.insert(p);
+        board[p] = '.';
+        break;
+      }
+      case '>': {
+        ret.blizzards.right.insert(p);
+        board[p] = '.';
+        break;
+      }
+      case '.': {
+        if (p.y == 0) ret.start = p;
+        if (p.y == board.height() - 1) ret.end = p;
+      }
+    }
+  }
+  return ret;
+}
+
 }  // namespace
 
 absl::StatusOr<std::string> Day_2022_24::Part1(
     absl::Span<absl::string_view> input) const {
-  VLOG(1) << "Part1";
   absl::StatusOr<CharBoard> board = ParseAsBoard(input);
   if (!board.ok()) return board.status();
 
-  VLOG(1) << *board;
+  Start start = ParseAndClearBoard(*board);
+  if (!start.start) return Error("No start");
+  if (!start.end) return Error("No end");
+  VLOG(1) << "Route: " << *start.start << " -> " << *start.end;
 
   BlizzardHistory hist;
-  hist.sets.push_back(BlizzardSet{});
-  hist.sets[0].up = board->Find('^');
-  hist.sets[0].down = board->Find('v');
-  hist.sets[0].left = board->Find('<');
-  hist.sets[0].right = board->Find('>');
+  hist.sets.push_back(std::move(start.blizzards));
 
-  std::optional<Point> start;
-  std::optional<Point> end;
-  for (Point p : board->range()) {
-    if (p.y == 0 && (*board)[p] == '.') start = p;
-    if (p.y == board->height() - 1 && (*board)[p] == '.') end = p;
-  }
-  if (!start) return Error("No start");
-  if (!end) return Error("No end");
-  VLOG(1) << "start: " << *start;
-  VLOG(1) << "end: " << *end;
-  for (Point p : board->range()) {
-    if ((*board)[p] != '.' && (*board)[p] != '#') {
-      (*board)[p] = '.';
-    }
-  }
-
-  return IntReturn(BFSWalk(*start, *end, *board, hist).FindMinSteps());
+  return IntReturn(
+      BFSWalk(*start.start, *start.end, *board, hist).FindMinSteps());
 }
 
 absl::StatusOr<std::string> Day_2022_24::Part2(
     absl::Span<absl::string_view> input) const {
-  VLOG(1) << "Part1";
   absl::StatusOr<CharBoard> board = ParseAsBoard(input);
   if (!board.ok()) return board.status();
 
-  VLOG(1) << *board;
+  Start start = ParseAndClearBoard(*board);
+  if (!start.start) return Error("No start");
+  if (!start.end) return Error("No end");
+  VLOG(1) << "Route: " << *start.start << " -> " << *start.end;
 
   BlizzardHistory hist;
-  hist.sets.push_back(BlizzardSet{});
-  hist.sets[0].up = board->Find('^');
-  hist.sets[0].down = board->Find('v');
-  hist.sets[0].left = board->Find('<');
-  hist.sets[0].right = board->Find('>');
-
-  std::optional<Point> start;
-  std::optional<Point> end;
-  for (Point p : board->range()) {
-    if (p.y == 0 && (*board)[p] == '.') start = p;
-    if (p.y == board->height() - 1 && (*board)[p] == '.') end = p;
-  }
-  if (!start) return Error("No start");
-  if (!end) return Error("No end");
-  VLOG(1) << "start: " << *start;
-  VLOG(1) << "end: " << *end;
-  for (Point p : board->range()) {
-    if ((*board)[p] != '.' && (*board)[p] != '#') {
-      (*board)[p] = '.';
-    }
-  }
-
-  std::optional<int> stage_1 = BFSWalk(*start, *end, *board, hist).FindMinSteps();
+  hist.sets.push_back(std::move(start.blizzards));
+  std::optional<int> stage_1 =
+      BFSWalk(*start.start, *start.end, *board, hist).FindMinSteps();
   if (!stage_1) return Error("stage 1 not found");
 
   BlizzardHistory hist2;
   hist2.sets.push_back(hist.sets.back());
-  std::optional<int> stage_2 = BFSWalk(*end, *start, *board, hist2).FindMinSteps();
+  std::optional<int> stage_2 =
+      BFSWalk(*start.end, *start.start, *board, hist2).FindMinSteps();
   if (!stage_2) return Error("stage 2 not found");
 
   BlizzardHistory hist3;
   hist3.sets.push_back(hist2.sets.back());
-  std::optional<int> stage_3 = BFSWalk(*start, *end, *board, hist3).FindMinSteps();
+  std::optional<int> stage_3 =
+      BFSWalk(*start.start, *start.end, *board, hist3).FindMinSteps();
   if (!stage_3) return Error("stage 3 not found");
 
   return IntReturn(*stage_1 + *stage_2 + *stage_3);
