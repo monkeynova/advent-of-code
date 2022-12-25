e1111db4324a87693f4b100ba5df8335646eaa8f
Keith Peters <keith@monkeynova.com>
2022-12-24 21:19:58 -0800

    2022/25 Answer

--- advent_of_code/2022/day25/day25.cc
+++ advent_of_code/2022/day25/day25.cc
@@ -16,13 +16,65 @@ namespace advent_of_code {
 
 namespace {
 
-// Helper methods go here.
+int64_t Snafu(absl::string_view line) {
+    int64_t num = 0;
+    for (char c : line) {
+      int next = 0;
+      switch (c) {
+        case '0': next = 0; break;
+        case '1': next = 1; break;
+        case '2': next = 2; break;
+        case '-': next = -1; break;
+        case '=': next = -2; break;
+        default: LOG(FATAL) << "Bad line: " << line;
+      }
+      num = num * 5 + next;
+    }
+  return num;
+}
+
+std::string UnSnafu(int64_t total) {
+  std::string ret;
+  for (int64_t c = total; c; c /= 5) {
+    ret.append(1, (c % 5) + '0');
+  }
+  for (int i = 0; i < ret.size(); ++i) {
+    VLOG(1) << ret;
+    if (ret[i] > '2') {
+      if (i == ret.size() - 1) {
+        ret.append(1, '0');
+      }
+      for (int j = i + 1; ++ret[j] == '5'; ++j) {
+        VLOG(1) << "  " << ret;
+        ret[j] = '0';
+        if (j == ret.size() - 1) {
+          ret.append(1, '0');
+        }
+      }
+      VLOG(1) << "  " << ret;
+      if (ret[i] == '3') {
+        ret[i] = '=';
+      } else if (ret[i] == '4') {
+        ret[i] = '-';
+      } else {
+        LOG(FATAL) << "Can't foo " << ret;
+      }
+    }
+  }
+  absl::c_reverse(ret);
+return ret;
+}
 
 }  // namespace
 
 absl::StatusOr<std::string> Day_2022_25::Part1(
     absl::Span<absl::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  int64_t total = 0;
+  for (absl::string_view line : input) {
+    total += Snafu(line);
+  }
+  CHECK_EQ(Snafu(UnSnafu(total)), total);
+  return UnSnafu(total);
 }
 
 absl::StatusOr<std::string> Day_2022_25::Part2(
