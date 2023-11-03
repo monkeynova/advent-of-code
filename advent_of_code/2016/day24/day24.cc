97c3261c96f3a25b1c824958fe6ba95d451f1c65
Keith Peters <keith@monkeynova.com>
2023-11-02 17:39:22 -0700

    even simpler 2016/24

--- advent_of_code/2016/day24/day24.cc
+++ advent_of_code/2016/day24/day24.cc
@@ -13,35 +13,6 @@
 
 namespace advent_of_code {
 
-namespace {
-
-struct KeyState {
-  int need = 0;
-  int have = 0;
-
-  KeyState(const CharBoard& b) {
-    for (Point p : b.range()) {
-      if (b[p] > '0' && b[p] <= '9') {
-        need |= 1 << (b[p] - '1');
-      }
-    }
-  }
-
-  bool operator==(const KeyState& ks) const {
-    return need == ks.need && have == ks.have;
-  }
-  template <typename H>
-  friend H AbslHashValue(H h, const KeyState& ks) {
-    return H::combine(std::move(h), ks.need, ks.have);
-  }
-  template <typename Sink>
-  friend void AbslStringify(Sink& sink, const KeyState& ks) {
-    absl::Format(&sink, "0x%x/0x%x", ks.have, ks.need);
-  }
-};
-
-}  // namespace
-
 absl::StatusOr<std::string> Day_2016_24::Part1(
     absl::Span<absl::string_view> input) const {
   absl::StatusOr<CharBoard> b = CharBoard::Parse(input);
@@ -51,19 +22,26 @@ absl::StatusOr<std::string> Day_2016_24::Part1(
   if (start_set.size() != 1) return Error("No unique start");
   Point start = *start_set.begin();
 
-  return AdventReturn(PointWalkData<KeyState>({
-    .start = {.p = start, .d = KeyState(*b)},
-    .is_good = [&](PointAndData<KeyState>& cur, int) {
+  int need = 0;
+  for (Point p : b->range()) {
+    if ((*b)[p] > '0' && (*b)[p] <= '9') {
+      need |= 1 << ((*b)[p] - '1');
+    }
+  }
+
+  return AdventReturn(PointWalkData<int>({
+    .start = {.p = start, .d = need},
+    .is_good = [&](PointAndData<int>& cur, int) {
       if (!b->OnBoard(cur.p)) return false;
       char board_char = (*b)[cur.p];
       if (board_char == '#') return false;
       if (board_char > '0' && board_char <= '9') {
-        cur.d.have |= 1 << (board_char - '1');
+        cur.d &= ~(1 << (board_char - '1'));
       }
       return true;
     },
-    .is_final = [&](PointAndData<KeyState> cur, int) {
-      return cur.d.need == cur.d.have;
+    .is_final = [&](PointAndData<int> cur, int) {
+      return cur.d == 0;
     },
   }).FindMinSteps());
 }
@@ -77,19 +55,26 @@ absl::StatusOr<std::string> Day_2016_24::Part2(
   if (start_set.size() != 1) return Error("No unique start");
   Point start = *start_set.begin();
 
-  return AdventReturn(PointWalkData<KeyState>({
-    .start = {.p = start, .d = KeyState(*b)},
-    .is_good = [&](PointAndData<KeyState>& cur, int) {
+  int need = 0;
+  for (Point p : b->range()) {
+    if ((*b)[p] > '0' && (*b)[p] <= '9') {
+      need |= 1 << ((*b)[p] - '1');
+    }
+  }
+
+  return AdventReturn(PointWalkData<int>({
+    .start = {.p = start, .d = need},
+    .is_good = [&](PointAndData<int>& cur, int) {
       if (!b->OnBoard(cur.p)) return false;
       char board_char = (*b)[cur.p];
       if (board_char == '#') return false;
       if (board_char > '0' && board_char <= '9') {
-        cur.d.have |= 1 << (board_char - '1');
+        cur.d &= ~(1 << (board_char - '1'));
       }
       return true;
     },
-    .is_final = [&](PointAndData<KeyState> cur, int) {
-      return cur.d.need == cur.d.have && cur.p == start;
+    .is_final = [&](PointAndData<int> cur, int) {
+      return cur.d == 0 && cur.p == start;
     },
   }).FindMinSteps());
 }
