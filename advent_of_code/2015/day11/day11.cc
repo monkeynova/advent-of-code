1bde072c60f6b29992b3ce465c6a60672f54713f
Keith Peters <keith@monkeynova.com>
2023-11-20 18:28:03 -0800

    TODONE. 2x performance improvement on 2015/11

--- advent_of_code/2015/day11/day11.cc
+++ advent_of_code/2015/day11/day11.cc
@@ -13,20 +13,28 @@ namespace advent_of_code {
 namespace {
 
 void Increment(std::string* password) {
+  static std::array<bool, 128> kBadChars = []() {
+    std::array<bool, 128> ret;
+    for (int i = 0; i < 128; ++i) ret[i] = false;
+    ret['i'] = ret['o'] = ret['l'] = true;
+    return ret;
+  }();
   for (int i = password->size() - 1; i >= 0; --i) {
-    if (++(*password)[i] != 'z' + 1) break;
+    if (++(*password)[i] != 'z' + 1) {
+      if (kBadChars[(*password)[i]]) {
+        Increment(password);
+      }
+      break;
+    }
     (*password)[i] = 'a';
   }
 }
 
 bool IsValid(std::string_view password) {
-  static absl::flat_hash_set<char> kBadChars = {'i', 'o', 'l'};
   bool has_inc_triple = false;
   bool has_two_pairs = false;
   std::string_view last_pair;
   for (int i = 0; i < password.size(); ++i) {
-    // TODO(@monkeynova): Push to Increment.
-    if (kBadChars.contains(password[i])) return false;
     if (i + 1 < password.size()) {
       if (password[i] == password[i + 1]) {
         if (last_pair.empty()) {
