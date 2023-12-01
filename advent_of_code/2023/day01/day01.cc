ac2f5aef9244edbb952b66769920caed6542251a
Keith Peters <keith@monkeynova.com>
2023-12-01 05:14:19 -0800

    2023/01 Futzing

--- advent_of_code/2023/day01/day01.cc
+++ advent_of_code/2023/day01/day01.cc
@@ -23,8 +23,8 @@ std::optional<int> TryParse(char c) {
 
 std::optional<int> TryParse(std::string_view prefix) {
   static constexpr std::array<std::string_view, 10> words = {
-    "zero", "one", "two", "three", "four", "five", "six", "seven",
-    "eight", "nine",
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+    "nine",
   };
   if (prefix[0] >= '0' && prefix[0] <= '9') {
     return prefix[0] - '0';
@@ -43,14 +43,15 @@ absl::StatusOr<std::string> Day_2023_01::Part1(
   int sum = 0;
   for (std::string_view line : input) {
     std::optional<int> first;
-    std::optional<int> last;
-    for (char c : line) {
-      std::optional<int> d = TryParse(c);
-      if (d) {
-        if (!first) first = d;
-        last = d;
-      }
+    for (int i = 0; !first && i < line.size(); ++i) {
+      first = TryParse(line[i]);
     }
+    if (!first) return Error("No digits in: ", line);
+    std::optional<int> last;
+    for (int i = line.size() - 1; !last && i >= 0; --i) {
+      last = TryParse(line[i]);
+    }
+    if (!last) return Error("No digits in: ", line);
     sum += *first * 10 + *last;
   }
   return AdventReturn(sum);
@@ -61,14 +62,15 @@ absl::StatusOr<std::string> Day_2023_01::Part2(
   int sum = 0;
   for (std::string_view line : input) {
     std::optional<int> first;
-    std::optional<int> last;
-    for (int i = 0; i < line.size(); ++i) {
-      std::optional<int> d = TryParse(line.substr(i));
-      if (d) {
-        if (!first) first = d;
-        last = d;
-      }
+    for (int i = 0; !first && i < line.size(); ++i) {
+      first = TryParse(line.substr(i));
     }
+    if (!first) return Error("No digits in: ", line);
+    std::optional<int> last;
+    for (int i = line.size() - 1; !last && i >= 0; --i) {
+      last = TryParse(line.substr(i));
+    }
+    if (!last) return Error("No digits in: ", line);
     sum += *first * 10 + *last;
   }
   return AdventReturn(sum);
