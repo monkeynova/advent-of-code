736ab4246ed65fd12b9b6ef5b2b89fa86cf6c822
Keith Peters <keith@monkeynova.com>
2023-12-01 05:39:35 -0800

    2023/01 More include pruning

--- advent_of_code/2023/day01/day01.cc
+++ advent_of_code/2023/day01/day01.cc
@@ -2,15 +2,7 @@
 
 #include "advent_of_code/2023/day01/day01.h"
 
-#include "absl/algorithm/container.h"
-#include "absl/container/flat_hash_map.h"
-#include "absl/container/flat_hash_set.h"
 #include "absl/log/log.h"
-#include "absl/strings/numbers.h"
-#include "absl/strings/str_cat.h"
-#include "absl/strings/str_join.h"
-#include "absl/strings/str_split.h"
-#include "re2/re2.h"
 
 namespace advent_of_code {
 
