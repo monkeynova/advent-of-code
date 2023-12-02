d92d46221d3ffdbfcb61010bcd736d0f3e084dbb
Keith Peters <keith@monkeynova.com>
2023-12-02 05:38:21 -0800

    2023/02 Class-ify Game

--- advent_of_code/2023/day02/day02.cc
+++ advent_of_code/2023/day02/day02.cc
@@ -11,39 +11,60 @@ namespace advent_of_code {
 
 namespace {
 
-struct Game {
+class Game {
+ public:
   struct View {
     int red = 0;
     int green = 0;
     int blue = 0;
   };
 
-  int num;
-  std::vector<View> views;
+  static absl::StatusOr<Game> Parse(std::string_view line);
+
+  int num() const { return num_; }
+  absl::Span<const View> views() const { return views_; }
+
+ private:
+  Game() = default;
+
+  int num_;
+  std::vector<View> views_;
 };
 
-absl::StatusOr<Game> Parse(std::string_view line) {
+absl::StatusOr<Game> Game::Parse(std::string_view line) {
   Game ret;
   auto [game_str, view_set] = PairSplit(line, ": ");
   static LazyRE2 game_re{"Game (\\d+)"};
-  if (!RE2::FullMatch(game_str, *game_re, &ret.num)) {
-    return Error("Bad game: ", game_str);
+  if (!RE2::FullMatch(game_str, *game_re, &ret.num_)) {
+    return Error("Bad game: ", game_str, "; ", line);
   }
   for (std::string_view view_str : absl::StrSplit(view_set, "; ")) {
     Game::View view;
+    Game::View dupe_check;
     for (std::string_view ball_res : absl::StrSplit(view_str, ", ")) {
       int count;
       std::string_view color;
       static LazyRE2 ball_re{"(\\d+) (blue|red|green)"};
       if (!RE2::FullMatch(ball_res, *ball_re, &count, &color)) {
-        return Error("Bad ball_res: ", ball_res);
+        return Error("Bad ball_res: ", ball_res, "; ", line);
       }
-      if (color == "red") view.red = count;
-      else if (color == "blue") view.blue = count;
-      else if (color == "green") view.green = count;
-      else return Error("Bad color: ", color);
+      if (color == "red") {
+        if (dupe_check.red) return Error("Dupe red: ", line);
+        dupe_check.red = 1;
+        view.red = count;
+      } else if (color == "blue") {
+        if (dupe_check.blue) return Error("Dupe red: ", line);
+        dupe_check.blue = 1;
+        view.blue = count;
+      } else if (color == "green") {
+        if (dupe_check.green) return Error("Dupe red: ", line);
+        dupe_check.green = 1;
+        view.green = count;
+      } else {
+        return Error("Bad color: ", color, "; ", line);
+      }
     }
-    ret.views.push_back(view);
+    ret.views_.push_back(view);
   }
   return ret;
 }
@@ -54,12 +75,12 @@ absl::StatusOr<std::string> Day_2023_02::Part1(
     absl::Span<std::string_view> input) const {
   int game_total = 0;
   for (std::string_view line : input) {
-    ASSIGN_OR_RETURN(Game game, Parse(line));
+    ASSIGN_OR_RETURN(Game game, Game::Parse(line));
     auto is_possible = [](const Game::View& v) {
       return v.red <= 12 && v.green <= 13 && v.blue <= 14;
     };
-    if (absl::c_all_of(game.views, is_possible)) {
-      game_total += game.num;
+    if (absl::c_all_of(game.views(), is_possible)) {
+      game_total += game.num();
     }
   }
   return AdventReturn(game_total);
@@ -69,9 +90,9 @@ absl::StatusOr<std::string> Day_2023_02::Part2(
     absl::Span<std::string_view> input) const {
   int total_power = 0;
   for (std::string_view line : input) {
-    ASSIGN_OR_RETURN(Game game, Parse(line));
+    ASSIGN_OR_RETURN(Game game, Game::Parse(line));
     Game::View min = absl::c_accumulate(
-      game.views, Game::View{},
+      game.views(), Game::View{},
       [](Game::View a, Game::View b) {
         a.red = std::max(a.red, b.red);
         a.green = std::max(a.green, b.green);
