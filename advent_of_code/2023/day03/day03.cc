eff75aebbd1c903f159462589acde7157bec3d41
Keith Peters <keith@monkeynova.com>
2023-12-03 06:08:09 -0800

    2023/03 Solution

--- advent_of_code/2023/day03/day03.cc
+++ advent_of_code/2023/day03/day03.cc
@@ -27,18 +27,70 @@ namespace advent_of_code {
 
 namespace {
 
-// Helper methods go here.
+bool IsDigit(char c) {
+  return c >= '0' && c <= '9';
+}
 
 }  // namespace
 
 absl::StatusOr<std::string> Day_2023_03::Part1(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  ASSIGN_OR_RETURN(CharBoard b, CharBoard::Parse(input));
+  int sum = 0;
+  absl::flat_hash_set<Point> used;
+  for (Point p : b.range()) {
+    bool has_symbol = false;
+    int cur_num = 0;
+    for (Point p2 = p; !used.contains(p2) && b.OnBoard(p2) && IsDigit(b[p2]); ++p2.x) {
+      used.insert(p2);
+      cur_num = cur_num * 10 + b[p2] - '0';
+      for (Point d : Cardinal::kEightDirs) {
+        Point t = p2 + d;
+        if (b.OnBoard(t) && b[t] != '.' && !IsDigit(b[t])) {
+          has_symbol = true;
+        }
+      }
+    }
+    if (has_symbol) {
+      sum += cur_num;
+    }
+  }
+  return AdventReturn(sum);
 }
 
 absl::StatusOr<std::string> Day_2023_03::Part2(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  ASSIGN_OR_RETURN(CharBoard b, CharBoard::Parse(input));
+  int64_t sum = 0;
+  absl::flat_hash_set<Point> used;
+  for (Point p : b.range()) {
+    if (b[p] != '*') continue;
+    std::vector<int64_t> adjacent;
+    for (Point dir : Cardinal::kEightDirs) {
+      Point t = p + dir;
+      if (used.contains(t) || !IsDigit(b[t])) continue;
+      Point min = t;
+      while (!used.contains(min) && b.OnBoard(min) && IsDigit(b[min])) {
+        used.insert(min);
+        --min.x;
+      }
+      ++min.x;
+      Point max = {t.x + 1, t.y};
+      while (!used.contains(max) && b.OnBoard(max) && IsDigit(b[max])) {
+        used.insert(max);
+        ++max.x;
+      }
+      int num = 0;
+      for (Point d = min; d != max; ++d.x) {
+        num = num * 10 + b[d] - '0';
+      }
+      adjacent.push_back(num);
+    }
+    if (adjacent.size() == 2) {
+      sum += adjacent[0] * adjacent[1];
+    }
+  }
+  return AdventReturn(sum);
 }
 
 }  // namespace advent_of_code
