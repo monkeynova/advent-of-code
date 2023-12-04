2e9d7d6adf54e9db58ebe679e194ad462d8ccae4
Keith Peters <keith@monkeynova.com>
2023-12-04 05:43:48 -0800

    2023/05 Empty

---
+++ advent_of_code/2023/day05/day05_test.cc
@@ -0,0 +1,12 @@
+#include "advent_of_code/2023/day05/day05.h"
+
+#include "advent_of_code/infra/file_test.h"
+#include "gmock/gmock.h"
+
+namespace advent_of_code {
+
+using DayTest = FileTest<Day_2023_05>;
+
+TEST_F(DayTest, FileBasedTest) { EXPECT_TRUE(RunTest()); }
+
+}  // namespace advent_of_code
