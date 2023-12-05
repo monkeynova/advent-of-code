27a3bd56bb2bdbccdf920c9305b9568f7ff69483
Keith Peters <keith@monkeynova.com>
2023-12-05 07:11:19 -0800

    2023/06 Empty

---
+++ advent_of_code/2023/day06/day06.h
@@ -0,0 +1,22 @@
+#ifndef ADVENT_OF_CODE_2023_DAY06_H
+#define ADVENT_OF_CODE_2023_DAY06_H
+
+#include "advent_of_code/advent_day.h"
+
+namespace advent_of_code {
+
+class Day_2023_06 : public AdventDay {
+ public:
+  virtual std::string_view test_file() const override {
+    return "advent_of_code/2023/day06/day06.test";
+  }
+
+  absl::StatusOr<std::string> Part1(
+      absl::Span<std::string_view> input) const override;
+  absl::StatusOr<std::string> Part2(
+      absl::Span<std::string_view> input) const override;
+};
+
+}  // namespace advent_of_code
+
+#endif  // ADVENT_OF_CODE_2023_DAY06_H
