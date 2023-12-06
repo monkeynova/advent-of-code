82946afc6ae205b4cb342208b67b124cf1ce5a4c
Keith Peters <keith@monkeynova.com>
2023-12-06 04:20:20 -0800

    2023/06 Answer

--- advent_of_code/2023/day06/day06.cc
+++ advent_of_code/2023/day06/day06.cc
@@ -34,12 +34,65 @@ namespace {
 
 absl::StatusOr<std::string> Day_2023_06::Part1(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  if (input.size() != 2) return Error("Bad input");
+  std::vector<int> times;
+  {
+    Tokenizer tok(input[0]);
+    if (tok.Next() != "Time") return Error("Bad time");
+    if (tok.Next() != ":") return Error("Bad time");
+    while (!tok.Done()) {
+      int time;
+      if (!absl::SimpleAtoi(tok.Next(), &time)) return Error("Bad time");
+      times.push_back(time);
+    }
+  }
+  std::vector<int> distances;
+  {
+    Tokenizer tok(input[1]);
+    if (tok.Next() != "Distance") return Error("Bad distance");
+    if (tok.Next() != ":") return Error("Bad distance");
+    while (!tok.Done()) {
+      int distance;
+      if (!absl::SimpleAtoi(tok.Next(), &distance)) return Error("Bad distance");
+      distances.push_back(distance);
+    }
+  }
+  if (times.size() != distances.size()) return Error("Bad lengths");
+  int64_t product = 1;
+  for (int i = 0; i < times.size(); ++i) {
+    int valid = 0;
+    for (int test = 1; test < times[i]; ++test) {
+      if ((times[i] - test) * test > distances[i]) {
+        ++valid;
+      }
+    }
+    product *= valid;
+  }
+  return AdventReturn(product);
 }
 
 absl::StatusOr<std::string> Day_2023_06::Part2(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  if (input.size() != 2) return Error("Bad input");
+  int64_t time = 0;
+  for (char c: input[0]) {
+    if (isdigit(c)) {
+      time = time * 10 + c - '0';
+    }
+  }
+  int64_t distance = 0;
+  for (char c: input[1]) {
+    if (isdigit(c)) {
+      distance = distance * 10 + c - '0';
+    }
+  }
+  int64_t valid = 0;
+  for (int64_t test = 1; test < time; ++test) {
+    if ((time - test) * test > distance) {
+      ++valid;
+    }
+  }
+  return AdventReturn(valid);
 }
 
 }  // namespace advent_of_code
