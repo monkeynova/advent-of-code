c0922728147833552ea22e34fa135a774783a733
Keith Peters <keith@monkeynova.com>
2023-12-13 05:10:34 -0800

    2023/13 Futzing; Part1 and Part2 use the same function now

--- advent_of_code/2023/day13/day13.cc
+++ advent_of_code/2023/day13/day13.cc
@@ -28,69 +28,24 @@ namespace advent_of_code {
 
 namespace {
 
-int ScoreReflection(const ImmutableCharBoard& b) {
-  int64_t ret = 0;
+int ScoreReflection(const ImmutableCharBoard& b, int diff_test = 0) {
   for (int v_test = 0; v_test + 1 < b.width(); ++v_test) {
-    bool is_symmetric = true;
+    int diff = 0;
     for (int y = 0; y < b.height(); ++y) {
       std::string_view row = b.row(y);
       for (int x = v_test; x >= 0; --x) {
         int test_x = 2 * v_test - x + 1;
         if (test_x >= b.width()) break;
         if (row[x] != row[test_x]) {
-          is_symmetric = false;
-          break;
-        }
-      }
-      if (!is_symmetric) break;
-    }
-    if (is_symmetric) {
-      VLOG(1) << "v_test = " << v_test;
-      ret += v_test + 1;
-    }
-  }
-  for (int h_test = 0; h_test + 1 < b.height(); ++h_test) {
-    bool is_symmetric = true;
-    for (int y = h_test; y >= 0; --y) {
-      int test_y = 2 * h_test - y + 1;
-      if (test_y >= b.height()) break;
-      if (b.row(y) != b.row(test_y)) {
-        is_symmetric = false;
-        break;
-      }
-    }
-    if (is_symmetric) {
-      VLOG(1) << "h_test = " << h_test;
-      ret += 100 * (h_test + 1);
-    }
-  }
-  VLOG(1) << "score = " << ret;
-  if (ret == 0) {
-    VLOG(1) << "No Score\n" << b;
-  }
-  return ret;
-}
-
-int ScoreReflectionSmudge(const ImmutableCharBoard& b) {
-  int64_t ret = 0;
-  for (int v_test = 0; v_test + 1 < b.width(); ++v_test) {
-    int diff = 0;;
-    for (int y = 0; y < b.height(); ++y) {
-      std::string_view row = b.row(y);
-      for (int x = v_test; x >= 0; --x) {
-        int test_x = 2 * v_test - x + 1;
-        if (test_x >= b.width()) break;
-        if (row[x] != row[test_x]) {
-          if (++diff > 1) {
+          if (++diff > diff_test) {
             break;
           }
         }
       }
-      if (diff > 1) break;
+      if (diff > diff_test) break;
     }
-    if (diff == 1) {
-      VLOG(1) << "v_test = " << v_test;
-      ret += v_test + 1;
+    if (diff == diff_test) {
+      return v_test + 1;
     }
   }
   for (int h_test = 0; h_test + 1 < b.height(); ++h_test) {
@@ -102,25 +57,20 @@ int ScoreReflectionSmudge(const ImmutableCharBoard& b) {
       std::string_view row2 = b.row(test_y);
       for (int x = 0; x < b.width(); ++x) {
         if (row1[x] != row2[x]) {
-          if (++diff > 1) {
+          if (++diff > diff_test) {
             break;
           }
         }
       }
-      if (diff > 1) {
+      if (diff > diff_test) {
         break;
       }
     }
-    if (diff == 1) {
-      VLOG(1) << "h_test = " << h_test;
-      ret += 100 * (h_test + 1);
+    if (diff == diff_test) {
+      return 100 * (h_test + 1);
     }
   }
-  VLOG(1) << "score = " << ret;
-  if (ret == 0) {
-    VLOG(1) << "No Score\n" << b;
-  }
-  return ret;
+  return 0;
 }
 
 }  // namespace
@@ -131,12 +81,16 @@ absl::StatusOr<std::string> Day_2023_13::Part1(
   int total = 0;
   for (int i = 0; i < input.size(); ++i) {
     if (input[i].empty()) {
-      ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(input.subspan(last_empty + 1, i - last_empty - 1)));
+      absl::Span<std::string_view> board =
+          input.subspan(last_empty + 1, i - last_empty - 1);
+      ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(board));
       total += ScoreReflection(b);
       last_empty = i;
     }
   }
-  ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(input.subspan(last_empty + 1, input.size() - last_empty - 1)));
+  absl::Span<std::string_view> board =
+      input.subspan(last_empty + 1, input.size() - last_empty - 1);
+  ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(board));
   total += ScoreReflection(b);
   return AdventReturn(total);
 }
@@ -147,13 +101,17 @@ absl::StatusOr<std::string> Day_2023_13::Part2(
   int total = 0;
   for (int i = 0; i < input.size(); ++i) {
     if (input[i].empty()) {
-      ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(input.subspan(last_empty + 1, i - last_empty - 1)));
-      total += ScoreReflectionSmudge(b);
+      absl::Span<std::string_view> board =
+          input.subspan(last_empty + 1, i - last_empty - 1);
+      ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(board));
+      total += ScoreReflection(b, 1);
       last_empty = i;
     }
   }
-  ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(input.subspan(last_empty + 1, input.size() - last_empty - 1)));
-  total += ScoreReflectionSmudge(b);
+  absl::Span<std::string_view> board =
+      input.subspan(last_empty + 1, input.size() - last_empty - 1);
+  ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(board));
+  total += ScoreReflection(b, 1);
   return AdventReturn(total);
 }
 
