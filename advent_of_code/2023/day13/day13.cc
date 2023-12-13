810d65c17d9d20f1ebaef16b4245588a8559f7de
Keith Peters <keith@monkeynova.com>
2023-12-13 05:05:28 -0800

    2023/13 Performance 1050us -> 350us; CharBoard::row rather than by poitn

--- advent_of_code/2023/day13/day13.cc
+++ advent_of_code/2023/day13/day13.cc
@@ -28,18 +28,21 @@ namespace advent_of_code {
 
 namespace {
 
-int ScoreReflection(const CharBoard& b) {
+int ScoreReflection(const ImmutableCharBoard& b) {
   int64_t ret = 0;
   for (int v_test = 0; v_test + 1 < b.width(); ++v_test) {
     bool is_symmetric = true;
-    for (Point p : b.range()) {
-      if (p.x > v_test) continue;
-      Point r = {(v_test - p.x) + v_test + 1, p.y};
-      if (!b.OnBoard(r)) continue;
-      if (b[p] != b[r]) {
-        is_symmetric = false;
-        break;
+    for (int y = 0; y < b.height(); ++y) {
+      std::string_view row = b.row(y);
+      for (int x = v_test; x >= 0; --x) {
+        int test_x = 2 * v_test - x + 1;
+        if (test_x >= b.width()) break;
+        if (row[x] != row[test_x]) {
+          is_symmetric = false;
+          break;
+        }
       }
+      if (!is_symmetric) break;
     }
     if (is_symmetric) {
       VLOG(1) << "v_test = " << v_test;
@@ -48,11 +51,10 @@ int ScoreReflection(const CharBoard& b) {
   }
   for (int h_test = 0; h_test + 1 < b.height(); ++h_test) {
     bool is_symmetric = true;
-    for (Point p : b.range()) {
-      if (p.y > h_test) continue;
-      Point r = {p.x, (h_test - p.y) + h_test + 1 };
-      if (!b.OnBoard(r)) continue;
-      if (b[p] != b[r]) {
+    for (int y = h_test; y >= 0; --y) {
+      int test_y = 2 * h_test - y + 1;
+      if (test_y >= b.height()) break;
+      if (b.row(y) != b.row(test_y)) {
         is_symmetric = false;
         break;
       }
@@ -69,42 +71,47 @@ int ScoreReflection(const CharBoard& b) {
   return ret;
 }
 
-int ScoreReflectionSmudge(const CharBoard& b) {
+int ScoreReflectionSmudge(const ImmutableCharBoard& b) {
   int64_t ret = 0;
   for (int v_test = 0; v_test + 1 < b.width(); ++v_test) {
-    std::optional<Point> smudge;
-    for (Point p : b.range()) {
-      if (p.x > v_test) continue;
-      Point r = {(v_test - p.x) + v_test + 1, p.y};
-      if (!b.OnBoard(r)) continue;
-      if (b[p] != b[r]) {
-        if (smudge) {
-          smudge = std::nullopt;
-          break;
+    int diff = 0;;
+    for (int y = 0; y < b.height(); ++y) {
+      std::string_view row = b.row(y);
+      for (int x = v_test; x >= 0; --x) {
+        int test_x = 2 * v_test - x + 1;
+        if (test_x >= b.width()) break;
+        if (row[x] != row[test_x]) {
+          if (++diff > 1) {
+            break;
+          }
         }
-        smudge = p;
       }
+      if (diff > 1) break;
     }
-    if (smudge) {
+    if (diff == 1) {
       VLOG(1) << "v_test = " << v_test;
       ret += v_test + 1;
     }
   }
   for (int h_test = 0; h_test + 1 < b.height(); ++h_test) {
-    std::optional<Point> smudge;
-    for (Point p : b.range()) {
-      if (p.y > h_test) continue;
-      Point r = {p.x, (h_test - p.y) + h_test + 1 };
-      if (!b.OnBoard(r)) continue;
-      if (b[p] != b[r]) {
-        if (smudge) {
-          smudge = std::nullopt;
-          break;
+    int diff = 0;
+    for (int y = h_test; y >= 0; --y) {
+      int test_y = 2 * h_test - y + 1;
+      if (test_y >= b.height()) break;
+      std::string_view row1 = b.row(y);
+      std::string_view row2 = b.row(test_y);
+      for (int x = 0; x < b.width(); ++x) {
+        if (row1[x] != row2[x]) {
+          if (++diff > 1) {
+            break;
+          }
         }
-        smudge = p;
+      }
+      if (diff > 1) {
+        break;
       }
     }
-    if (smudge) {
+    if (diff == 1) {
       VLOG(1) << "h_test = " << h_test;
       ret += 100 * (h_test + 1);
     }
@@ -124,12 +131,12 @@ absl::StatusOr<std::string> Day_2023_13::Part1(
   int total = 0;
   for (int i = 0; i < input.size(); ++i) {
     if (input[i].empty()) {
-      ASSIGN_OR_RETURN(CharBoard b, CharBoard::Parse(input.subspan(last_empty + 1, i - last_empty - 1)));
+      ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(input.subspan(last_empty + 1, i - last_empty - 1)));
       total += ScoreReflection(b);
       last_empty = i;
     }
   }
-  ASSIGN_OR_RETURN(CharBoard b, CharBoard::Parse(input.subspan(last_empty + 1, input.size() - last_empty - 1)));
+  ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(input.subspan(last_empty + 1, input.size() - last_empty - 1)));
   total += ScoreReflection(b);
   return AdventReturn(total);
 }
@@ -140,12 +147,12 @@ absl::StatusOr<std::string> Day_2023_13::Part2(
   int total = 0;
   for (int i = 0; i < input.size(); ++i) {
     if (input[i].empty()) {
-      ASSIGN_OR_RETURN(CharBoard b, CharBoard::Parse(input.subspan(last_empty + 1, i - last_empty - 1)));
+      ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(input.subspan(last_empty + 1, i - last_empty - 1)));
       total += ScoreReflectionSmudge(b);
       last_empty = i;
     }
   }
-  ASSIGN_OR_RETURN(CharBoard b, CharBoard::Parse(input.subspan(last_empty + 1, input.size() - last_empty - 1)));
+  ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(input.subspan(last_empty + 1, input.size() - last_empty - 1)));
   total += ScoreReflectionSmudge(b);
   return AdventReturn(total);
 }
