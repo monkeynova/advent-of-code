7d39e399052add79e5a51d81e1726af464997d82
Keith Peters <keith@monkeynova.com>
2023-12-14 04:47:55 -0800

    2023/14 Answer

--- advent_of_code/2023/day14/day14.cc
+++ advent_of_code/2023/day14/day14.cc
@@ -28,18 +28,67 @@ namespace advent_of_code {
 
 namespace {
 
-// Helper methods go here.
+void Roll(CharBoard& b, Point dir) {
+  std::vector<Point> rocks = b.FindVec('O');
+  absl::c_sort(
+     rocks,
+     [dir](Point a, Point b) {
+       if (dir == Cardinal::kNorth) {
+         return a.y < b.y;
+       } else if (dir == Cardinal::kSouth) {
+         return a.y > b.y;
+       } else if (dir == Cardinal::kWest) {
+         return a.x < b.x;
+       } else if (dir == Cardinal::kEast) {
+         return a.x > b.x;
+       } else {
+         LOG(FATAL) << "dir: " << dir;
+       }
+     });
+
+  for (Point p : rocks) {
+    for (Point move = p + dir; 
+         b.OnBoard(move) && b[move] == '.';
+         move += dir, p += dir) {
+      b[move] = 'O';
+      b[p] = '.';
+    }
+  }
+}
+
+int CountLoad(const CharBoard& b) {
+  std::vector<Point> rocks = b.FindVec('O');
+  int load = 0;
+  for (Point p : rocks) {
+    load += b.height() - p.y;
+  }
+  return load;
+}
 
 }  // namespace
 
 absl::StatusOr<std::string> Day_2023_14::Part1(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  ASSIGN_OR_RETURN(CharBoard b, CharBoard::Parse(input));
+  Roll(b, Cardinal::kNorth);
+  VLOG(2) << "Moved:\n" << b;
+  return AdventReturn(CountLoad(b));
 }
 
 absl::StatusOr<std::string> Day_2023_14::Part2(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  ASSIGN_OR_RETURN(CharBoard b, CharBoard::Parse(input));
+  LoopHistory<CharBoard> hist;
+  for (int i = 0; i < 1000000000; ++i) {
+    if (hist.AddMaybeNew(b)) {
+      VLOG(1) << "Loop at " << i;
+      return AdventReturn(CountLoad(hist.FindInLoop(1000000000)));
+    }
+    for (Point dir : {Cardinal::kNorth, Cardinal::kWest, Cardinal::kSouth, Cardinal::kEast}) {
+      Roll(b, dir);
+    }
+  }
+  return AdventReturn(CountLoad(b));
 }
 
 }  // namespace advent_of_code
