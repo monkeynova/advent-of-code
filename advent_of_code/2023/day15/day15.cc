565fd032a53275e7eca1bd289eb8b7ac1dd63d2f
Keith Peters <keith@monkeynova.com>
2023-12-14 19:30:47 -0800

    2023/15 Empty

---
+++ advent_of_code/2023/day15/day15.cc
@@ -0,0 +1,45 @@
+// http://adventofcode.com/2023/day/15
+
+#include "advent_of_code/2023/day15/day15.h"
+
+#include "absl/algorithm/container.h"
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/log/log.h"
+#include "absl/strings/numbers.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_join.h"
+#include "absl/strings/str_split.h"
+#include "advent_of_code/bfs.h"
+#include "advent_of_code/char_board.h"
+#include "advent_of_code/conway.h"
+#include "advent_of_code/directed_graph.h"
+#include "advent_of_code/graph_walk.h"
+#include "advent_of_code/interval.h"
+#include "advent_of_code/loop_history.h"
+#include "advent_of_code/point.h"
+#include "advent_of_code/point3.h"
+#include "advent_of_code/point_walk.h"
+#include "advent_of_code/splice_ring.h"
+#include "advent_of_code/tokenizer.h"
+#include "re2/re2.h"
+
+namespace advent_of_code {
+
+namespace {
+
+// Helper methods go here.
+
+}  // namespace
+
+absl::StatusOr<std::string> Day_2023_15::Part1(
+    absl::Span<std::string_view> input) const {
+  return absl::UnimplementedError("Problem not known");
+}
+
+absl::StatusOr<std::string> Day_2023_15::Part2(
+    absl::Span<std::string_view> input) const {
+  return absl::UnimplementedError("Problem not known");
+}
+
+}  // namespace advent_of_code
