1e5f86637f58f166a3a0680ea25613e77596f75b
Keith Peters <keith@monkeynova.com>
2023-12-17 07:17:24 -0800

    2023/17 Performance (580ms -> 220ms)

--- advent_of_code/2023/day17/day17.cc
+++ advent_of_code/2023/day17/day17.cc
@@ -44,11 +44,10 @@ void AddRange(
     const CharBoard& b,
     State s, Dir dir,
     int start, int end,
-    absl::flat_hash_map<State, int>& heat_map,
+    std::vector<int>& heat_map,
     std::deque<State>& queue) {
-  auto it = heat_map.find(s);
-  CHECK(it != heat_map.end());
-  int heat = it->second;
+  int heat = heat_map[s.first * 4 + s.second];
+  CHECK_NE(heat, std::numeric_limits<int>::max());
   CHECK(s.second != dir);
   s.second = dir;
   const char* base = b.row(0).data();
@@ -67,10 +66,9 @@ void AddRange(
     heat += base[s.first] - '0';
     if (i + 1 < start) continue;
 
-    auto [it, inserted] = heat_map.emplace(s, heat);
-    if (inserted || it->second > heat) {
-      it->second = heat;
-      queue.push_back(s);
+    if (heat < heat_map[s.first * 4 + s.second]) {
+      heat_map[s.first * 4 + s.second] = heat;
+      queue.push_back(s); 
     }
   }
 }
@@ -81,26 +79,22 @@ absl::StatusOr<std::string> Day_2023_17::Part1(
     absl::Span<std::string_view> input) const {
   ASSIGN_OR_RETURN(CharBoard b, CharBoard::Parse(input));
   std::deque<State> queue;
-  absl::flat_hash_map<State, int> heat;
+  int stride = b.row(1).data() - b.row(0).data();
+  int max_idx = stride * (b.height() - 1) + b.width() - 1;
+  std::vector<int> heat_map(4 * (max_idx + 1), std::numeric_limits<int>::max());
   for (Dir d : {kEast, kSouth}) {
     State s = {0, kNorth};
-    heat[s] = 0;
-    AddRange(b, s, d, 1, 3, heat, queue);
+    heat_map[kNorth] = 0;
+    AddRange(b, s, d, 1, 3, heat_map, queue);
   }
   for (;!queue.empty(); queue.pop_front()) {
     const State& cur = queue.front();
-    AddRange(b, cur, kRotateLeft[cur.second], 1, 3, heat, queue);
-    AddRange(b, cur, kRotateRight[cur.second], 1, 3, heat, queue);
+    AddRange(b, cur, kRotateLeft[cur.second], 1, 3, heat_map, queue);
+    AddRange(b, cur, kRotateRight[cur.second], 1, 3, heat_map, queue);
   }
   int answer = std::numeric_limits<int>::max();
-  int stride = b.row(1).data() - b.row(0).data();
-  int end = (b.width() - 1) * stride + b.height() - 1;
-  for (Dir d : {kEast, kSouth}) {
-    auto it = heat.find({end, d});
-    if (it != heat.end()) {
-      answer = std::min(answer, it->second);
-    }
-  }
+  answer = std::min(answer, heat_map[max_idx * 4 + kEast]);
+  answer = std::min(answer, heat_map[max_idx * 4 + kSouth]);
   return AdventReturn(answer);
 }
 
@@ -108,26 +102,22 @@ absl::StatusOr<std::string> Day_2023_17::Part2(
     absl::Span<std::string_view> input) const {
   ASSIGN_OR_RETURN(CharBoard b, CharBoard::Parse(input));
   std::deque<State> queue;
-  absl::flat_hash_map<State, int> heat;
+  int stride = b.row(1).data() - b.row(0).data();
+  int max_idx = stride * (b.height() - 1) + b.width() - 1;
+  std::vector<int> heat_map(4 * (max_idx + 1), std::numeric_limits<int>::max());
   for (Dir d : {kEast, kSouth}) {
     State s = {0, kNorth};
-    heat[s] = 0;
-    AddRange(b, s, d, 4, 10, heat, queue);
+    heat_map[kNorth] = 0;
+    AddRange(b, s, d, 4, 10, heat_map, queue);
   }
   for (;!queue.empty(); queue.pop_front()) {
     const State& cur = queue.front();
-    AddRange(b, cur, kRotateLeft[cur.second], 4, 10, heat, queue);
-    AddRange(b, cur, kRotateRight[cur.second], 4, 10, heat, queue);
+    AddRange(b, cur, kRotateLeft[cur.second], 4, 10, heat_map, queue);
+    AddRange(b, cur, kRotateRight[cur.second], 4, 10, heat_map, queue);
   }
   int answer = std::numeric_limits<int>::max();
-  int stride = b.row(1).data() - b.row(0).data();
-  int end = (b.width() - 1) * stride + b.height() - 1;
-  for (Dir d : {kEast, kSouth}) {
-    auto it = heat.find({end, d});
-    if (it != heat.end()) {
-      answer = std::min(answer, it->second);
-    }
-  }
+  answer = std::min(answer, heat_map[max_idx * 4 + kEast]);
+  answer = std::min(answer, heat_map[max_idx * 4 + kSouth]);
   return AdventReturn(answer);
 }
 
