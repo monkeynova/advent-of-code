5965a0a01831d220992192969903b7a26aa3772f
Keith Peters <keith@monkeynova.com>
2023-12-22 11:32:37 -0800

    2023/22 Performance (11ms -> 7.5ms)

--- advent_of_code/2023/day22/day22.cc
+++ advent_of_code/2023/day22/day22.cc
@@ -60,10 +60,31 @@ absl::StatusOr<std::vector<Cube>> Parse(absl::Span<std::string_view> input) {
   return list;
 }
 
-struct SupportGraph {
-  absl::flat_hash_map<int, absl::flat_hash_set<int>> supports;
-  absl::flat_hash_map<int, absl::flat_hash_set<int>> supported_by;
+class SupportGraph {
+ public:
+  explicit SupportGraph(int size)
+   : supports_(size + 1), supported_by_(size + 1) {}
+
+  void Add(int supportee, int supported) {
+    supports_[supportee].push_back(supported);
+    supported_by_[supported ].push_back(supportee);
+  }
+
+  const std::vector<int>& Supports(int supported) {
+    return supports_[supported];
+  }
+
+  const std::vector<int>& SupportedBy(int supportee) {
+    return supported_by_[supportee];
+  }
+
+ private:
+  static const std::vector<int> kEmpty;
+  std::vector<std::vector<int>> supports_;
+  std::vector<std::vector<int>> supported_by_;
 };
+
+const std::vector<int> SupportGraph::kEmpty;
 
 SupportGraph Drop(std::vector<Cube>& list) {
   VLOG(1) << "Drop Start";
@@ -74,21 +95,29 @@ SupportGraph Drop(std::vector<Cube>& list) {
     r.ExpandInclude({c.max.x, c.max.y});
   }
   CHECK(r.min == Cardinal::kOrigin);
-  std::vector<std::vector<int>> heights(r.max.y + 1, std::vector<int>(r.max.x + 1, 0));
-  std::vector<std::vector<int>> support(r.max.y + 1, std::vector<int>(r.max.x + 1, -1));
 
-  SupportGraph ret;
+  const int kFloorIndex = list.size();
+
+  std::vector<std::vector<int>> heights(
+      r.max.y + 1, std::vector<int>(r.max.x + 1, 0));
+  std::vector<std::vector<int>> support(
+      r.max.y + 1, std::vector<int>(r.max.x + 1, kFloorIndex));
+
+  SupportGraph ret(list.size() + 1);
   for (int i = 0; i < list.size(); ++i) {
-    PointRectangle xy = {{list[i].min.x, list[i].min.y}, {list[i].max.x, list[i].max.y}};
+    PointRectangle xy =
+        {{list[i].min.x, list[i].min.y}, {list[i].max.x, list[i].max.y}};
     int out_z = 0;
     for (Point p : xy) {
       out_z = std::max(out_z, heights[p.y][p.x]);
     }
+    std::vector<bool> inserted(list.size() + 1, false);
     for (Point p : xy) {
       if (heights[p.y][p.x] == out_z) {
         int j = support[p.y][p.x];
-        ret.supports[j].insert(i);
-        ret.supported_by[i].insert(j);
+        if (inserted[j]) continue;
+        inserted[j] = true;
+        ret.Add(j, i);
       }
     }
     list[i].max.z -= list[i].min.z - (out_z + 1);
@@ -113,20 +142,9 @@ absl::StatusOr<std::string> Day_2023_22::Part1(
 
   int disintigrable = 0;
   for (int i = 0; i < list.size(); ++i) {
-    auto it1 = support.supports.find(i);
-    if (it1 == support.supports.end()) {
-      VLOG(2) << i << ": No supports";
-      VLOG(2) << list[i].min << "-" << list[i].max;
-      ++disintigrable;
-      continue;
-    }
     bool all_multi_support = true;
-    for (int s : it1->second) {
-      auto it2 = support.supported_by.find(s);
-      if (it2 == support.supported_by.end()) return Error("Integrity check");
-      if (it2->second.size() == 0) return Error("Integrity check");
-      if (it2->second.size() == 1) {
-        if (!it2->second.contains(i)) return Error("Integrity check");
+    for (int s : support.Supports(i)) {
+      if (support.SupportedBy(s).size() == 1) {
         all_multi_support = false;
         break;
       }
@@ -153,14 +171,9 @@ absl::StatusOr<std::string> Day_2023_22::Part2(
     absl::flat_hash_set<int> falling = {i};
 
     for (; !queue.empty(); queue.pop_front()) {
-      auto it = support.supports.find(queue.front());
-      if (it == support.supports.end()) continue;
-
-      for (int s : it->second) {
+      for (int s : support.Supports(queue.front())) {
         bool supported = false;
-        auto it2 = support.supported_by.find(s);
-        CHECK(it2 != support.supported_by.end());
-        for (int os : it2->second) {
+        for (int os : support.SupportedBy(s)) {
           if (!falling.contains(os)) {
             supported = true;
           }
