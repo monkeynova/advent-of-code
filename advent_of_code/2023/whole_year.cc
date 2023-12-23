0bb899df8c1af481c184635184d630e2c7265e7a
Keith Peters <keith@monkeynova.com>
2023-12-23 06:30:43 -0800

    2023/23 -> whole_year_bin

--- advent_of_code/2023/whole_year.cc
+++ advent_of_code/2023/whole_year.cc
@@ -22,6 +22,7 @@
 #include "advent_of_code/2023/day20/day20.h"
 #include "advent_of_code/2023/day21/day21.h"
 #include "advent_of_code/2023/day22/day22.h"
+#include "advent_of_code/2023/day23/day23.h"
 #include "advent_of_code/infra/file_util.h"
 #include "advent_of_code/vlog.h"
 #include "benchmark/benchmark.h"
@@ -55,6 +56,7 @@ absl::Span<const std::unique_ptr<AdventDay>> AllDays() {
     days.emplace_back(new Day_2023_21());
     days.back()->set_param("64,26501365");
     days.emplace_back(new Day_2023_22());
+    days.emplace_back(new Day_2023_23());
     return days;
   }();
   return days;
