0870665135c06627e8cca41e2ec75f4b2d6df91a
Keith Peters <keith@monkeynova.com>
2023-12-27 12:32:23 -0800

    2023/07 get_problem

--- advent_of_code/2023/day07/day07.h
+++ advent_of_code/2023/day07/day07.h
@@ -7,6 +7,10 @@ namespace advent_of_code {
 
 class Day_2023_07 : public AdventDay {
  public:
+  virtual std::string_view title() const override {
+    return "Day 7: Camel Cards";
+  }
+
   virtual std::string_view test_file() const override {
     return "advent_of_code/2023/day07/day07.test";
   }
