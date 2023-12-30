f3bd13b72038aeac66d7792bd99de752f1d6b9a0
Keith Peters <keith@monkeynova.com>
2023-12-30 12:35:12 -0800

    Update new_day to current standards

--- advent_of_code/new_day_path/new_day_test.cc
+++ advent_of_code/new_day_path/new_day_test.cc
@@ -1,12 +1,14 @@
 #include "advent_of_code/new_day_path/new_day.h"
 
+#include "advent_of_code/infra/file_benchmark.h"
 #include "advent_of_code/infra/file_test.h"
 #include "gmock/gmock.h"
 
 namespace advent_of_code {
 
-using DayTest = FileTest<NewDay>;
+TEST_F(FileTest_Day, FileBasedTest) { EXPECT_TRUE(RunTest<NewDay>()); }
 
-TEST_F(DayTest, FileBasedTest) { EXPECT_TRUE(RunTest()); }
+BENCHMARK_TEMPLATE(BM_Day, NewDay)
+    ->DenseRange(0, FileBenchmarkMaxIdx<NewDay>());
 
 }  // namespace advent_of_code
