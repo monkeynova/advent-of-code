426fff2771c233741660b8c06116556bd82842cc
Keith Peters <keith@monkeynova.com>
2024-12-26 15:49:32 -0800

    2024/02 Answer

--- advent_of_code/2024/day02/day02.cc
+++ advent_of_code/2024/day02/day02.cc
@@ -29,18 +29,107 @@ namespace advent_of_code {
 
 namespace {
 
-// Helper methods go here.
+struct Delta {
+  Delta(int m1, int m2) {
+    if (m1 > m2) { sign = 1; mag = m1 - m2; }
+    else if (m1 < m2) { sign = -1; mag = m2 - m1; }
+    else { sign = 0; mag = 0; }
+  }
+  int sign;
+  int mag;
+
+  bool Valid() {
+    return mag >= 1 && mag <= 3;
+  }
+};
+
+absl::StatusOr<bool> CheckLine(Delta test, int last, Tokenizer& t) {
+  while (!t.Done()) {
+    ASSIGN_OR_RETURN(int next, t.NextInt());
+    Delta delta(last, next);
+    if (!delta.Valid()) {
+      return false;
+    }
+    if (test.sign != delta.sign) {
+      return false;
+    }
+    last = next;
+  }
+  return true;
+}
+
+bool CheckList(std::vector<int> list) {
+  for (int i = 1; i + 1 < list.size(); ++i) {
+    Delta d1(list[i - 1], list[i]);
+    Delta d2(list[i], list[i + 1]);
+    if (!d1.Valid()) return false;
+    if (!d2.Valid()) return false;
+    if (d1.sign != d2.sign) return false;
+  }
+  return true;
+}
+
+bool CheckListWithSkip(std::vector<int> list, int skip) {
+  std::vector<int> skipped;
+  for (int i = 0; i < list.size(); ++i) {
+    if (i == skip) continue;
+    skipped.push_back(list[i]);
+  }
+  return CheckList(skipped);
+}
+
+bool CheckListAllowFail(std::vector<int> list) {
+  for (int i = 1; i < list.size() - 1; ++i) {
+    Delta d1(list[i - 1], list[i]);
+    Delta d2(list[i], list[i + 1]);
+    if (!d1.Valid()) {
+      return CheckListWithSkip(list, i - 1) || CheckListWithSkip(list, i);
+    }
+    if (!d2.Valid()) {
+      return CheckListWithSkip(list, i) || CheckListWithSkip(list, i + 1);      
+    }
+    if (d1.sign != d2.sign) {
+      return CheckListWithSkip(list, i - 1) || CheckListWithSkip(list, i) || CheckListWithSkip(list, i + 1);      
+
+    }
+  }
+  return true;
+}
 
 }  // namespace
 
 absl::StatusOr<std::string> Day_2024_02::Part1(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  int safe_count = 0;
+  for (absl::string_view line : input) {
+    Tokenizer t(line);
+    std::vector<int> list;
+    while (!t.Done()) {
+      ASSIGN_OR_RETURN(int val, t.NextInt());
+      list.push_back(val);
+    }
+    if (CheckList(list)) {
+      ++safe_count;
+    }
+  }
+  return AdventReturn(safe_count);
 }
 
 absl::StatusOr<std::string> Day_2024_02::Part2(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  int safe_count = 0;
+  for (absl::string_view line : input) {
+    Tokenizer t(line);
+    std::vector<int> list;
+    while (!t.Done()) {
+      ASSIGN_OR_RETURN(int val, t.NextInt());
+      list.push_back(val);
+    }
+    if (CheckListAllowFail(list)) {
+      ++safe_count;
+    }
+  }
+  return AdventReturn(safe_count);
 }
 
 static AdventRegisterEntry registry = RegisterAdventDay(
