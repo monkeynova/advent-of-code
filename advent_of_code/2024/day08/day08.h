1091f85d7e693158144306db38ee53a23b3284c2
Keith Peters <keith@monkeynova.com>
2024-12-27 13:31:27 -0800

    2024/08 get_problem

--- advent_of_code/2024/day08/day08.h
+++ advent_of_code/2024/day08/day08.h
@@ -7,7 +7,9 @@ namespace advent_of_code {
 
 class Day_2024_08 : public AdventDay {
  public:
-  virtual std::string_view title() const override { return "Day 8: ???"; }
+  virtual std::string_view title() const override {
+    return "Day 8: Resonant Collinearity";
+  }
 
   virtual std::string_view test_file() const override {
     return "advent_of_code/2024/day08/day08.test";
