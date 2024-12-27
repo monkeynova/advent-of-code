b3b5e328d1e5f8ae5e38dc6a8959b6a44fab50b0
Keith Peters <keith@monkeynova.com>
2024-12-27 13:29:32 -0800

    2024/08 Answer

--- advent_of_code/2024/day08/day08.cc
+++ advent_of_code/2024/day08/day08.cc
@@ -35,12 +35,51 @@ namespace {
 
 absl::StatusOr<std::string> Day_2024_08::Part1(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(input));
+  absl::flat_hash_map<char, std::vector<Point>> antenae;
+  for (Point p : b.range()) {
+    if (b[p] != '.') antenae[b[p]].push_back(p);
+  }
+  absl::flat_hash_set<Point> antinodes;
+  for (const auto& [_, p_list] : antenae) {
+    for (int i = 0; i < p_list.size(); ++i) {
+      for (int j = i + 1; j < p_list.size(); ++j) {
+        Point p1 = p_list[i];
+        Point p2 = p_list[j];
+        Point a1 = p1 + (p1 - p2);
+        Point a2 = p2 + (p2 - p1);
+        if (b.OnBoard(a1)) antinodes.insert(a1);
+        if (b.OnBoard(a2)) antinodes.insert(a2);
+      }
+    }
+  }
+  return AdventReturn(antinodes.size());
 }
 
 absl::StatusOr<std::string> Day_2024_08::Part2(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(input));
+  absl::flat_hash_map<char, std::vector<Point>> antenae;
+  for (Point p : b.range()) {
+    if (b[p] != '.') antenae[b[p]].push_back(p);
+  }
+  absl::flat_hash_set<Point> antinodes;
+  for (const auto& [_, p_list] : antenae) {
+    for (int i = 0; i < p_list.size(); ++i) {
+      for (int j = i + 1; j < p_list.size(); ++j) {
+        Point p1 = p_list[i];
+        Point p2 = p_list[j];
+        Point d = (p1 - p2).min_step();
+        for (Point p3 = p1; b.OnBoard(p3); p3 += d) {
+          antinodes.insert(p3);
+        }
+        for (Point p3 = p1; b.OnBoard(p3); p3 -= d) {
+          antinodes.insert(p3);
+        }
+      }
+    }
+  }
+  return AdventReturn(antinodes.size());
 }
 
 static AdventRegisterEntry registry = RegisterAdventDay(
