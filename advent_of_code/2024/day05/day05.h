9def73f9b3b020a120aff46558347656ebbc6bb5
Keith Peters <keith@monkeynova.com>
2024-12-27 08:12:02 -0800

    2024/05 get_problem

--- advent_of_code/2024/day05/day05.h
+++ advent_of_code/2024/day05/day05.h
@@ -7,7 +7,9 @@ namespace advent_of_code {
 
 class Day_2024_05 : public AdventDay {
  public:
-  virtual std::string_view title() const override { return "Day 5: ???"; }
+  virtual std::string_view title() const override {
+    return "Day 5: Print Queue";
+  }
 
   virtual std::string_view test_file() const override {
     return "advent_of_code/2024/day05/day05.test";
