e8dd2e6a9ff2b5da3545732b7193c7b4f42022ef
Keith Peters <keith@monkeynova.com>
2024-12-27 08:11:11 -0800

    2024/05 Answer

--- advent_of_code/2024/day05/day05.cc
+++ advent_of_code/2024/day05/day05.cc
@@ -35,12 +35,102 @@ namespace {
 
 absl::StatusOr<std::string> Day_2024_05::Part1(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  std::vector<std::pair<int, int>> rules;
+  bool parse_rules = true;
+  int sum = 0;
+  for (std::string_view line : input) {
+    if (parse_rules) {
+      if (line.empty()) {
+        parse_rules = false;
+      } else {
+        ASSIGN_OR_RETURN(std::vector<int64_t> rule,
+                         ParseAsInts(absl::StrSplit(line, "|")));
+        if (rule.size() != 2) return absl::InvalidArgumentError("bad line");
+        rules.emplace_back(rule[0], rule[1]);
+      }
+      continue;
+    }
+
+    ASSIGN_OR_RETURN(std::vector<int64_t> ordered,
+                     ParseAsInts(absl::StrSplit(line, ",")));
+    int valid = true;
+    for (int i = 0; valid && i < ordered.size(); ++i) {
+      for (const auto& [pre, post] : rules) {
+        if (pre == ordered[i]) {
+          for (int j = 0; j < i; ++j) {
+            if (post == ordered[j]) {
+              valid = false;
+              break;
+            }
+          }
+        }
+        if (post == ordered[i]) {
+          for (int j = i + 1; j < ordered.size(); ++j) {
+            if (pre == ordered[j]) {
+              valid = false;
+              break;
+            }
+          }
+        }
+      }
+
+    }
+    if (!valid) continue;
+    sum += ordered[ordered.size() / 2];
+  }
+  return AdventReturn(sum);
 }
 
 absl::StatusOr<std::string> Day_2024_05::Part2(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  std::vector<std::pair<int, int>> rules;
+  bool parse_rules = true;
+  int sum = 0;
+  for (std::string_view line : input) {
+    if (parse_rules) {
+      if (line.empty()) {
+        parse_rules = false;
+      } else {
+        ASSIGN_OR_RETURN(std::vector<int64_t> rule,
+                         ParseAsInts(absl::StrSplit(line, "|")));
+        if (rule.size() != 2) return absl::InvalidArgumentError("bad line");
+        rules.emplace_back(rule[0], rule[1]);
+      }
+      continue;
+    }
+
+    ASSIGN_OR_RETURN(std::vector<int64_t> ordered,
+                     ParseAsInts(absl::StrSplit(line, ",")));
+    int valid = true;
+  RETRY:
+    for (int i = 0; i < ordered.size(); ++i) {
+      for (const auto& [pre, post] : rules) {
+        if (pre == ordered[i]) {
+          for (int j = 0; j < i; ++j) {
+            if (post == ordered[j]) {
+              valid = false;
+              std::swap(ordered[i], ordered[j]);
+              goto RETRY;
+            }
+          }
+        }
+        if (post == ordered[i]) {
+          for (int j = i + 1; j < ordered.size(); ++j) {
+            if (pre == ordered[j]) {
+              valid = false;
+              std::swap(ordered[i], ordered[j]);
+              goto RETRY;
+            }
+          }
+        }
+      }
+
+    }
+    if (valid) continue;
+
+    sum += ordered[ordered.size() / 2];
+  }
+  return AdventReturn(sum);
 }
 
 static AdventRegisterEntry registry = RegisterAdventDay(
