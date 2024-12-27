c4656f3d1163f83b02dda57c78fd35ad95d0a854
Keith Peters <keith@monkeynova.com>
2024-12-27 09:33:28 -0800

    2024/06 Include prune

--- advent_of_code/2024/day06/day06.cc
+++ advent_of_code/2024/day06/day06.cc
@@ -2,28 +2,9 @@
 
 #include "advent_of_code/2024/day06/day06.h"
 
-#include "absl/algorithm/container.h"
-#include "absl/container/flat_hash_map.h"
-#include "absl/container/flat_hash_set.h"
 #include "absl/log/log.h"
-#include "absl/strings/numbers.h"
-#include "absl/strings/str_cat.h"
-#include "absl/strings/str_join.h"
-#include "absl/strings/str_split.h"
-#include "advent_of_code/bfs.h"
 #include "advent_of_code/char_board.h"
-#include "advent_of_code/conway.h"
-#include "advent_of_code/directed_graph.h"
 #include "advent_of_code/fast_board.h"
-#include "advent_of_code/interval.h"
-#include "advent_of_code/loop_history.h"
-#include "advent_of_code/mod.h"
-#include "advent_of_code/point.h"
-#include "advent_of_code/point3.h"
-#include "advent_of_code/point_walk.h"
-#include "advent_of_code/splice_ring.h"
-#include "advent_of_code/tokenizer.h"
-#include "re2/re2.h"
 
 namespace advent_of_code {
 
