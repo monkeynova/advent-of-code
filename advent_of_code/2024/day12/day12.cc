6409d281d4b6a50dce1c98138a39cfcf263fc54c
Keith Peters <keith@monkeynova.com>
2024-12-28 12:35:50 -0800

    2024/12 Answer

--- advent_of_code/2024/day12/day12.cc
+++ advent_of_code/2024/day12/day12.cc
@@ -29,18 +29,90 @@ namespace advent_of_code {
 
 namespace {
 
-// Helper methods go here.
+absl::flat_hash_set<Point> Contiguous(
+    const ImmutableCharBoard& b, Point p) {
+  std::vector<Point> frontier = {p};
+  absl::flat_hash_set<Point> ret = {p};
+  while (!frontier.empty()) {
+    std::vector<Point> new_frontier;
+    for (Point p : frontier) {
+      for (Point d : Cardinal::kFourDirs) {
+        Point t = p + d;
+        if (b.OnBoard(t) && b[p] == b[t] && !ret.contains(t)) {
+          new_frontier.push_back(t);
+          ret.insert(t);
+        }
+      }
+    }
+    frontier = std::move(new_frontier);
+  }
+
+  return ret;
+}
+
+int64_t Score(const absl::flat_hash_set<Point>& set) {
+  int area = set.size();
+  int perimeter = 0;
+  for (Point p : set) {
+    for (Point d : Cardinal::kFourDirs) {
+      if (!set.contains(p + d)) ++perimeter;
+    }
+  }
+  return area * perimeter;
+}
+
+int64_t Score2(const absl::flat_hash_set<Point>& set) {
+  int area = set.size();
+  int sides = 0;
+  for (Point p : set) {
+    for (Point d : Cardinal::kFourDirs) {
+      if (!set.contains(p + d)) {
+        Point ld = d.rotate_left();
+        if (!set.contains(p + ld) ||
+            set.contains(p + d + ld)) {
+          ++sides;
+        }
+      }
+    }
+  }
+  return area * sides;
+}
+
 
 }  // namespace
 
 absl::StatusOr<std::string> Day_2024_12::Part1(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(input));
+  absl::flat_hash_set<Point> used;
+  int total_score = 0;
+  for (Point p : b.range()) {
+    if (used.contains(p)) continue;
+    absl::flat_hash_set<Point> contiguous = Contiguous(b, p);
+    for (Point p : contiguous) {
+      auto [it, inserted] = used.insert(p);
+      if (!inserted) return absl::InternalError("used?");
+    }
+    total_score += Score(contiguous);
+  }
+  return AdventReturn(total_score);
 }
 
 absl::StatusOr<std::string> Day_2024_12::Part2(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(input));
+  absl::flat_hash_set<Point> used;
+  int total_score = 0;
+  for (Point p : b.range()) {
+    if (used.contains(p)) continue;
+    absl::flat_hash_set<Point> contiguous = Contiguous(b, p);
+    for (Point p : contiguous) {
+      auto [it, inserted] = used.insert(p);
+      if (!inserted) return absl::InternalError("used?");
+    }
+    total_score += Score2(contiguous);
+  }
+  return AdventReturn(total_score);
 }
 
 static AdventRegisterEntry registry = RegisterAdventDay(
