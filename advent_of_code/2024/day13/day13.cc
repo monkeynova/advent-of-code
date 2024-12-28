9c7254b48547d348b5d0c069e8d19a90380c8e27
Keith Peters <keith@monkeynova.com>
2024-12-28 14:21:35 -0800

    2024/13 Answer

--- advent_of_code/2024/day13/day13.cc
+++ advent_of_code/2024/day13/day13.cc
@@ -29,18 +29,128 @@ namespace advent_of_code {
 
 namespace {
 
-// Helper methods go here.
+class PrizeWalk : public BFSInterface<PrizeWalk, Point> {
+ public:
+  PrizeWalk(Point a, Point b, Point prize)
+      : a_(a), b_(b), prize_(prize) {}
+
+  Point identifier() const override { return cur_; }
+  bool IsFinal() const override { return cur_ == prize_; }
+  void AddNextSteps(State* state) const override {
+    Point t = cur_ + a_;
+    if (t.x <= prize_.x && t.y <= prize_.y) {
+      PrizeWalk next = *this;
+      next.cur_ = t;
+      next.add_steps(2);
+      state->AddNextStep(next);
+    }
+    t = cur_ + b_;
+    if (t.x <= prize_.x && t.y <= prize_.y) {
+      PrizeWalk next = *this;
+      next.cur_ = t;
+      next.cost_ += 1;
+      state->AddNextStep(next);
+    }
+  }
+
+ private:
+  int cost_ = 0;
+  Point cur_ = {0, 0};
+  Point a_;
+  Point b_;
+  Point prize_;
+};
+
+std::optional<int64_t> FindCost(Point a, Point b, Point prize) {
+  // a_c * a_x + b_c * b_x = p_x
+  // a_c * a_y + b_c * b_y = p_y
+
+  // bc * (bx * ay - by * ax) = px * ay - py * ax
+  // bc = px * ay - py * ax / (bx * ay - by * ax)
+  
+  // ac * (ax*by - ay *bx) = px * by - py * bx
+  // ac = px * by - py * bx / (ax*by - ay *bx)
+
+  int64_t b_c = prize.x * a.y - prize.y * a.x;
+  int64_t b_c_d = b.x * a.y - b.y * a.x;
+  if (b_c % b_c_d != 0) return std::nullopt;
+  b_c /= b_c_d;
+  if (b_c < 0) return std::nullopt;
+
+  int64_t a_c = prize.x * b.y - prize.y * b.x;
+  int64_t a_c_d = a.x * b.y - a.y * b.x;
+  if (a_c % a_c_d != 0) return std::nullopt;
+  a_c /= a_c_d;
+  if (a_c < 0) return std::nullopt;
+
+  return 3 * a_c + b_c;
+}
+
+std::optional<int64_t> FindCost2(Point a, Point b, Point prize) {
+  const int64_t kDelta = 10000000000000;
+
+  int64_t b_c = (prize.x + kDelta) * a.y - (prize.y + kDelta) * a.x;
+  int64_t b_c_d = b.x * a.y - b.y * a.x;
+  if (b_c % b_c_d != 0) return std::nullopt;
+  b_c /= b_c_d;
+  if (b_c < 0) return std::nullopt;
+
+  int64_t a_c = (prize.x + kDelta) * b.y - (prize.y + kDelta) * b.x;
+  int64_t a_c_d = a.x * b.y - a.y * b.x;
+  if (a_c % a_c_d != 0) return std::nullopt;
+  a_c /= a_c_d;
+  if (a_c < 0) return std::nullopt;
+
+  return 3 * a_c + b_c;
+}
+
 
 }  // namespace
 
 absl::StatusOr<std::string> Day_2024_13::Part1(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  if (input.size() % 4 != 3) return absl::InvalidArgumentError("% 4");
+
+  int64_t total_cost = 0;
+  for (int i = 0; i < input.size(); i += 4) {
+    Point a;
+    Point b;
+    Point prize;
+    if (!RE2::FullMatch(input[i], "Button A: X\\+(\\d+), Y\\+(\\d+)", &a.x, &a.y)) {
+      return absl::InvalidArgumentError("parse a");
+    }
+    if (!RE2::FullMatch(input[i + 1], "Button B: X\\+(\\d+), Y\\+(\\d+)", &b.x, &b.y)) {
+      return absl::InvalidArgumentError("parse b");
+    }
+    if (!RE2::FullMatch(input[i + 2], "Prize: X=(\\d+), Y=(\\d+)", &prize.x, &prize.y)) {
+      return absl::InvalidArgumentError("parse prize");
+    }
+    std::optional<int64_t> cost = FindCost(a, b, prize); //PrizeWalk(a, b, prize).FindMinStepsAStar();
+    if (cost) total_cost += *cost;
+  }
+  return AdventReturn(total_cost);
 }
 
 absl::StatusOr<std::string> Day_2024_13::Part2(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  int64_t total_cost = 0;
+  for (int i = 0; i < input.size(); i += 4) {
+    Point a;
+    Point b;
+    Point prize;
+    if (!RE2::FullMatch(input[i], "Button A: X\\+(\\d+), Y\\+(\\d+)", &a.x, &a.y)) {
+      return absl::InvalidArgumentError("parse a");
+    }
+    if (!RE2::FullMatch(input[i + 1], "Button B: X\\+(\\d+), Y\\+(\\d+)", &b.x, &b.y)) {
+      return absl::InvalidArgumentError("parse b");
+    }
+    if (!RE2::FullMatch(input[i + 2], "Prize: X=(\\d+), Y=(\\d+)", &prize.x, &prize.y)) {
+      return absl::InvalidArgumentError("parse prize");
+    }
+    std::optional<int64_t> cost = FindCost2(a, b, prize);
+    if (cost) total_cost += *cost;
+  }
+  return AdventReturn(total_cost);
 }
 
 static AdventRegisterEntry registry = RegisterAdventDay(
