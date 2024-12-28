ee2ed7d048cbb161677877ee371f69a083cfe4f6
Keith Peters <keith@monkeynova.com>
2024-12-28 09:55:08 -0800

    2024/11 Answer

--- advent_of_code/2024/day11/day11.cc
+++ advent_of_code/2024/day11/day11.cc
@@ -29,18 +29,80 @@ namespace advent_of_code {
 
 namespace {
 
+std::vector<int64_t> NextStones(int64_t stone) {
+  if (stone == 0) return {1};
+
+  int64_t pow10_half = 1;
+  int base = 0;
+  for (int64_t pow10 = 1;; pow10 *= 10) {
+    if (pow10 > stone) {
+      if (base % 2 == 0) {
+       return {stone / pow10_half, stone % pow10_half};
+      }
+      break;
+    }
+    if (base % 2 == 1) {
+      pow10_half *= 10;
+    }
+    ++base;
+  }
+
+  return {stone * 2024};
+}
+
+absl::flat_hash_map<int64_t, int64_t> RunStep(
+    const absl::flat_hash_map<int64_t, int64_t>& stones) {
+  absl::flat_hash_map<int64_t, int64_t> new_stones;
+  for (const auto& [s, c] : stones) {
+    for (int64_t new_s : NextStones(s)) {
+      new_stones[new_s] += c;
+    }
+  }
+  return new_stones;
+}
+
 // Helper methods go here.
 
 }  // namespace
 
 absl::StatusOr<std::string> Day_2024_11::Part1(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  if (input.size() != 1) return absl::InvalidArgumentError("1 line?");
+  ASSIGN_OR_RETURN(std::vector<int64_t> in_stones,
+                   ParseAsInts(absl::StrSplit(input[0], " ")));
+  absl::flat_hash_map<int64_t, int64_t> stones;
+  for (int64_t s : in_stones) ++stones[s];
+
+  for (int i = 0; i < 25; ++i) {
+    stones = RunStep(stones);
+  }
+  int64_t total_count = absl::c_accumulate(
+    stones, int64_t{0},
+    [](int64_t a, const std::pair<int64_t, int64_t>& s_and_c) {
+      return a + s_and_c.second;
+    });
+
+  return AdventReturn(total_count);
 }
 
 absl::StatusOr<std::string> Day_2024_11::Part2(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  if (input.size() != 1) return absl::InvalidArgumentError("1 line?");
+  ASSIGN_OR_RETURN(std::vector<int64_t> in_stones,
+                   ParseAsInts(absl::StrSplit(input[0], " ")));
+  absl::flat_hash_map<int64_t, int64_t> stones;
+  for (int64_t s : in_stones) ++stones[s];
+
+  for (int i = 0; i < 75; ++i) {
+    stones = RunStep(stones);
+  }
+  int64_t total_count = absl::c_accumulate(
+    stones, int64_t{0},
+    [](int64_t a, const std::pair<int64_t, int64_t>& s_and_c) {
+      return a + s_and_c.second;
+    });
+
+  return AdventReturn(total_count);
 }
 
 static AdventRegisterEntry registry = RegisterAdventDay(
