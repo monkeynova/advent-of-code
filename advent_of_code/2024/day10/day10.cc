0a58ce4d24dff5ede3f6fc90fee9492e9e338c11
Keith Peters <keith@monkeynova.com>
2024-12-27 17:30:12 -0800

    2024/10 Answer

--- advent_of_code/2024/day10/day10.cc
+++ advent_of_code/2024/day10/day10.cc
@@ -29,18 +29,64 @@ namespace advent_of_code {
 
 namespace {
 
-// Helper methods go here.
+int Score(const ImmutableCharBoard& b, Point p) {
+  absl::flat_hash_set<Point> cur = {p};
+  for (int i = 0; i < 9; ++i) {
+    absl::flat_hash_set<Point> next;
+    for (Point p : cur) {
+      for (Point dir : Cardinal::kFourDirs) {
+        Point t = p + dir;
+        if (b.OnBoard(t) && b[t] == b[p] + 1) {
+          next.insert(t);
+        }
+      }
+    }
+    cur = std::move(next);
+  }
+  return cur.size();
+}
+
+int Rating(const ImmutableCharBoard& b, Point p) {
+  absl::flat_hash_set<std::vector<Point>> cur = {std::vector{p}};
+  for (int i = 0; i < 9; ++i) {
+    absl::flat_hash_set<std::vector<Point>> next;
+    for (const std::vector<Point>& path : cur) {
+      for (Point dir : Cardinal::kFourDirs) {
+        Point t = path.back() + dir;
+        if (b.OnBoard(t) && b[t] == b[path.back()] + 1) {
+          std::vector<Point> next_path = path;
+          next_path.push_back(t);
+          next.insert(next_path);
+        }
+      }
+    }
+    cur = std::move(next);
+  }
+  return cur.size();
+}
 
 }  // namespace
 
 absl::StatusOr<std::string> Day_2024_10::Part1(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(input));
+  absl::flat_hash_set<Point> trailheads = b.Find('0');
+  int total_score = 0;
+  for (Point p : trailheads) {
+    total_score += Score(b, p);
+  }
+  return AdventReturn(total_score);
 }
 
 absl::StatusOr<std::string> Day_2024_10::Part2(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  ASSIGN_OR_RETURN(ImmutableCharBoard b, ImmutableCharBoard::Parse(input));
+  absl::flat_hash_set<Point> trailheads = b.Find('0');
+  int total_score = 0;
+  for (Point p : trailheads) {
+    total_score += Rating(b, p);
+  }
+  return AdventReturn(total_score);
 }
 
 static AdventRegisterEntry registry = RegisterAdventDay(
