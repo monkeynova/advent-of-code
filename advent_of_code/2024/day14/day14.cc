627123201538feef9e40f42ae219b19267d9c1f9
Keith Peters <keith@monkeynova.com>
2024-12-29 08:01:19 -0800

    2024/14 Answer

--- advent_of_code/2024/day14/day14.cc
+++ advent_of_code/2024/day14/day14.cc
@@ -29,18 +29,102 @@ namespace advent_of_code {
 
 namespace {
 
-// Helper methods go here.
+struct Drone {
+  Point p;
+  Point v;
+};
 
 }  // namespace
 
 absl::StatusOr<std::string> Day_2024_14::Part1(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  Point tile;
+  std::vector<Drone> drones;
+  if (!RE2::FullMatch(param(), "(\\d+,\\d+)", tile.Capture())) {
+    return absl::InternalError("bad param");
+  }
+  if (tile.x % 2 != 1) return absl::InternalError("bad param %x");
+  if (tile.y % 2 != 1) return absl::InternalError("bad param %y");
+
+  for (std::string_view line : input) {
+    Drone d;
+    if (!RE2::FullMatch(line, "p=(-?\\d+,-?\\d+) v=(-?\\d+,-?\\d+)", d.p.Capture(), d.v.Capture())) {
+      return absl::InvalidArgumentError("bad line");
+    }
+    drones.push_back(d);
+  }
+  for (int i = 0; i < 100; ++i) {
+    for (Drone& d : drones) {
+      d.p += d.v;
+      d.p.x %= tile.x;
+      if (d.p.x < 0) d.p.x += tile.x;
+      d.p.y %= tile.y;
+      if (d.p.y < 0) d.p.y += tile.y;
+    } 
+  }
+  std::array<int, 4> quadrant_count = {0, 0, 0, 0};
+  for (const Drone& d : drones) {
+    if (d.p.x < tile.x / 2) {
+      if (d.p.y < tile.y / 2) {
+        ++quadrant_count[0];
+      } else if (d.p.y > tile.y / 2) {
+        ++quadrant_count[1];
+      }
+    } else if (d.p.x > tile.x / 2) {
+      if (d.p.y < tile.y / 2) {
+        ++quadrant_count[2];
+      } else if (d.p.y > tile.y / 2) {
+        ++quadrant_count[3];
+      }
+    }
+  }
+  int score = quadrant_count[0] * quadrant_count[1] * quadrant_count[2] * quadrant_count[3];
+  return AdventReturn(score);
 }
 
 absl::StatusOr<std::string> Day_2024_14::Part2(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  Point tile;
+  std::vector<Drone> drones;
+  if (!RE2::FullMatch(param(), "(\\d+,\\d+)", tile.Capture())) {
+    return absl::InternalError("bad param");
+  }
+  if (tile.x % 2 != 1) return absl::InternalError("bad param %x");
+  if (tile.y % 2 != 1) return absl::InternalError("bad param %y");
+
+  for (std::string_view line : input) {
+    Drone d;
+    if (!RE2::FullMatch(line, "p=(-?\\d+,-?\\d+) v=(-?\\d+,-?\\d+)", d.p.Capture(), d.v.Capture())) {
+      return absl::InvalidArgumentError("bad line");
+    }
+    drones.push_back(d);
+  }
+  int steps = 0;
+  for (;; ++steps) {
+    absl::flat_hash_set<Point> test;
+    for (Drone& d : drones) {
+      test.insert(d.p);
+      d.p += d.v;
+      d.p.x %= tile.x;
+      if (d.p.x < 0) d.p.x += tile.x;
+      d.p.y %= tile.y;
+      if (d.p.y < 0) d.p.y += tile.y;
+    }
+    int edge_count = 0;
+    for (Point p : test) {
+      for (Point d : Cardinal::kFourDirs) {
+        if (test.contains(p + d)) {
+          ++edge_count;
+        }
+      }
+    }
+    if (edge_count > 1000) {
+      CharBoard b = CharBoard::DrawNew(test);
+      LOG(ERROR) << "Tree @" << steps << ";" << edge_count << "\n" << b;
+      break;
+    }
+  }
+  return AdventReturn(steps);
 }
 
 static AdventRegisterEntry registry = RegisterAdventDay(
