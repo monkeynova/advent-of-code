27f91e996cf83e380f911c7182408b5e4395a4b4
Keith Peters <keith@monkeynova.com>
2024-12-30 15:21:06 -0800

    2024/18 Include prune (and remove use of CharBoard)

--- advent_of_code/2024/day18/day18.cc
+++ advent_of_code/2024/day18/day18.cc
@@ -2,28 +2,12 @@
 
 #include "advent_of_code/2024/day18/day18.h"
 
-#include "absl/algorithm/container.h"
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
 #include "absl/log/log.h"
-#include "absl/strings/numbers.h"
-#include "absl/strings/str_cat.h"
-#include "absl/strings/str_join.h"
-#include "absl/strings/str_split.h"
-#include "advent_of_code/bfs.h"
-#include "advent_of_code/char_board.h"
-#include "advent_of_code/conway.h"
-#include "advent_of_code/directed_graph.h"
-#include "advent_of_code/fast_board.h"
-#include "advent_of_code/interval.h"
-#include "advent_of_code/loop_history.h"
-#include "advent_of_code/mod.h"
 #include "advent_of_code/point.h"
-#include "advent_of_code/point3.h"
 #include "advent_of_code/point_walk.h"
-#include "advent_of_code/splice_ring.h"
 #include "advent_of_code/tokenizer.h"
-#include "re2/re2.h"
 
 namespace advent_of_code {
 
@@ -35,17 +19,21 @@ namespace {
 
 absl::StatusOr<std::string> Day_2024_18::Part1(
     absl::Span<std::string_view> input) const {
-  CharBoard b;
+  PointRectangle bounds = {{0, 0}, {0, 0}};
+  Point end;
   int steps = 0;
   {
     Tokenizer t(param());
     ASSIGN_OR_RETURN(int dim, t.NextInt());
-    b = CharBoard(dim, dim);
+    end = {dim - 1, dim - 1};
+    bounds.ExpandInclude(end);
     RETURN_IF_ERROR(t.NextIs(","));
     ASSIGN_OR_RETURN(steps, t.NextInt());
     if (!t.Done()) return absl::InvalidArgumentError("bad param");
   }
   if (input.size() < steps) return absl::InvalidArgumentError("bad steps");
+
+  absl::flat_hash_set<Point> points;
   for (int i = 0; i < steps; ++i) {
     Point p;
     {
@@ -53,28 +41,30 @@ absl::StatusOr<std::string> Day_2024_18::Part1(
       RETURN_IF_ERROR(p.From(t));
       if (!t.Done()) return absl::InvalidArgumentError("bad point");
     }
-    if (!b.OnBoard(p)) return absl::InvalidArgumentError("off board");
-    b[p] = '#';
+    if (!bounds.Contains(p)) return absl::InvalidArgumentError("off board");
+    points.insert(p);
   }
   return AdventReturn(PointWalk({
     .start = Point{0, 0},
     .is_good = [&](Point p, int) {
-      return b.OnBoard(p) && b[p] != '#';
+      return bounds.Contains(p) && !points.contains(p);
     },
     .is_final = [&](Point p, int) {
-      return p.x == b.width() - 1 && p.y == b.height() - 1;
+      return p == end;
     }
   }).FindMinSteps());
 }
 
 absl::StatusOr<std::string> Day_2024_18::Part2(
     absl::Span<std::string_view> input) const {
-  CharBoard board;
+  PointRectangle bounds = {{0, 0}, {0, 0}};
+  Point end;
   int steps = 0;
   {
     Tokenizer t(param());
     ASSIGN_OR_RETURN(int dim, t.NextInt());
-    board = CharBoard(dim, dim);
+    end = {dim - 1, dim - 1};
+    bounds.ExpandInclude(end);
     RETURN_IF_ERROR(t.NextIs(","));
     ASSIGN_OR_RETURN(steps, t.NextInt());
     if (!t.Done()) return absl::InvalidArgumentError("bad param");
@@ -90,12 +80,10 @@ absl::StatusOr<std::string> Day_2024_18::Part2(
       RETURN_IF_ERROR(p.From(t));
       if (!t.Done()) return absl::InvalidArgumentError("bad point");
     }
-    if (!board.OnBoard(p)) return absl::InvalidArgumentError("off board");
+    if (!bounds.Contains(p)) return absl::InvalidArgumentError("off board");
     points.push_back(p);
     point_to_idx[p] = i;
   }
-
-  Point end = {board.width() - 1, board.height() - 1};
 
   int a = 0;
   int b = points.size();
@@ -104,7 +92,7 @@ absl::StatusOr<std::string> Day_2024_18::Part2(
     std::optional<int> min_steps = PointWalk({
       .start = Point{0, 0},
       .is_good = [&](Point p, int) {
-        if (!board.OnBoard(p)) return false;
+        if (!bounds.Contains(p)) return false;
         auto it = point_to_idx.find(p);
         if (it == point_to_idx.end()) return true;
         return it->second > idx;
