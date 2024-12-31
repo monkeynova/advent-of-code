7bd2f6dc9bf51092dc936943c07eeb7f7b34186b
Keith Peters <keith@monkeynova.com>
2024-12-31 11:09:07 -0800

    2024/21 Include (and code) prune

--- advent_of_code/2024/day21/day21.cc
+++ advent_of_code/2024/day21/day21.cc
@@ -2,133 +2,19 @@
 
 #include "advent_of_code/2024/day21/day21.h"
 
-#include "absl/algorithm/container.h"
 #include "absl/container/flat_hash_map.h"
-#include "absl/container/flat_hash_set.h"
 #include "absl/log/log.h"
-#include "absl/strings/numbers.h"
-#include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
-#include "absl/strings/str_split.h"
-#include "advent_of_code/bfs.h"
-#include "advent_of_code/char_board.h"
-#include "advent_of_code/conway.h"
-#include "advent_of_code/directed_graph.h"
-#include "advent_of_code/fast_board.h"
-#include "advent_of_code/interval.h"
-#include "advent_of_code/loop_history.h"
-#include "advent_of_code/mod.h"
 #include "advent_of_code/point.h"
-#include "advent_of_code/point3.h"
-#include "advent_of_code/point_walk.h"
-#include "advent_of_code/splice_ring.h"
-#include "advent_of_code/tokenizer.h"
-#include "re2/re2.h"
 
 namespace advent_of_code {
 
 namespace {
 
-class ButtonPusher : public BFSInterface<ButtonPusher> {
- public:
-  ButtonPusher(std::string_view target) : target_(target) {}
-
-  const ButtonPusher& identifier() const override { return *this; }
-  bool operator==(const ButtonPusher& o) const {
-    return target_ == o.target_ && human_ == o.human_ && robot_1_ == o.robot_1_ && robot_2_ == o.robot_2_ && robot_3_ == o.robot_3_;
-  }
-
-  template <typename H>
-  friend H AbslHashValue(H h, const ButtonPusher& bp) {
-    return H::combine(std::move(h), bp.target_, bp.human_, bp.robot_1_, bp.robot_2_, bp.robot_3_);
-  }
-
-  template <typename Sink>
-  friend void AbslStringify(Sink& sink, const ButtonPusher& bp) {
-    absl::Format(&sink, "target:%s {%v, %v, %v, %v}", bp.target_, bp.human_, bp.robot_1_, bp.robot_2_, bp.robot_3_);
-  }
-
-  bool IsFinal() const override { return target_.empty(); }
-
-  void AddNextSteps(State* state) const override {
-    // Move human.
-    for (Point d : Cardinal::kFourDirs) {
-      if (OkArrow(human_ + d)) {
-        ButtonPusher next = *this;
-        next.human_ += d;
-        state->AddNextStep(next);
-      }
-    }
-
-    // Press human.
-    {
-      ButtonPusher next = *this;
-      bool add_ok = true;
-      if (std::optional<Point> p = ArrowDir(human_)) {
-        next.robot_1_ += *p;
-      } else if (std::optional<Point> p = ArrowDir(robot_1_)) {
-        next.robot_2_ += *p;
-      } else if (std::optional<Point> p = ArrowDir(robot_2_)) {
-        next.robot_3_ += *p;
-      } else if (std::optional<char> c = KeyPadChar(robot_3_)) {
-        if (c == target_[0]) {
-          next.target_ = next.target_.substr(1);
-        } else {
-          add_ok = false;
-        }
-      }
-      if (add_ok) {
-        state->AddNextStep(next);
-      }
-    }
-
-  }
-
- private:
-  static bool OkArrow(Point p) {
-    static const PointRectangle kArrowBounds = {{0, 0}, {2, 1}};
-    static const Point kBadArrowSpot = {0, 0};
-    return kArrowBounds.Contains(p) && p != kBadArrowSpot;
-  }
-
-  static std::optional<Point> ArrowDir(Point p) {
-    if (p == Point{0, 1}) return Cardinal::kNorth;
-    if (p == Point{1, 0}) return Cardinal::kWest;
-    if (p == Point{1, 1}) return Cardinal::kSouth;
-    if (p == Point{1, 2}) return Cardinal::kEast;
-    return std::nullopt;
-  }
-
-  static bool OkKeyPad(Point p) {
-    static const PointRectangle kKeyPadBounds = {{0, 0}, {2, 3}};
-    static const Point kBadKeyPadSpot = {0, 3};
-    return kKeyPadBounds.Contains(p) && p != kBadKeyPadSpot;
-  }
-
-  static std::optional<char> KeyPadChar(Point p) {
-    if (p == Point{0, 0}) return '7';
-    if (p == Point{0, 1}) return '8';
-    if (p == Point{0, 2}) return '9';
-    if (p == Point{1, 0}) return '4';
-    if (p == Point{1, 1}) return '5';
-    if (p == Point{1, 2}) return '6';
-    if (p == Point{2, 0}) return '1';
-    if (p == Point{2, 1}) return '2';
-    if (p == Point{2, 2}) return '3';
-    if (p == Point{3, 1}) return '0';
-    return std::nullopt;
-  }
-
-  std::string_view target_;
-  Point human_ = {2, 0};
-  Point robot_1_ = {2, 0};
-  Point robot_2_ = {2, 0};
-  Point robot_3_ = {2, 3};
-};
-
-const std::vector<std::string>& AllCombinations(int a, int b, char ac, char bc) {
-  static absl::flat_hash_map<std::tuple<int, int, char, char>, std::vector<std::string>> memo;
+const std::vector<std::string>& AllCombinations(
+    int a, int b, char ac, char bc) {
   auto key = std::make_tuple(a, b, ac, bc);
+  static absl::flat_hash_map<decltype(key), std::vector<std::string>> memo;
   if (auto it = memo.find(key); it != memo.end()) {
     return it->second;
   }
@@ -157,13 +43,11 @@ const std::vector<std::string>& AllCombinations(int a, int b, char ac, char bc) 
   return memo[key] = ret;
 }
 
-using State = std::vector<Point>;
-
 int64_t ToDirectional(std::string_view line, int max_level, int level) {
   if (level == max_level) return line.size();
 
-  static absl::flat_hash_map<std::tuple<std::string, int, int>, int64_t> memo;
   auto key = std::make_tuple(std::string(line), max_level, level);
+  static absl::flat_hash_map<decltype(key), int64_t> memo;
   if (auto it = memo.find(key); it != memo.end()) {
     return it->second;
   }
@@ -270,7 +154,7 @@ absl::StatusOr<std::string> Day_2024_21::Part1(
     }
 
     int64_t best_path_size = ToKeyPad(line, 2);
-    LOG(ERROR) << line << ": " << best_path_size << " * " << num_code;
+    VLOG(2) << line << ": " << best_path_size << " * " << num_code;
     sum += best_path_size * num_code;
   }
 
@@ -288,7 +172,7 @@ absl::StatusOr<std::string> Day_2024_21::Part2(
     }
 
     int64_t best_path_size = ToKeyPad(line, 25);
-    LOG(ERROR) << line << ": " << best_path_size << " * " << num_code;
+    VLOG(2) << line << ": " << best_path_size << " * " << num_code;
     sum += best_path_size * num_code;
   }
 
