cb72db9fa077dbad4e2e87c44b78c8c259cbe051
Keith Peters <keith@monkeynova.com>
2024-12-30 17:29:30 -0800

    2024/19 Answer

--- advent_of_code/2024/day19/day19.cc
+++ advent_of_code/2024/day19/day19.cc
@@ -29,18 +29,69 @@ namespace advent_of_code {
 
 namespace {
 
-// Helper methods go here.
+bool CanMake(std::string_view test,
+             absl::Span<const std::string_view> patterns,
+             absl::flat_hash_map<std::string_view, bool>& hist) {
+  auto it = hist.find(test);
+  if (it != hist.end()) return it->second;
+
+  bool ret = false;
+  for (std::string_view p : patterns) {
+    if (test.substr(0, p.size()) != p) continue;
+    if (!CanMake(test.substr(p.size()), patterns, hist)) continue;
+    ret = true;
+    break;
+  }
+
+  return hist[test] = ret;
+}
+
+int64_t CountMakable(std::string_view test,
+                 absl::Span<const std::string_view> patterns,
+                 absl::flat_hash_map<std::string_view, int64_t>& hist) {
+  auto it = hist.find(test);
+  if (it != hist.end()) return it->second;
+
+  int64_t ret = 0;
+  for (std::string_view p : patterns) {
+    if (test.substr(0, p.size()) != p) continue;
+    ret += CountMakable(test.substr(p.size()), patterns, hist);
+  }
+
+  return hist[test] = ret;
+}
+
 
 }  // namespace
 
 absl::StatusOr<std::string> Day_2024_19::Part1(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  if (input.size() < 3) return absl::InvalidArgumentError("Too short");
+  std::vector<std::string_view> patterns = absl::StrSplit(input[0], ", ");
+  if (!input[1].empty()) return absl::InvalidArgumentError("No blank line");
+
+  absl::flat_hash_map<std::string_view, bool> hist = {{"", true}};
+  int count = 0;
+  for (int i = 2; i < input.size(); ++i) {
+    if (CanMake(input[i], patterns, hist)) {
+      ++count;
+    }
+  }
+  return AdventReturn(count);
 }
 
 absl::StatusOr<std::string> Day_2024_19::Part2(
     absl::Span<std::string_view> input) const {
-  return absl::UnimplementedError("Problem not known");
+  if (input.size() < 3) return absl::InvalidArgumentError("Too short");
+  std::vector<std::string_view> patterns = absl::StrSplit(input[0], ", ");
+  if (!input[1].empty()) return absl::InvalidArgumentError("No blank line");
+
+  absl::flat_hash_map<std::string_view, int64_t> hist = {{"", 1}};
+  int64_t count = 0;
+  for (int i = 2; i < input.size(); ++i) {
+    count += CountMakable(input[i], patterns, hist);
+  }
+  return AdventReturn(count);
 }
 
 static AdventRegisterEntry registry = RegisterAdventDay(
