68f43b16824cd025b5f3edda1cc6cf7fa3a8fde3
Keith Peters <keith@monkeynova.com>
2025-01-24 19:19:46 -0800

    run_year --year=all --summary produces an all-time per-year summary

--- run_year.cc
+++ run_year.cc
@@ -11,6 +11,9 @@
 ABSL_FLAG(std::string, year, "current",
           "Year to run. Use 'all' to run all years.");
 
+ABSL_FLAG(bool, summary, false,
+          "If true show a summary table of selected years.");
+
 namespace advent_of_code {
 namespace {
 
@@ -35,30 +38,42 @@ TextTable::Cell::Color YearTimeColor(absl::Duration d) {
   return kRed;
 };
 
-int RunYear(int year) {
+std::vector<advent_of_code::DayRun> RunYear(int year) {
   std::vector<std::unique_ptr<advent_of_code::AdventDay>> days;
   for (int day = 1; day <= 25; ++day) {
     days.push_back(advent_of_code::CreateAdventDay(year, day));
   }
   std::vector<advent_of_code::DayRun> runs;
-  absl::Duration total_time = absl::Seconds(0);
   for (const auto& day : days) {
-    if (day == nullptr) {
-      runs.push_back({
+    absl::StatusOr<advent_of_code::DayRun> run = RunDay(day.get());
+    if (!run.ok()) {
+      run = advent_of_code::DayRun{
         .time = absl::Seconds(0),
-        .title = "???",
+        .title = absl::StrCat("Error: ", run.status().message()),
         .part1 = "",
         .part1_solved = false,
         .part2 = "",
-        .part2_solved = false});
-      continue;
+        .part2_solved = false};
     }
-    absl::StatusOr<advent_of_code::DayRun> run = RunDay(day.get());
-    CHECK(run.ok()) << run.status().message() << day->test_file();
     runs.push_back(*std::move(run));
-    total_time += runs.back().time;
   }
+  return runs;
+}
 
+struct YearSummary {
+  int year;
+  absl::Duration time;
+};
+
+YearSummary SummarizeYear(int year, std::vector<advent_of_code::DayRun> runs) {
+  YearSummary ret{.year = year, .time = absl::Seconds(0)};
+  for (const advent_of_code::DayRun& run : runs) {
+    ret.time += run.time;
+  }
+  return ret;
+}
+
+std::string RenderYear(int year, std::vector<advent_of_code::DayRun> runs) {
   using enum TextTable::Cell::Color;
   using enum TextTable::Cell::Justify;
 
@@ -75,6 +90,7 @@ int RunYear(int year) {
                 TextTable::Cell{.entry = "Time", .justify = kCenter}});
   table.AddBreaker();
 
+  absl::Duration total_time = absl::Seconds(0);
   for (const advent_of_code::DayRun& run : runs) {
     table.AddRow(
         {TextTable::Cell{.entry = run.title}, TextTable::Cell{.entry = run.part1},
@@ -84,17 +100,52 @@ int RunYear(int year) {
          TextTable::Cell{.entry = TimeString(run.time),
                      .justify = kRight,
                      .color = DayTimeColor(run.time)}});
+    total_time += run.time;
   }
   table.AddBreaker();
+
   table.AddRow({TextTable::Cell{.entry = "Total", .span = 5},
                 TextTable::Cell{.entry = TimeString(total_time),
                             .justify = kRight,
                             .color = YearTimeColor(total_time)}});
   table.AddBreaker();
 
-  std::cout << table.Render();
+  return table.Render();
+}
 
-  return 0;
+std::string RenderSummaries(std::vector<YearSummary> years) {
+  using enum TextTable::Cell::Color;
+  using enum TextTable::Cell::Justify;
+
+  advent_of_code::TextTable table;
+  table.AddBreaker();
+  table.AddRow({TextTable::Cell{.entry = absl::StrCat("Advent of Code"),
+                            .justify = kCenter,
+                            .bold = true,
+                            .span = 2}});
+  table.AddBreaker();
+  table.AddRow({TextTable::Cell{.entry = "Year", .justify = kCenter},
+                TextTable::Cell{.entry = "Time", .justify = kCenter}});
+  table.AddBreaker();
+
+  absl::Duration total_time = absl::Seconds(0);
+  for (const YearSummary& year : years) {
+    table.AddRow(
+        {TextTable::Cell{.entry = absl::StrCat(year.year)},
+         TextTable::Cell{.entry = TimeString(year.time),
+                     .justify = kRight,
+                     .color = YearTimeColor(year.time)}});
+    total_time += year.time;
+  }
+  table.AddBreaker();
+
+  table.AddRow({TextTable::Cell{.entry = "Total"},
+                TextTable::Cell{.entry = TimeString(total_time),
+                            .justify = kRight,
+                            .color = YearTimeColor(total_time)}});
+  table.AddBreaker();
+
+  return table.Render();
 }
 
 absl::StatusOr<std::vector<int>> GetYearsFromFlag() {
@@ -122,6 +173,31 @@ absl::StatusOr<std::vector<int>> GetYearsFromFlag() {
   return std::vector<int>(1, year_int);
 }
 
+int RunYearMain() {
+  absl::StatusOr<std::vector<int>> years = GetYearsFromFlag();
+  if (!years.ok()) {
+    std::cerr << years.status() << std::endl;
+    return 1;
+  }
+
+  std::vector<YearSummary> summaries;
+  for (int year : *years) {
+    std::vector<DayRun> runs = RunYear(year);
+    if (absl::GetFlag(FLAGS_summary)) {
+      summaries.push_back(SummarizeYear(year, std::move(runs)));
+    } else {
+      std::string year_table = RenderYear(year, std::move(runs));
+      std::cout << year_table;
+    }
+  }
+  if (absl::GetFlag(FLAGS_summary)) {
+    std::string summary_table = RenderSummaries(std::move(summaries));
+    std::cout << summary_table;
+  }
+
+  return 0;
+}
+
 }  // namespace
 }  // namespace advent_of_code
 
@@ -135,16 +211,5 @@ int main(int argc, char** argv) {
   QCHECK_EQ(args.size(), 1) << "Extra argument specified." << std::endl
                             << absl::ProgramUsageMessage();
 
-  absl::StatusOr<std::vector<int>> years = advent_of_code::GetYearsFromFlag();
-  if (!years.ok()) {
-    std::cerr << years.status() << std::endl;
-    return 1;
-  }
-
-  for (int year : *years) {
-    int error = advent_of_code::RunYear(year);
-    if (error) return error;
-  }
-
-  return 0;
+  return advent_of_code::RunYearMain();
 }
